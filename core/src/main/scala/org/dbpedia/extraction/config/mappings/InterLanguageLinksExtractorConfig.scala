5b32236d75c36b45db79558d98a4a93e8bd83f45
Max Jakob <max.jakob@gmail.com>
2011-06-22 13:57:52 +0200

    bugfix (order of configuration statements)

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/InterLanguageLinksExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/InterLanguageLinksExtractorConfig.scala
@@ -3,12 +3,12 @@ package org.dbpedia.extraction.config.mappings
 
 object InterLanguageLinksExtractorConfig
 {
-    val supportedLanguages = intLinksMap.keySet
-
     val intLinksMap = Map(
         "de" -> Set("en", "el"),
         "el" -> Set("en", "de", "ru"),
         "en" -> Set("el", "de", "co"),
         "ru" -> Set("en", "el", "de")
     )
+
+    val supportedLanguages = intLinksMap.keySet
 }
