87448a157c13488bc5fab3c2b1e11437a573aa86
Max Jakob <max.jakob@gmail.com>
2011-06-24 15:27:11 +0200

    lazy loading of context parameters in server (optional) and dump

---
+++ server/src/main/scala/org/dbpedia/extraction/server/DynamicExtractionManager.scala
@@ -0,0 +1,54 @@
+package org.dbpedia.extraction.server
+
+import org.dbpedia.extraction.sources._
+import org.dbpedia.extraction.wikiparser.WikiTitle
+import org.dbpedia.extraction.mappings._
+import org.dbpedia.extraction.util.Language
+import org.dbpedia.extraction.ontology.Ontology
+
+/**
+ * Loads all extraction context parameters at start-up: ontology pages, mapping pages, ontology.
+ * Is able to update the ontology and the mappings.
+ */
+class DynamicExtractionManager(languages : Set[Language], extractors : List[Class[Extractor]]) extends ExtractionManager(languages, extractors)
+{
+    @volatile private var _ontologyPages : Map[WikiTitle, WikiPage] = loadOntologyPages
+
+    @volatile private var _mappingPages : Map[Language, Map[WikiTitle, WikiPage]] = loadMappingPages
+
+    @volatile private var _ontology : Ontology = loadOntology
+
+    @volatile private var _extractors : Map[Language, Extractor] = loadExtractors
+
+    @volatile private lazy val _redirects = Redirects.load(new MemorySource)
+
+
+    def extractor(language : Language) = _extractors(language)
+
+    def ontology = _ontology
+
+    def ontologyPages = _ontologyPages
+
+    def ontologyPages_= (pages : Map[WikiTitle, WikiPage])
+    {
+        _ontologyPages = pages
+        _ontology = loadOntology
+        _extractors = loadExtractors
+    }
+
+    def mappingPages(language : Language) = _mappingPages(language)
+
+    def updateMappingPage(page : WikiPage, language : Language)
+    {
+        _mappingPages = _mappingPages.updated(language, _mappingPages(language) + ((page.title, page)))
+        _extractors = _extractors.updated(language, loadExtractor(language))
+    }
+
+    def removeMappingPage(title : WikiTitle, language : Language)
+    {
+        _mappingPages = _mappingPages.updated(language, _mappingPages(language) - title)
+        _extractors = _extractors.updated(language, loadExtractor(language))
+    }
+
+    def redirects = _redirects
+}
