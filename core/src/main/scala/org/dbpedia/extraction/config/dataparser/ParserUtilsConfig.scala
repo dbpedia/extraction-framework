482f9c2ba2292f79ef910135ba8ece7d29d890cb
Jimmy O'Regan <jimregan@users.sourceforge.net>
2011-07-22 22:42:02 +0100

    add pl, +2 en

--- core/src/main/scala/org/dbpedia/extraction/config/dataparser/ParserUtilsConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/dataparser/ParserUtilsConfig.scala
@@ -9,7 +9,9 @@ object ParserUtilsConfig
             "thousand" -> 3,
             "million" -> 6,
             "mio" -> 6,
+            "mln" -> 6,
             "billion" -> 9,
+            "bln" -> 9,
             "trillion" -> 12,
             "quadrillion" -> 15
         ),
@@ -20,6 +22,25 @@ object ParserUtilsConfig
             "milliarde" -> 9,
             "mrd" -> 9,
             "billion" -> 12
+        ),
+        "pl" -> Map(
+            "tysiąc" -> 3,
+            "tysiące" -> 3,
+            "tysięcy" -> 3,
+            "tys" -> 3,
+            "tyś" -> 3,
+            "milion" -> 6,
+            "miliony" -> 6,
+            "milionów" -> 6,
+            "mln" -> 6,
+            "miliard" -> 9,
+            "miliardy" -> 9,
+            "miliardów" -> 9,
+            "mld" -> 9,
+            "bilion" -> 12,
+            "biliony" -> 12,
+            "bilionów" -> 12,
+            "bln" -> 12
         )
     )
 
