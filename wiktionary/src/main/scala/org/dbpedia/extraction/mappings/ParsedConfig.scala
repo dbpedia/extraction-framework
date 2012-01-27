496d013e241f012153c836fcad2e0151bfeccab5
Jonas Brekle <jonas.brekle@gmail.com>
2012-01-27 17:32:48 +0100

    changed config format (only german for now)

--- wiktionary/src/main/scala/org/dbpedia/extraction/mappings/ParsedConfig.scala
+++ wiktionary/src/main/scala/org/dbpedia/extraction/mappings/ParsedConfig.scala
@@ -19,27 +19,17 @@ import MyStack._
  * these classes represent configuration from the xml
  * they are simple wrappers that provide a object representation to avoid xml in the extractor
  */
+class TripleTemplate(val s : String, val p : String, val o : String, val oType : String = "URI", val oMapping : Boolean = false, val oNewBlock : Boolean = false)
 
-class Var (val name : String, val property : String, val senseBound : Boolean, val toUri : Boolean, val format : String, val doMapping : Boolean, val values : List[String])
-object Var {
-  def fromNode(n:XMLNode) = {
-    new Var(
-      (n \ "@name").text,
-      (n \ "@property").text,
-      n.attribute("senseBound").isDefined && (n \ "@senseBound").text.equals("true"),
-      n.attribute("type").isDefined && (n \ "@type").text.equals("resource"),
-      (n \ "@format").text,
-      n.attribute("doMapping").isDefined && (n \ "@doMapping").text.equals("true"),
-      (n \ "mapping").map((mapping:XMLNode)=>(mapping \"@from").text).asInstanceOf[List[String]]
-    )
-  }
-}
+class ResultTemplate(val triples : Seq[TripleTemplate]){}
 
-class Tpl (val name : String, val tpl : Stack[org.dbpedia.extraction.wikiparser.Node], val vars : scala.collection.immutable.Seq[Var], var needsPostProcessing : Boolean, var ppClass : Option[String], var ppMethod : Option[String])
+class PostProcessing(var ppClass : String, var ppMethod : String){}
+
+class Tpl (val name : String, val wiki : Stack[org.dbpedia.extraction.wikiparser.Node], val pp : Option[PostProcessing], val resultTemplates : Seq[ResultTemplate])
 object Tpl {
-  //$var, () for repetitions, and ##link## to subsumpt the three link types
+  //$var, ()*+? for repetitions, and ~~link~~ to subsumpt the three link types
   def expandTpl(orig : String) : String = {
-    println(orig)
+    //println(orig)
     val pattern = new Regex("(?<!\\\\)\\$[a-zA-Z0-9]+")
     val tplVarsExpanded = pattern.replaceAllIn(
        orig, 
@@ -59,45 +49,45 @@ object Tpl {
     val tplLinksExpanded = pattern3.replaceAllIn(tpl, 
        (m) => "{{Extractiontpl|link|"+m.matched.replace("~","")+"}}")
     val tplExpanded = tplLinksExpanded.replace("\\","")
-    println(tplExpanded)
+    //println(tplExpanded)
     return tplExpanded
   }
 
   def fromNode(n:XMLNode) = {
-    val pp = n.attribute("needsPostProcessing").isDefined && (n \ "@needsPostProcessing").text.equals("true")
+    val pp = if(n.attribute("needsPostProcessing").isDefined && (n \ "@needsPostProcessing").text.equals("true")){Some(new PostProcessing((n \ "@ppClass").text, (n \ "@ppMethod").text))} else None
 
-    //post prcessing: class and method names to call via reflection    
-    val ppClass = if(pp){
-      Some((n \ "@ppClass").text)
-    } else {
-      None
-    }
-    val ppMethod = if(pp){
-      Some((n \ "@ppMethod").text)
-    } else {
-      None
-    }
-
-    val vars = (n \ "vars" \ "var").map(Var.fromNode(_))
-    val tplString = (n \ "wikiSyntax").text
+    val tplString = (n \ "wikiTemplate").text
     // expand the terse template syntax 
     val tplExpanded = expandTpl(tplString)
 
     //println(tplExpanded);
-    val tpl =  MyStack.fromString(tplExpanded).filterNewLines
+    val tpl = MyStack.fromString(tplExpanded).filterNewLines
     if(tplExpanded.startsWith("\n")){
         tpl.push(new TextNode("\n",0))
     }
-    println(tpl)
-    val t = new Tpl(
+    
+    val rt = (n \ "resultTemplates" \ "resultTemplate").map(
+            rtn => new ResultTemplate(
+                (rtn \ "triples" \ "triple").map(
+                    t => new TripleTemplate( 
+                        (t \ "@s").text, 
+                        (t \ "@p").text, 
+                        (t \ "@o").text,
+                        if(t.attribute("oType").isDefined){(t \ "@oType").text} else {"URI"} ,
+                        t.attribute("oMapping").isDefined && (t \ "@oMapping").text.equals("true") ,
+                        t.attribute("oNewBlock").isDefined && (t \ "@oNewBlock").text.equals("true") 
+                    ) 
+                )
+            )
+   )
+    
+    //return
+    new Tpl(
       (n \ "@name").text,
       tpl,
-      vars,
-      pp,
-      ppClass,
-      ppMethod
+      pp, 
+      rt
     )
-    t
   }
 }
 
@@ -105,7 +95,7 @@ class Block (n : XMLNode, val parent : Block
 //val name : String, val indTpl : Tpl, val blocks : List[Block], val templates : List[Tpl], val property : String, val parent : Block
 ){
       val name = (n \ "@name").text
-      val indTpl = if((n \ "template").size > 0) {Tpl.fromNode((n \ "template").head)} else null
+      val indTpl = if((n \ "indicator").size > 0) {Tpl.fromNode((n \ "indicator").head)} else null
       val blocks = (n \ "block").map(b => new Block(b, this)).toList
       val templates  = (n \ "templates" \ "template").map(t=>Tpl.fromNode(t)).toList
       val property = (n \ "@property").text
--- wiktionary/src/main/scala/org/dbpedia/extraction/mappings/WiktionaryPageExtractor.scala
+++ wiktionary/src/main/scala/org/dbpedia/extraction/mappings/WiktionaryPageExtractor.scala
@@ -13,6 +13,7 @@ import java.io.FileWriter
 import java.net.URLEncoder
 import java.lang.StringBuffer
 import xml.{XML, Node => XMLNode}
+import scala.util.matching.Regex
 import collection.mutable.{HashMap, Stack, ListBuffer, Set}
 
 //some of my utilities
@@ -55,7 +56,6 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
   val language = properties("language")
   val logLevel = properties("logLevel").toInt
 
-  //todo refactor  constructor to use the new format (how to pass values like logLevel?)
   val langObj = new Language(language, new Locale(language))
 
   val vf = ValueFactoryImpl.getInstance
@@ -67,13 +67,17 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
   val senseProperty = properties.get("senseProperty").getOrElse("http://undefined.com/sense")
   val senseIdVarName = properties.get("senseVarName").getOrElse("meaningId")
   val labelProperty = properties.get("labelProperty").getOrElse("http://undefined.com/label")
+  val varPattern = new Regex("\\$[a-zA-Z0-9]+")
 
   private val languageConfig = XML.loadFile("config-"+language+".xml")
 
-  //private val templates = (languageConfig \ "templates" \ "sections" \ "template").map((n : XMLNode) => Tpl.fromNode(n))
-
   private val mappings : Map[String, String] = (languageConfig \\ "mapping").map(
-      (n : XMLNode) =>
+      (n : XMLNode) => //language specific mappings (from language to global vocabulary)
+        ( (n \ "@from").text,
+          (n \ "@to").text
+        )
+      ).toMap ++ (config \ "mappings"\ "mapping").map(
+      (n : XMLNode) => // general mappings (from language codes to global vocabulary)
         ( (n \ "@from").text,
           (n \ "@to").text
         )
@@ -95,9 +99,10 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
   override def extract(page: PageNode, subjectUri: String, pageContext: PageContext): Graph =
   {
     // wait a random number of seconds. kills parallelism - otherwise debug output from different threads is mixed
-    //TODO remove if in production
-    //val r = new scala.util.Random
-    //Thread sleep r.nextInt(10)*1000
+    if(logLevel > 0){
+      val r = new scala.util.Random
+      Thread sleep r.nextInt(10)*1000
+    }
     val missingMappingsT = Set[String]()
     missingMappingsT ++= missingMappings
     missingMappings.empty
@@ -137,7 +142,7 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
         val prologtpl = Tpl.fromNode(prolog)
         WiktionaryLogging.printMsg("try "+prologtpl.name, 2)
          try {
-          proAndEpilogBindings.append( (prologtpl, parseNodesWithTemplate(prologtpl.tpl, pageStack)) )
+          proAndEpilogBindings.append( (prologtpl, parseNodesWithTemplate(prologtpl.wiki, pageStack)) )
         } catch {
           case e : WiktionaryException => proAndEpilogBindings.append( (prologtpl, e.vars) )
         }
@@ -149,7 +154,7 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
       for(epilog <- languageConfig \ "page" \ "epilogs" \ "template"){
         val epilogtpl = Tpl.fromNode(epilog)
         try {
-          proAndEpilogBindings.append( (epilogtpl, parseNodesWithTemplate(epilogtpl.tpl, rev)) )
+          proAndEpilogBindings.append( (epilogtpl, parseNodesWithTemplate(epilogtpl.wiki, rev)) )
         } catch {
           case e : WiktionaryException => proAndEpilogBindings.append( (epilogtpl, e.vars) )
         }
@@ -162,7 +167,7 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
 
       //handle the bindings from pro- and epilog
       proAndEpilogBindings.foreach({case (tpl : Tpl, tplBindings : VarBindingsHierarchical) => {
-         quads appendAll handleBlockBinding(pageConfig, tpl, tplBindings, senses, blockIris)
+         quads appendAll handleFlatBindings(pageConfig, tpl, tplBindings.getFlat(), blockIris, blockIris("page").stringValue)
       }})
       WiktionaryLogging.printMsg("pro- and epilog bindings handled", 2)
 
@@ -191,84 +196,64 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
           try {
             //println("vs")
             //println(block.indTpl.tpl.map(_.dumpStrShort).mkString )
-            val blockBindings =  parseNodesWithTemplate(tpl.tpl.clone, pageStack)
+            val blockBindings =  parseNodesWithTemplate(tpl.wiki.clone, pageStack)
             //no exception -> success -> stuff below here will be executed on success
             consumed = true
             WiktionaryLogging.printMsg("finished template "+tpl.name+" successfully", 2)
 
             //generate triples
             //println(tpl.name +": "+ blockBindings.dump())
-            quads appendAll handleBlockBinding(curBlock, tpl, blockBindings, senses, blockIris)
+            quads appendAll handleFlatBindings(curBlock, tpl, blockBindings.getFlat(), blockIris, blockIris(curBlock.name).stringValue)
           } catch {
             case e : WiktionaryException => //did not match
           }
         }
 
         if(pageStack.size > 0){
-        // try recognizing block starts of blocks. if recognized we go somewhere UP the hierarchy (the block ended) or one step DOWN (new sub block)
-        // each block has a "indicator-template" (indTpl)
-        // when it matches, the block starts. and from that template we get bindings that describe the block
+          // try recognizing block starts of blocks. if recognized we go somewhere UP the hierarchy (the block ended) or one step DOWN (new sub block)
+          // each block has a "indicator-template" (indTpl)
+          // when it matches, the block starts. and from that template we get bindings that describe the block
 
-        WiktionaryLogging.printMsg("trying block indicator templates. page node: "+pageStack.head.toWikiText, 2)
-        breakable {
-        for(block <- possibleBlocks){
-          if(block.indTpl == null){
-            //continue - the "page" block has no indicator template, it starts implicitly with the page
-          } else {
-            //println(pageStack.take(1).map(_.dumpStrShort).mkString)
-            try {
-
-              //println("vs")
-              //println(block.indTpl.tpl.map(_.dumpStrShort).mkString )
-              val blockIndBindings = parseNodesWithTemplate(block.indTpl.tpl.clone, pageStack)
-              //no exception -> success -> stuff below here will be executed on success
-              consumed = true
-              curBlock = block
-              WiktionaryLogging.printMsg("block indicator template "+block.indTpl.name+" matched", 2)
-
-              //check where in the hierarchy the new opended block is
-              if(!curOpenBlocks.contains(block)){
-                // the new block is not up in the hierarchy
-                // go one step down/deeper 
-               curOpenBlocks append block
+          WiktionaryLogging.printMsg("trying block indicator templates. page node: "+pageStack.head.toWikiText, 2)
+          breakable {
+            for(block <- possibleBlocks){
+              if(block.indTpl == null){
+                //continue - the "page" block has no indicator template, it starts implicitly with the page
               } else {
-                //the new block somewhere up the hierarchy
-                val newOpen = curOpenBlocks.takeWhile(_ != block) 
-                curOpenBlocks.clear()
-                curOpenBlocks.appendAll(newOpen) 
-                curOpenBlocks.append(block)// up
-              }
-    
-              //build a uri for the block
-              val blockIdentifier = new StringBuffer(blockIris(curBlock.parent.name).stringValue)
-
-              block.indTpl.vars.foreach((varr : Var) => {
-                //concatenate all binding values of the block indicator tpl to form a proper name for the block (sufficient?)
-                val objStr = blockIndBindings.getFirstBinding(varr.name).getOrElse(List()).myToString
-                val localBlockPropertyMapped = handleObjectMapping(varr, objStr, false) //this false forces a literal to be returned
-                blockIdentifier append ("-" + URLEncoder.encode(localBlockPropertyMapped.asInstanceOf[Literal].getLabel, "UTF-8") )
-              })
-              val blockIri = vf.createURI(blockIdentifier.toString.replace(" ",""))
-              blockIris(block.name) = blockIri
-              //generate triples that identify the block
-              block.indTpl.vars.foreach((varr : Var) => {
-                val objStr = blockIndBindings.getFirstBinding(varr.name).getOrElse(List()).myToString
-                if(objStr != ""){
-                    val obj = handleObjectMapping(varr, objStr)
-                    quads += new Quad(langObj,wiktionaryDataset, blockIri, vf.createURI(varr.property), obj, tripleContext)
+                //println(pageStack.take(1).map(_.dumpStrShort).mkString)
+                try {
+                  //println("vs")
+                  //println(block.indTpl.tpl.map(_.dumpStrShort).mkString )
+                  val blockIndBindings = parseNodesWithTemplate(block.indTpl.wiki.clone, pageStack)
+                  //no exception -> success -> stuff below here will be executed on success
+                  consumed = true
+                  var oldBlockUri = blockIris(curBlock.name).stringValue
+                  curBlock = block //switch to new block
+                  WiktionaryLogging.printMsg("block indicator template "+block.indTpl.name+" matched", 2)
+                  
+                  //check where in the hierarchy the new opended block is
+                  if(!curOpenBlocks.contains(block)){
+                    // the new block is not up in the hierarchy
+                    // go one step down/deeper 
+                   curOpenBlocks append block
+                  } else {
+                    //the new block somewhere up the hierarchy
+                    val newOpen = curOpenBlocks.takeWhile(_ != block) 
+                    oldBlockUri = blockIris(newOpen.last.name).stringValue
+                    curOpenBlocks.clear()
+                    curOpenBlocks.appendAll(newOpen) 
+                    curOpenBlocks.append(block)// up
+                  }
+                  
+                  quads appendAll handleFlatBindings(block, block.indTpl, blockIndBindings.getFlat(), blockIris, oldBlockUri)
+                  
+                  break; //dont match another block indicator template right away (continue with this blocks templates)
+                } catch {
+                  case e : WiktionaryException => //did not match
                 }
-              })
-
-              //generate a triple that connects the parent block to the new block
-              quads += new Quad(langObj,wiktionaryDataset, blockIris(curBlock.parent.name), vf.createURI(block.property), blockIri, tripleContext)
-              WiktionaryLogging.printMsg("entering "+blockIri, 2)
-              break; //dont match another block indicator template right away (continue with this blocks templates)
-            } catch {
-              case e : WiktionaryException => //did not match
+              }
             }
           }
-        }
-        }
         }
         if(!consumed){
           WiktionaryLogging.printMsg("skipping unconsumable node ", 2)
@@ -291,110 +276,78 @@ class WiktionaryPageExtractor( context : {} ) extends Extractor {
     }
 
     WiktionaryLogging.printMsg(""+quads.size+" quads extracted for "+word, 1)
-    val quadsSorted = quads.sortWith((q1, q2)=> q1.subject.compare(q2.subject) < 0).toList
+    val seen : Set[Quad] = Set()
+    val quadsSorted = quads.groupBy(_.renderNTriple).map(_._2.head).toList.sortWith((q1, q2)=> q1.renderNTriple.compareTo(q2.renderNTriple) > 0)
     quadsSorted.foreach( q => { WiktionaryLogging.printMsg(q.renderNTriple, 1) } )
     new Graph(quadsSorted)
   }
 
-  /**
-  * transform var bindings to triples
-  */
-  def handleBlockBinding(block : Block, tpl : Tpl, blockBindings : VarBindingsHierarchical, emittedBlockSenseConnections : HashMap[String, Set[String]], blockIris : HashMap[String, URI]) : List[Quad] = {
+  def handleFlatBindings(block : Block, tpl : Tpl, blockBindings : VarBindings, blockIris : HashMap[String, URI], thisBlockIri : String) : List[Quad] = {
     val quads = new ListBuffer[Quad]
-
-    val blockName = block.name
-    if(tpl.needsPostProcessing){
-      //TODO does not work yet, implement the invocation of a static method that does a transformation of the bindings
-      val clazz = ClassLoader.getSystemClassLoader().loadClass(tpl.ppClass.get)
-      val method = clazz.getDeclaredMethod(tpl.ppMethod.get, null);
-      val ret = method.invoke(blockBindings, null)
-      quads ++= ret.asInstanceOf[List[Quad]]
-    } else {
-      //generate a triple for each var binding
-      tpl.vars.foreach((varr : Var) => {
-        if(varr.senseBound){
-          //handle sense bound vars (e.g. meaning)
-          val bindings = blockBindings.getAllSenseBoundVarBindings(varr.name, senseIdVarName)
-          bindings.foreach({case (sense, senseBindings) =>
-            senseBindings.foreach((binding)=>{
-              //the sense identifier is mostly something like "[1]" - sense is then List(TextNode("1"))
-              val objStr = binding.myToString
-
-              //avoid useless triples
-              if(!objStr.equals("")){
-                //map object from (language-specific) literals to (universal) URIs if possible
-                val obj = handleObjectMapping(varr, objStr)
-                val senseUri = vf.createURI(blockIris(blockName).stringValue + "-" + sense.myToString)
-
-                quads += new Quad(langObj,wiktionaryDataset, senseUri, vf.createURI(varr.property), obj, tripleContext)
-
-                //connect emittedBlockSenseConnections to their blocks (collect for distinctness)
-                var emitThis = false
-                if(!emittedBlockSenseConnections.contains(blockIris(blockName).stringValue)  ){
-                  emittedBlockSenseConnections(blockIris(blockName).stringValue) = Set(senseUri.stringValue)
-                  emitThis = true
-                } else if(!emittedBlockSenseConnections(blockIris(blockName).stringValue).contains(senseUri.stringValue)){
-                  emittedBlockSenseConnections(blockIris(blockName).stringValue).add(senseUri.stringValue)
-                  emitThis = true
-                }
-                if(emitThis){
-                  quads += new Quad(langObj,wiktionaryDataset, blockIris(blockName), senseIriRef, senseUri, tripleContext)
-                }
+    val bindings = blockBindings.bindings
+    var curBlockIri = thisBlockIri
+    bindings.foreach( (binding : HashMap[String, List[Node]]) => {
+      WiktionaryLogging.printMsg("bindings "+binding, 2)
+      tpl.resultTemplates.foreach( (rt : ResultTemplate) => {
+        try {
+          val thisTplQuads = new ListBuffer[Quad]
+          rt.triples.foreach( (tt : TripleTemplate) => {
+            val s = varPattern.replaceAllIn( tt.s, 
+              (m) => {
+                val varName = m.matched.replace("$","");
+                if(!varName.equals("block")){
+                  if(!binding.contains(varName)){throw new Exception("missing binding for "+varName)};
+                  binding(varName).myToString
+                } else {curBlockIri}
               }
-            })
-          })
-        } else {
-          //handle non-sense bound vars - they are related to the whole block/usage (e.g. hyphenation)
-          val bindings = blockBindings.getAllBindings(varr.name)
-          for(binding <- bindings){
-            val objStr = binding.myToString
-            if(!objStr.equals("")){
-              val obj = handleObjectMapping(varr, objStr)
-              if(!(varr.property == "" | varr.property == null)){
-                quads += new Quad(langObj, wiktionaryDataset, blockIris(blockName), vf.createURI(varr.property), obj, tripleContext)
+            )
+
+            val p = varPattern.replaceAllIn( tt.p, 
+              (m) => {
+                val varName = m.matched.replace("$","");
+                if(!varName.equals("block")){
+                  if(!binding.contains(varName)){throw new Exception("missing binding for "+varName)};
+                  binding(varName).myToString
+                } else {curBlockIri}
+              }
+            )
+
+            val o = varPattern.replaceAllIn( tt.o, 
+              (m) => {
+                val varName = m.matched.replace("$","");
+                if(!varName.equals("block")){
+                  if(!binding.contains(varName)){throw new Exception("missing binding for "+varName)};
+                  if(tt.oMapping){
+                    val b = binding(varName).myToString
+                    mappings.getOrElse(b, b)
+                  } else {
+                    binding(varName).myToString
+                  }
+                } else {curBlockIri}
+              }
+            )
+            WiktionaryLogging.printMsg("emmiting triple "+s+" "+p+" "+o, 2)
+            val oObj = if(tt.oType == "URI"){
+                val blockIri = vf.createURI(o)
+                if(tt.oNewBlock){
+                  blockIris(block.name) = blockIri //save for later
+                  WiktionaryLogging.printMsg("entering "+blockIri, 2)
+                  curBlockIri = o
+                }
+                blockIri
               } else {
-                WiktionaryLogging.printMsg("empty property for var="+varr.name+" blockIri="+blockIris(blockName),2)
-                //throw new Exception("property")
+                vf.createLiteral(o)
               }
-            } else {
-                WiktionaryLogging.printMsg("empty value for var="+varr.name+" blockIri="+blockIris(blockName),2)
-                //throw new Exception("value")
-            }
-          }
+            thisTplQuads += new Quad(langObj, wiktionaryDataset, vf.createURI(s), vf.createURI(p), oObj, tripleContext)
+          })
+          quads appendAll thisTplQuads //the triples are added at once after they all were created without a exception
+        } catch {
+          case e1:java.util.NoSuchElementException => e1.printStackTrace
+          case e => println(e)
         }
       })
-    }
-
-    /*//emit connections from block to its emittedBlockSenseConnections
-    for((parentBlock:URI, sense:URI) <- emittedBlockSenseConnections)  {
-      quads += new Quad(langObj,wiktionaryDataset, parentBlock, senseIriRef, sense, tripleContext)
-    }*/
+    })
     quads.toList
   }
 
-  /**
-  * Transform a extracted String to Literal or URI according to the definition of the variable 
-  */
-  def handleObjectMapping(varr: Var, objectStr: String, toUri : Boolean = true) : Value = {
-    if(varr.doMapping){
-      if(mappings.contains(objectStr)){
-        usedMappings += objectStr
-        val mapped = mappings(objectStr)
-        if(toUri){
-          if(!varr.format.equals("")){
-            vf.createURI(varr.format.format(mapped) )
-          } else {
-            vf.createURI(ns + mapped)
-          }
-        } else {
-          vf.createLiteral(mapped)
-        }
-      } else {
-        missingMappings += objectStr
-        vf.createLiteral(objectStr)
-      }
-    } else {
-      vf.createLiteral(objectStr)
-    }
-  }
 }
