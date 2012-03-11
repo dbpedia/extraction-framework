9dd2c77fd181e557eb3fa0b29ea65de2e4f8ef8e
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-03-11 06:14:52 +0100

    helper class to limit number of written triples

---
+++ core/src/main/scala/org/dbpedia/extraction/destinations/ThrottlingDestination.scala
@@ -0,0 +1,20 @@
+package org.dbpedia.extraction.destinations
+
+/**
+ * Passes quads through to the target destination until a maximum number of
+ * quads is reached. After that, additional quads are ignored.
+ */
+class ThrottlingDestination( destination : Destination, limit : Int ) extends Destination
+{
+    private var count = 0
+    
+    override def write( g : Graph ) = if (count < limit)
+    {
+      var graph = g
+      if (count + graph.quads.length > limit) graph = new Graph(graph.quads.take(limit - count))
+      destination.write(graph)
+      count += graph.quads.length
+    }
+
+    override def close = destination.close
+}
