7be33ed4fdfddeb10c16f6e930d4b5a2343ba283
Pablo Mendes <pablomendes@gmail.com>
2012-03-16 13:15:27 +0100

    GenderExtractor: added reflexive pronouns

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/GenderExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/GenderExtractorConfig.scala
@@ -4,8 +4,8 @@ object GenderExtractorConfig
 {
 
     val pronounsMap = Map(
-        "en" -> Map("she" -> "female", "her" -> "female", "he" -> "male", "his" -> "male", "him" -> "male",
-                    "She" -> "female", "Her" -> "female", "He" -> "male", "His" -> "male"
+        "en" -> Map("she" -> "female", "her" -> "female", "he" -> "male", "his" -> "male", "him" -> "male", "herself" -> "female", "himself" -> "male",
+                    "She" -> "female", "Her" -> "female", "He" -> "male", "His" -> "male", "Him" -> "male", "Herself" -> "female", "Himself" -> "male" //TODO why not just do case insensitive matches?
         )
     )
 
