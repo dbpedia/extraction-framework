344b23fe3a73384e169e75e63726c96ada203fbd
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-03-19 21:03:15 +0100

    comment: make mutable

--- core/src/main/scala/org/dbpedia/extraction/destinations/Graph.scala
+++ core/src/main/scala/org/dbpedia/extraction/destinations/Graph.scala
@@ -1,7 +1,13 @@
 package org.dbpedia.extraction.destinations
 
 /**
- * Holds a set of statements, organized in different datasets
+ * Holds a set of statements, organized in different datasets.
+ * 
+ * TODO: make this mutable - allow adding quads to an existing graph. 
+ * There are hundreds of places where we need this. If concurrency
+ * is a problem for dbpedia live, use concurrent collections or
+ * synchronization, but it probably isn't - usually, a graph is 
+ * extracted by one extractor and then sent to a destination.
  */
 class Graph(val quads : List[Quad] = Nil)
 {
@@ -12,6 +18,9 @@ class Graph(val quads : List[Quad] = Nil)
 
     /**
      * Retrieves all statements, grouped by their dataset.
+     * TODO: if this class is made mutable, this should return
+     * a read-only view of a private map . When a quad is added 
+     * to the list, it must also be added to a private map.
      */
     lazy val quadsByDataset : scala.collection.Map[Dataset, List[Quad]] = quads.groupBy(quad => quad.dataset)
 
