4c633e6bbca98d6f09ff709b6d0b9f7a9fd75cb0
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-03-19 21:01:22 +0100

    whitespace

--- core/src/main/scala/org/dbpedia/extraction/destinations/Quad.scala
+++ core/src/main/scala/org/dbpedia/extraction/destinations/Quad.scala
@@ -9,34 +9,34 @@ import org.dbpedia.extraction.util.Language
  * Represents a statement in the N-Quads format (see: http://sw.deri.org/2008/07/n-quads/)
  */
 //TODO write out equivalent properties
-class Quad(	val language : Language,
+class Quad(    val language : Language,
             val dataset : Dataset,
             val subject : String,
-		    val predicate : String,
-		    val value : String,
-		    val context : String,
-		    var datatype : Datatype )
+            val predicate : String,
+            val value : String,
+            val context : String,
+            var datatype : Datatype )
 {
     //Validate input
-	if(subject == null) throw new NullPointerException("subject")
-	if(predicate == null) throw new NullPointerException("predicate")
-	if(value == null) throw new NullPointerException("value")
-	if(context == null) throw new NullPointerException("context")
+    if(subject == null) throw new NullPointerException("subject")
+    if(predicate == null) throw new NullPointerException("predicate")
+    if(value == null) throw new NullPointerException("value")
+    if(context == null) throw new NullPointerException("context")
 
     if(value.isEmpty) throw new IllegalArgumentException("Value is empty")
 
     //TODO validate them on creation, now can be either URI/IRI
-	//new URI(subject)
-	//new URI(context)
-	//if(datatype == null) new URI(value)
+    //new URI(subject)
+    //new URI(context)
+    //if(datatype == null) new URI(value)
 
     def this(language : Language,
              dataset : Dataset,
              subject : String,
-		     predicate : OntologyProperty,
-		     value : String,
-		     context : String,
-		     datatype : Datatype = null) = this(language, dataset, subject, Quad.validatePredicate(predicate, datatype), value, context, Quad.getType(predicate, datatype))
+             predicate : OntologyProperty,
+             value : String,
+             context : String,
+             datatype : Datatype = null) = this(language, dataset, subject, Quad.validatePredicate(predicate, datatype), value, context, Quad.getType(predicate, datatype))
 
 
     def renderNTriple = render(false)
@@ -47,7 +47,7 @@ class Quad(	val language : Language,
 
     private def render(includeContext : Boolean) : String =
     {
-    	val sb = new StringBuilder
+        val sb = new StringBuilder
 
         sb append "<" append subject append "> "
 
@@ -57,9 +57,9 @@ class Quad(	val language : Language,
         {
             if (datatype.uri == "http://www.w3.org/2001/XMLSchema#string")
             {
-            	sb append '"'
-            	escapeString(sb, value) //sb append value //#int escapeString(sb, value)
-            	sb append "\""
+                sb append '"'
+                escapeString(sb, value) //sb append value //#int escapeString(sb, value)
+                sb append "\""
 
                 sb append "@" + language.locale.getLanguage + " "
             }
@@ -97,11 +97,11 @@ class Quad(	val language : Language,
         sb.toString()
     }
 
-	/**
-	 * Escapes an unicode string according to N-Triples format
-	 */
-	private def escapeString(sb : StringBuilder, input : String) : StringBuilder =
-	{
+    /**
+     * Escapes an unicode string according to N-Triples format
+     */
+    private def escapeString(sb : StringBuilder, input : String) : StringBuilder =
+    {
         // iterate over code points (http://blogs.sun.com/darcy/entry/iterating_over_codepoints)
         val inputLength = input.length
         var offset = 0
@@ -111,30 +111,30 @@ class Quad(	val language : Language,
             val c = input.codePointAt(offset)
             offset += Character.charCount(c)
 
-    		//Ported from Jena's NTripleWriter
-			if (c == '\\' || c == '"')
-			{
-				sb append '\\' append c.toChar
-			}
-			else if (c == '\n')
-			{
-				sb append "\\n"
-			}
-			else if (c == '\r')
-			{
-				sb append "\\r";
-			}
-			else if (c == '\t')
-			{
-				sb append "\\t"
-			}
-			else if (c >= 32 && c < 127)
-			{
-				sb append c.toChar
-			}
-			else
-			{
-				val hexStr = c.toHexString.toUpperCase
+            //Ported from Jena's NTripleWriter
+            if (c == '\\' || c == '"')
+            {
+                sb append '\\' append c.toChar
+            }
+            else if (c == '\n')
+            {
+                sb append "\\n"
+            }
+            else if (c == '\r')
+            {
+                sb append "\\r";
+            }
+            else if (c == '\t')
+            {
+                sb append "\\t"
+            }
+            else if (c >= 32 && c < 127)
+            {
+                sb append c.toChar
+            }
+            else
+            {
+                val hexStr = c.toHexString.toUpperCase
                 val hexStrLen = hexStr.length
 
                 if (c <= 0xffff)
@@ -154,11 +154,11 @@ class Quad(	val language : Language,
                     throw new CharConversionException("code point "+c+" outside of range (0x0000..0x10ffff)")
                 }
 
-				sb append hexStr
-			}
-		}
-		sb
-	}
+                sb append hexStr
+            }
+        }
+        sb
+    }
 }
 
 object Quad
@@ -166,15 +166,15 @@ object Quad
     private def validatePredicate(predicate : OntologyProperty, datatype : Datatype) : String = predicate.uri //TODO
 
     // TODO: why check this special case here, but not others? We should
-	// check that the given type is a sub type of the predicate range.
-	//	if ($this->predicate instanceof \dbpedia\ontology\OntologyDataTypeProperty)
-	//	{
-	//		if (($this->type instanceof \dbpedia\ontology\dataTypes\DimensionDataType))
-	//		{
-	//			$this->validationErrors[] = "- Ontology Property Range is not a Unit: '".$this->type->getName()."'";
-	//			$valid = false;
-	//		}
-	//	}
+    // check that the given type is a sub type of the predicate range.
+    //    if ($this->predicate instanceof \dbpedia\ontology\OntologyDataTypeProperty)
+    //    {
+    //        if (($this->type instanceof \dbpedia\ontology\dataTypes\DimensionDataType))
+    //        {
+    //            $this->validationErrors[] = "- Ontology Property Range is not a Unit: '".$this->type->getName()."'";
+    //            $valid = false;
+    //        }
+    //    }
 
     private def getType(predicate : OntologyProperty, datatype : Datatype) : Datatype =
     {
