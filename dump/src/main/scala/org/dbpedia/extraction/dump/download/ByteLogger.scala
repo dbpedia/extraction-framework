bd0884f2f29dff90f7f1ab8a4a99f0f78f027559
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-03-22 15:01:45 +0100

    new download stuff

---
+++ dump/src/main/scala/org/dbpedia/extraction/dump/download/ByteLogger.scala
@@ -0,0 +1,45 @@
+package org.dbpedia.extraction.dump.download
+
+class ByteLogger( step : Long ) extends ((Long, Boolean) => Unit)
+{
+  private var nanos = System.nanoTime
+  
+  private var next : Long = step
+  
+  var length : Long = 0
+  
+  def apply( bytes : Long, close : Boolean = false ) : Unit =
+  {
+    if (close || bytes >= next)
+    {
+      val millis = (System.nanoTime - nanos) / 1000000
+      print("read "+formatBytes(bytes)+" of "+formatBytes(length)+" in "+formatMillis(millis)+" ("+formatRate(bytes, millis)+")")
+      if (close) println // new line 
+      else print('\r') // back to start of line
+      next = (bytes / step + 1) * step
+    }
+  }
+  
+  private def formatBytes( bytes : Long ) : String =
+  {
+    if (bytes < 1024) bytes+" B"
+    else if (bytes < 1048576) (bytes / 1024F)+" KB"
+    else if (bytes < 1073741824) (bytes / 1048576F)+" MB"
+    else (bytes / 1073741824F)+" GB"
+  }
+  
+  private def formatMillis( millis : Long ) : String =
+  {
+    if (millis < 60000) millis / 1000F+" seconds"
+    else if (millis < 3600000) zeros(millis / 60000)+":"+zeros(millis % 60000 / 1000)+" minutes"
+    else zeros(millis / 3600000)+":"+zeros(millis % 3600000 / 60000)+":"+zeros(millis % 60000 / 1000)+" hours"
+  }
+  
+  private def formatRate(bytes : Long, millis : Long) : String =
+  {
+    if (bytes / millis > 1024) (bytes / 1048.576F / millis)+" MB/s"
+    else (bytes / 1.024F / millis)+" KB/s"
+  }
+  
+  private def zeros( num : Long ) : String = if (num < 10) "0"+num else num.toString
+}
---
+++ dump/src/main/scala/org/dbpedia/extraction/dump/download/WikiInfo.scala
@@ -0,0 +1,90 @@
+package org.dbpedia.extraction.dump.download
+
+import java.net.URL
+import scala.io.{Source, Codec}
+import java.io.File
+import scala.collection.Seq
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Information about a Wikipedia.
+ */
+case class WikiInfo(language : String, pages : Int)
+
+/**
+ * Helper methods to create WikiInfo objects.
+ */
+object WikiInfo
+{
+  /**
+   * Simple regex matching Wikipedia language codes.
+   * Language codes have at least two characters, start with a lower-case letter and contain only 
+   * lower-case letters and dash, but there are also dumps for "wikimania2005wiki" etc.
+   */
+  val Language = """([a-z][a-z0-9-]+)""".r
+    
+  def fromFile( file : File, codec : Codec ) : Seq[WikiInfo] =
+  {
+    val source = Source.fromFile(file)(codec)
+    try
+    {
+      fromSource(source)
+    }
+    finally source.close
+  }
+  
+  def fromURL( url : URL, codec : Codec ) : Seq[WikiInfo] =
+  {
+    val source = Source.fromURL(url)(codec)
+    try
+    {
+      fromSource(source)
+    }
+    finally source.close
+  }
+  
+  def fromSource( source : Source ) : Seq[WikiInfo] =
+  {
+    fromLines(source.getLines)
+  }
+  
+  /**
+  * Retrieves a list of all available Wikipedias from a CSV file like
+  * 
+  */
+  def fromLines( lines : Iterator[String] ) : Seq[WikiInfo] =
+  {
+    val info = new ArrayBuffer[WikiInfo]
+    
+    if (! lines.hasNext) throw new Exception("empty file")
+    lines.next // skip first line
+    
+    lines.foreach(line => if (line.nonEmpty) info += fromLine(line))
+    
+    info
+  }
+  
+  /**
+   * Reads a WikiInfo object from a single CSV line.
+   */
+  def fromLine( line : String ) : WikiInfo =
+  {
+      val fields = line.split(",", -1)
+      
+      if (fields.length != 15) throw new Exception("expected [15] fields, found ["+fields.length+"] in line ["+line+"]")
+      
+      val pages = try
+      {
+        fields(5).toInt
+      }
+      catch
+      {
+        case nfe : NumberFormatException => throw new Exception("expected page count in field with index [5], found line ["+line+"]")
+      }
+      
+      val wikiCode = fields(2)
+      if (! Language.pattern.matcher(fields(2)).matches) throw new Exception("expected language code in field with index [2], found line ["+line+"]")
+      
+      new WikiInfo(wikiCode, pages)
+  }
+}
