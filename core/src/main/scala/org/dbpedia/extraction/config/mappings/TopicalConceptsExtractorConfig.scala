2f0495c3577d2655293f53413d2c91954cbc68f8
Pablo Mendes <pablomendes@gmail.com>
2012-03-26 18:22:33 +0200

    cat main for other languages

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
@@ -3,6 +3,10 @@ package org.dbpedia.extraction.config.mappings
 object TopicalConceptsExtractorConfig
 {
 
-    val catMainTemplates = Set("Cat_main")
+    //FIXME name conflict between categories might exist between languages. add language-specific maps
+    val catMainTemplates = Set("Cat_main",
+                               "Artigo principal", // for pt.wikipedia.org
+                               "AP"                // for es.wikipedia.org
+                            )
 
 }
