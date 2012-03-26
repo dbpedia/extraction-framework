bb8b6cb23f99541fcaee1067e65585b9658decfe
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-03-26 02:38:19 +0200

    cleanup copied code; we don't need an extra class for statistics

--- server/src/main/scala/org/dbpedia/extraction/server/resources/Root.scala
+++ server/src/main/scala/org/dbpedia/extraction/server/resources/Root.scala
@@ -6,8 +6,7 @@ import javax.ws.rs.{GET, Path, Produces}
 @Path("/")
 class Root
 {
-    @GET
-    @Produces(Array("application/xhtml+xml"))
+    @GET @Produces(Array("application/xhtml+xml"))
     def get =
     {
         <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
@@ -15,43 +14,30 @@ class Root
           <body>
             <h2>Server</h2>
             <p><a href="ontology/">Ontology</a></p>
-            <p><a href="statistics/">Statistics</a></p>
+            <p><a href="statistics/">Statistics</a> - <a href="mappings/">Mappings</a> - <a href="extraction/">Extractors</a></p>
             {
               for(lang <- Server.config.languages; code = lang.wikiCode) yield
               {
-                  <p><a href={"mappings/" + code + "/"}>Mappings in {code}</a> - <a href={"extraction/" + code + "/"}>Extractor in {code}</a></p>
+                  <p><a href={"statistics/" + code + "/"}>Statistics for {code}</a> - <a href={"mappings/" + code + "/"}>Mappings in {code}</a> - <a href={"extraction/" + code + "/"}>Extractor in {code}</a></p>
               }
             }
           </body>
         </html>
     }
     
-    @GET
-    @Path("mappings/")
-    @Produces(Array("application/xhtml+xml"))
-    def mappings =
-    {
-        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-          <head></head>
-          <body>
-            <h2>Mappings</h2>
-            {
-              for(lang <- Server.config.languages; code = lang.wikiCode) yield
-              {
-                  <p><a href={code + "/"}>Mappings in {code}</a></p>
-              }
-            }
-          </body>
-        </html>
-    }
+    @GET @Path("statistics/") @Produces(Array("application/xhtml+xml"))
+    def statistics = list("DBpedia Mapping Statistics", "Statistics", "Mapping Statistics for")
+
+    @GET @Path("mappings/") @Produces(Array("application/xhtml+xml"))
+    def mappings = list("DBpedia Template Mappings", "Mappings", "Mappings for")
     
-    @GET
-    @Path("extraction/")
-    @Produces(Array("application/xhtml+xml"))
-    def extraction =
+    @GET @Path("extraction/") @Produces(Array("application/xhtml+xml"))
+    def extraction = list("DBpedia Test Extractor", "Extractors", "Extractor for")
+    
+    private def list(title : String, header : String, prefix : String) =
     {
         <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-          <head></head>
+          <head><title>DBpedia Test Extractor</title></head>
           <body>
             <h2>Extractors</h2>
             {
