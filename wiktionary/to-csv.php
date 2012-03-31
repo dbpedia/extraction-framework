ed868617c5481ad25c1d04e2d39ff480fe740584
Jonas Brekle <jonas.brekle@gmail.com>
2012-03-31 14:56:26 +0200

    to-csv a bit more robust

--- wiktionary/to-csv.php
+++ wiktionary/to-csv.php
@@ -4,7 +4,7 @@ $query = "
 PREFIX terms:<http://wiktionary.dbpedia.org/terms/>
 PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
 PREFIX dc:<http://purl.org/dc/elements/1.1/>
-SELECT ?sword ?slang ?spos ?ssense ?tword ?tlang
+SELECT ?sword ?slang ?spos ?ssense ?twordRes ?tword ?tlang
 FROM <http://wiktionary.dbpedia.org/>
 WHERE {
     ?swordRes terms:hasTranslation ?twordRes .
@@ -12,14 +12,19 @@ WHERE {
     ?swordRes dc:language ?slang .
     ?swordRes terms:hasPoS ?spos .
     OPTIONAL { ?swordRes terms:hasMeaning ?ssense . }
-    ?twordBaseRes terms:hasLangUsage ?twordRes . 
-    ?twordRes dc:language ?tlang .
-    ?twordBaseRes rdfs:label ?tword .
+    OPTIONAL { 
+           ?twordBaseRes terms:hasLangUsage ?twordRes . 
+           ?twordBaseRes rdfs:label ?tword .
+    }
+    OPTIONAL { ?twordRes dc:language ?tlang . }
 }
 ";
 
 function stripNS($s){
   return substr($s, 36);
+}
+function startWithUpper($str){
+  return $str{0} === strtoupper($str{0});
 }
 
 $conn   = @odbc_connect('VOS', 'dba', 'dba');
@@ -35,7 +40,33 @@ if($conn){
             echo "error";
         } else {
             while ($row = odbc_fetch_array($result)){
-                $line = '"'.addslashes(trim($row['sword'])).'","'.addslashes(stripNS($row['slang'])).'","'.addslashes(stripNS($row['spos'])).'","'.addslashes(trim($row['ssense'])).'","'.addslashes(trim($row['tword'])).'","'.addslashes(stripNS($row['tlang'])).'"'.PHP_EOL; 
+                if(empty($row['tword']) || empty($row['tlang']) ){
+                    $twordRes = $row['twordRes'];
+                    $tail = array_pop(explode('/', $twordRes)); 
+                    $parts = explode('-', $tail);
+                    $numParts = count($parts);
+                    if($numParts < 2) continue;
+                    $firstUpper = 1;
+
+                    while(!startWithUpper($parts[$firstUpper]) && $firstUpper < ($numParts-1)){
+                        $firstUpper++;
+                    }
+                    
+                    if(empty($row['tword'])){
+                        $tword = implode('-', array_slice($parts, 0, $firstUpper));
+                    } else {
+                        $tword = $row['tword'];
+                    }
+                    if(empty($row['tlang'])){
+                        $tlang = implode('-', array_slice($parts, $firstUpper, count($parts) - $firstUpper));
+                    } else {
+                        $tlang = $row['tlang'];
+                    }
+                } else {
+                    $tword = $row['tword'];
+                    $tlang = $row['tword'];
+                }
+                $line = '"'.addslashes(trim($row['sword'])).'","'.addslashes(stripNS($row['slang'])).'","'.addslashes(stripNS($row['spos'])).'","'.addslashes(trim($row['ssense'])).'","'.addslashes(trim($tword)).'","'.addslashes(trim($tlang)).'"'.PHP_EOL; 
                 fwrite($f, $line);
                 $ok = true;
             }
