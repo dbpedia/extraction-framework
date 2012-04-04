7162647abaf6afd74f3b45b9d9ed89822175e482
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-04-04 10:43:17 +0200

    download redirect and namespace settings from wikipedia

---
+++ core/src/test/scala/org/dbpedia/extraction/wikiparser/impl/wikipedia/GenerateWikiConfig.scala
@@ -0,0 +1,114 @@
+package org.dbpedia.extraction.wikiparser.impl.wikipedia
+
+import scala.io.{Source, Codec}
+import javax.xml.stream.XMLInputFactory
+import scala.collection.mutable
+import org.dbpedia.extraction.util.WikiConfigDownloader
+import java.io.{IOException, OutputStreamWriter, FileOutputStream, Writer}
+
+object GenerateWikiConfig {
+  
+  def main(args: Array[String]) : Unit = {
+    
+    val errors = mutable.LinkedHashMap[String, String]()
+    
+    val namespaceMap = mutable.LinkedHashMap[String, mutable.Map[String, Int]]()
+    
+    val aliasMap = mutable.LinkedHashMap[String, mutable.Map[String, Int]]()
+    
+    val redirectMap = mutable.LinkedHashMap[String, mutable.Set[String]]()
+    
+    for (language <- getLanguages) {
+      print(language)
+      try
+      {
+        val (namespaces, aliases, redirects) = new WikiConfigDownloader(language).download()
+        namespaceMap.put(language, namespaces)
+        aliasMap.put(language, aliases)
+        redirectMap.put(language, redirects)
+        println(" - OK")
+      } catch {
+        case ioex : IOException => {
+          errors.put(language, ioex.getMessage)
+          println(" - "+ioex.getMessage)
+        }
+      }
+    }
+    
+    val Insert = """// @ insert (\w+) here @ //""".r
+  
+    val nsSrc = Source.fromFile("src/test/resources/org/dbpedia/extraction/wikiparser/impl/wikipedia/Namespaces.scala.txt")(Codec.UTF8)
+    try {
+      val nsDst = new OutputStreamWriter(new FileOutputStream("src/main/scala/org/dbpedia/extraction/wikiparser/impl/wikipedia/Namespaces.scala"), "UTF-8")
+      try
+      {
+        for (line <- nsSrc.getLines) line match {
+          case Insert("namespaces") => insertNamespaces(nsDst, namespaceMap)
+          case Insert("aliases") => insertNamespaces(nsDst, aliasMap)
+          case Insert("errors") => insertErrors(nsDst, errors)
+          case Insert(_) => throw new Exception(line)
+          case _ => nsDst.write(line); nsDst.write('\n')
+        }
+      } finally nsDst.close
+    } 
+    finally nsSrc.close
+    
+    val reSrc = Source.fromFile("src/test/resources/org/dbpedia/extraction/wikiparser/impl/wikipedia/Redirect.scala.txt")(Codec.UTF8)
+    try {
+      val reDst = new OutputStreamWriter(new FileOutputStream("src/main/scala/org/dbpedia/extraction/wikiparser/impl/wikipedia/Redirect.scala"), "UTF-8")
+      try
+      {
+        for (line <- reSrc.getLines) line match {
+          case Insert("redirects") => insertRedirects(reDst, redirectMap)
+          case Insert(_) => throw new Exception(line)
+          case _ => reDst.write(line); reDst.write('\n')
+        }
+      } finally reDst.close
+    } 
+    finally reSrc.close
+  }
+  
+  def getLanguages() : List[String] = {
+    val source = Source.fromURL("http://noc.wikimedia.org/conf/langlist")(Codec.ISO8859)
+    try source.getLines.toList finally source.close
+  }
+
+  private def insertNamespaces(dst : Writer, map : mutable.Map[String, mutable.Map[String, Int]] ) : Unit = {
+    var firstLang = true
+    for ((language, namespaces) <- map) {
+      if (firstLang) firstLang = false else dst.write(",\n") 
+      dst.write("        \""); dst.write(language); dst.write("\" -> Map(\n")
+      var firstNS = true
+      for ((name, code) <- namespaces) {
+        if (firstNS) firstNS = false else dst.write(",\n")
+        if (name.isEmpty) dst.write("//")
+        dst.write("            \""); dst.write(name); dst.write("\" -> "); dst.write(code.toString);
+      }
+      dst.write("\n        )")
+    }
+    dst.write("\n")
+  }
+  
+  private def insertErrors(dst : Writer, map : mutable.Map[String, String]) : Unit = {
+    for ((language, message) <- map) {
+      dst.write("// "); dst.write(language); dst.write(" - "); dst.write(message); dst.write("\n")
+    }
+  }
+  
+  private def insertRedirects(dst : Writer, map : mutable.Map[String, mutable.Set[String]] ) : Unit = {
+    var firstLang = true
+    for ((language, redirects) <- map) {
+      if (firstLang) firstLang = false else dst.write(",\n") 
+      dst.write("        \""); dst.write(language); dst.write("\" -> Set(")
+      var firstRe = true
+      for (name : String <- redirects) {
+        if (firstRe) firstRe = false else dst.write(",")
+        dst.write("\""); dst.write(name); dst.write("\"");
+      }
+      dst.write(")")
+    }
+    dst.write("\n")
+  }
+  
+}
+
