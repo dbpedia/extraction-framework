52789849adaecee283dd7076d0bc7520ec5aaf8e
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-04-17 23:24:13 +0200

    comment

--- core/src/main/scala/org/dbpedia/extraction/ontology/OntologyClass.scala
+++ core/src/main/scala/org/dbpedia/extraction/ontology/OntologyClass.scala
@@ -9,11 +9,11 @@ import org.dbpedia.extraction.util.Language
  * @param labels The labels of this type. Map: LanguageCode -> Label
  * @param comments Comments describing this type. Map: LanguageCode -> Comment
  * @param subClassOf The super class of this class. May be null for owl:Thing or classes in 
- * namespaces that we don't validate. See OntologyNamespaces.skipValidation()
+ * namespaces that we don't validate. See OntologyNamespaces.skipValidation() TODO: use type ListSet, not List?
  * @param equivalentClasses
  */
 class OntologyClass(name : String, labels : Map[Language, String], comments : Map[Language, String],
-                    val subClassOf : List[OntologyClass], val equivalentClasses : Set[OntologyClass]) extends OntologyType(name, labels, comments)
+                    val subClassOf : List/*TODO:ListSet?*/[OntologyClass], val equivalentClasses : Set[OntologyClass]) extends OntologyType(name, labels, comments)
 {
     require(name != null, "name != null")
     require(labels != null, "labels != null")
