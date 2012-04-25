fd81e741a7aff944a696cab39ea67beaafab595b
Pablo Mendes <pablomendes@gmail.com>
2012-04-25 14:32:17 +0200

    new version of mapping sprint call for action page

--- server/web/sprint/percentage.php
+++ server/web/sprint/percentage.php
@@ -1,10 +1,6 @@
 <?php
-$contents = file_get_contents("http://mappings.dbpedia.org/server/p");
-$start = stripos($contents,"<body>");
-$end = stripos($contents,"</body>");
-$p = trim(substr($contents,$start+7,$end-$start-7));
-
-echo '{ "percentage": "'.$p.'" }';
-
+$contents = file_get_contents("http://mappings.dbpedia.org/sprint/data/en.stats.json");
+$p = json_decode($contents,true);
+echo '{ "percentage": "'.$p["template_occurrences"].'" }';
 ?>
 
