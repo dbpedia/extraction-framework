f753e1f455d1ca00d02e6eb415ec65a2ee686306
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-04-28 02:23:59 +0200

    add bg and a few other languages

--- server/src/main/web/sprint/config.php
+++ server/src/main/web/sprint/config.php
@@ -1,26 +1,4 @@
 <?php
-/*
-Mappings (bn)
-Mappings (ca)
-Mappings (cs)
-Mappings (de)
-Mappings (el)
-Mappings (en)
-Mappings (es)
-Mappings (fr)
-Mappings (ga)
-Mappings (hi)
-Mappings (hr)
-Mappings (hu)
-Mappings (it)
-Mappings (ko)
-Mappings (nl)
-Mappings (pl)
-Mappings (pt)
-Mappings (ru)
-Mappings (sl)
-Mappings (tr)
-*/
-$languages = array("bn","ca","cs","de","el","en","es","fr","ga","hi","hr","hu","it","ko","nl","pl","pt","ru","sl","tr");
-
+// TODO: don't hard-code all the languages here, get them from http://mappings.dbpedia.org/server/statistics/ or some other place 
+$languages = array("ar","bg","bn","ca","cs","de","el","en","es","eu","fr","ga","hi","hr","hu","it","ko","nl","pl","pt","ru","sl","tr");
 ?>
