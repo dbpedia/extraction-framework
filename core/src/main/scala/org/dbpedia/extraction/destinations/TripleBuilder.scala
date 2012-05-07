f35cbd0ece5f967f05a89547041f26c35bf7993e
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-07 16:58:29 +0200

    the great formatter clean-up is done for now. use all twelve, let's see what happens.

---
+++ core/src/main/scala/org/dbpedia/extraction/destinations/TripleBuilder.scala
@@ -0,0 +1,35 @@
+package org.dbpedia.extraction.destinations
+
+import org.dbpedia.extraction.ontology.datatypes.Datatype
+import org.dbpedia.extraction.util.Language
+import java.net.{URI,URISyntaxException}
+
+abstract class TripleBuilder {
+  
+  def render(quad: Quad): String = {
+    
+    start(quad.context)
+    
+    uri(quad.subject)
+    
+    uri(quad.predicate)
+    
+    if (quad.datatype == null) uri(quad.value)
+    else if (quad.datatype.name == "xsd:string") string(quad.value, quad.language.isoCode)
+    else value(quad.value, quad.datatype.uri)
+    
+    end(quad.context)
+    
+    toString()
+  }
+    
+  def start(context: String): Unit
+  
+  def uri(uri: String): Unit
+  
+  def string(value: String, isoLang: String): Unit
+  
+  def value(value: String, datatype: String): Unit
+  
+  def end(context: String): Unit
+}
