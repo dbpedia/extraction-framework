cc1364cdc4c13dda1b90d89e00d2bfcaecdf3a16
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-07 13:44:07 +0200

    the great formatter clean-up. who knows, one fine day, we might even get IRI/URI stuff right.

--- core/src/main/scala/org/dbpedia/extraction/destinations/TripleBuilder.scala
+++ core/src/main/scala/org/dbpedia/extraction/destinations/TripleBuilder.scala
@@ -1,92 +1,99 @@
 package org.dbpedia.extraction.destinations
 
 import org.dbpedia.extraction.ontology.datatypes.Datatype
-import org.dbpedia.extraction.ontology.OntologyProperty
 import org.dbpedia.extraction.util.Language
-import java.net.URI
+import java.net.{URI,URISyntaxException}
 
 /**
  * Helps to build one triple/quad line in Turtle/Turtle-Quads/N-Triples/N-Quads format.
  */
-class TripleBuilder(turtle: Boolean)
+class TripleBuilder(iri: Boolean, turtle: Boolean)
 {
-    // Scala's StringBuilder doesn't have appendCodePoint
-    private val sb = new java.lang.StringBuilder
-    
-    // FIXME Handle URI vs IRI stuff only in this method. Use IRIs in all other classes and methods.
-    def uri(uri: String): TripleBuilder = {
-      this append '<' escape uri append "> "
-      this
+  // Scala's StringBuilder doesn't have appendCodePoint
+  private val sb = new java.lang.StringBuilder
+  
+  def uri(str: String): Unit = {
+    val value = try {
+      val uri = new URI(str)
+      if (iri) uri.toString else uri.toASCIIString
+    } catch {
+      case usex: URISyntaxException =>
+        // this triple is unusable, comment it out
+        sb.insert(0, "# ")
+        "BAD URI: "+usex.getMessage() 
     }
     
-    def value(value: String, datatype: Datatype, language: Language): TripleBuilder = {
-      if (datatype == null) this uri value
-      else {
-        this append '"' escape value append '"' 
-        if (datatype.name == "xsd:string") this append '@' append language.isoCode append ' '
-        else this append "^^" uri datatype.uri
-      }
-      this
-    }
+    // Note: If the string contains ">", the line cannot be parsed. But it's broken anyway, so what.
+    // For Turtle, we could escape ">" as "\>", but for N-Triples, we can't.
+    this add '<' esc value add "> "
+  }
+  
+  /**
+   * @param datatype must not be null
+   */
+  def value(value: String, datatype: Datatype, language: Language): Unit = {
+    this add '"' esc value add '"' 
+    if (datatype.name == "xsd:string") this add '@' add language.isoCode add ' '
+    else this add "^^" uri datatype.uri
+  }
+  
+  def close(): Unit = {
+    // use UNIX EOL. N-Triples and Turtle don't care:
+    // http://www.w3.org/TR/rdf-testcases/#eoln and http://www.w3.org/TR/turtle/#term-turtle2-WS
+    // and it's probably better to be consistent instead of using the EOL of the platform
+    // where the file was generated. These files are moved around a lot anyway.
+    this add ".\n"
+  }
     
-    def close(): TripleBuilder = {
-      // use UNIX EOL. N-Triples and Turtle don't care:
-      // http://www.w3.org/TR/rdf-testcases/#eoln and http://www.w3.org/TR/turtle/#term-turtle2-WS
-      // and it's probably better to be consistent instead of using the EOL of the platform
-      // where the file was generated. These files are moved around a lot anyway.
-      sb append ".\n"
-      this
-    }
-      
-    override def toString: String = {
-      sb toString
-    }
+  override def toString: String = {
+    sb toString
+  }
+  
+  private def add(s: String): TripleBuilder = { 
+    sb append s
+    this 
+  }
+  
+  private def add(c: Char): TripleBuilder = { 
+    sb append c
+    this 
+  }
+  
+  /**
+   * Escapes a Unicode string according to N-Triples / Turtle format.
+   */
+  private def esc(input: String): TripleBuilder =
+  {
+    val length = input.length
     
-    private def append(s: String) = { 
-      sb append s
-      this 
-    }
-    
-    private def append(c: Char) = { 
-      sb append c
-      this 
-    }
-    
-    /**
-     * Escapes a Unicode string according to N-Triples / Turtle format.
-     */
-    private def escape(input: String): TripleBuilder =
+    var offset = 0
+    while (offset < length)
     {
-      val length = input.length
-      
-      var offset = 0
-      while (offset < length)
-      {
-        val c = input.codePointAt(offset)
-        offset += Character.charCount(c)
+      val c = input.codePointAt(offset)
+      offset += Character.charCount(c)
 
-        // TODO: use a lookup table for c < 0x80
-             if (c == '\\') sb append "\\\\"
-        else if (c == '\"') sb append "\\\""
-        else if (c == '\n') sb append "\\n"
-        else if (c == '\r') sb append "\\r";
-        else if (c == '\t') sb append "\\t"
-        else if (c >= 0x0020 && c < 0x007F) sb append c.toChar
-        else if (turtle && c >= 0x00A0 && c <= 0xFFFF) sb append c.toChar
-        else if (turtle && c >= 0x10000) sb appendCodePoint c
-        else this appendHex c
-      }
-      
-      this
+      // TODO: use a lookup table for c < 0x80
+           if (c == '\\') sb append "\\\\"
+      else if (c == '\"') sb append "\\\""
+      else if (c == '\n') sb append "\\n"
+      else if (c == '\r') sb append "\\r";
+      else if (c == '\t') sb append "\\t"
+      else if (c >= 0x0020 && c < 0x007F) sb append c.toChar
+      else if (turtle && c >= 0x00A0 && c <= 0xFFFF) sb append c.toChar
+      else if (turtle && c >= 0x10000) sb appendCodePoint c
+      else this appendHex c
     }
     
-    private def appendHex(c: Int) {
-      // TODO: use something faster than c.toHexString.toUpperCase and "0" *.
-      // Append chars to the string buffer directly. 
-      val hex = c.toHexString.toUpperCase
-      if (c <= 0xFFFF) sb append "\\u" append "0" * (4 - hex.length)
-      else sb append "\\U" append "0" * (8 - hex.length)
-      sb append hex
-    }
-    
+    this
+  }
+  
+  private def appendHex(c: Int) {
+    // TODO: use something faster than c.toHexString.toUpperCase and "0" *.
+    // Append chars to the string buffer directly. 
+    val hex = c.toHexString.toUpperCase
+    if (c <= 0xFFFF) sb append "\\u" append "0" * (4 - hex.length)
+    else sb append "\\U" append "0" * (8 - hex.length)
+    sb append hex
+  }
+  
 }
--- core/src/main/scala/org/dbpedia/extraction/destinations/formatters/TextFormatter.scala
+++ core/src/main/scala/org/dbpedia/extraction/destinations/formatters/TextFormatter.scala
@@ -2,12 +2,43 @@ package org.dbpedia.extraction.destinations.formatters
 
 import org.dbpedia.extraction.destinations.Formatter
 import java.io.Writer
+import org.dbpedia.extraction.destinations.Quad
+import org.dbpedia.extraction.destinations.TripleBuilder
 
 /**
  * TODO: rename this class.
  * TODO: add functionality - the comments may contain more useful info
  */
-trait TextFormatter extends Formatter {
-    override def writeHeader(writer : Writer) : Unit = { writer.write("# started\n") }
-    override def writeFooter(writer : Writer) : Unit = { writer.write("# complete\n") }
+final class TextFormatter(iri: Boolean, turtle: Boolean, quads: Boolean) extends Formatter {
+  
+    override def writeHeader(writer : Writer) = {
+      
+      writer.write("# started\n")
+    }
+    
+    override def writeFooter(writer : Writer) = {
+      
+      writer.write("# complete\n")
+    }
+    
+    override def write(quad : Quad, writer : Writer) = {
+      
+      val triple = new TripleBuilder(iri, turtle)
+      
+      triple.uri(quad.subject)
+      
+      triple.uri(quad.predicate)
+      
+      if (quad.datatype == null) triple.uri(quad.value) 
+      else triple.value(quad.value, quad.datatype, quad.language)
+      
+      if (quads) triple.uri(quad.context)
+      
+      triple.close
+      
+      writer.write(triple.toString)
+    }
+}
+
+object TextFormatter {
 }
