7d74f8953713226195999a900480ae9da5cd6336
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-09 21:21:42 +0200

    added "ja"

--- server/src/main/web/sprint/config.php
+++ server/src/main/web/sprint/config.php
@@ -1,4 +1,4 @@
 <?php
 // TODO: don't hard-code all the languages here, get them from http://mappings.dbpedia.org/server/statistics/ or some other place 
-$languages = array("ar","bg","bn","ca","cs","de","el","en","es","eu","fr","ga","hi","hr","hu","it","ko","nl","pl","pt","ru","sl","tr");
+$languages = array("ar","bg","bn","ca","cs","de","el","en","es","eu","fr","ga","hi","hr","hu","it","ja","ko","nl","pl","pt","ru","sl","tr");
 ?>
