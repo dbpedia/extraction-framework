dd8728ead1649e460fba217aa5b8e7c99741fbad
Pablo Mendes <pablomendes@gmail.com>
2012-05-10 12:14:47 +0200

    list of languages for sprint is scraped from server now, no need to hardcode.

--- server/src/main/web/sprint/config.php
+++ server/src/main/web/sprint/config.php
@@ -1,4 +1,25 @@
 <?php
-// TODO: don't hard-code all the languages here, get them from http://mappings.dbpedia.org/server/statistics/ or some other place 
-$languages = array("ar","bg","bn","ca","cs","de","el","en","es","eu","fr","ga","hi","hr","hu","it","ja","ko","nl","pl","pt","ru","sl","tr");
+//$languages = array("ar","bg","bn","ca","cs","de","el","en","es","eu","fr","ga","hi","hr","hu","it","ja","ko","nl","pl","pt","ru","sl","tr");
+require 'scraperwiki/scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+
+$languages = getLangs();
+
+function getLangs() {
+  $url = "http://mappings.dbpedia.org/server/statistics/";
+
+  $html = scraperwiki::scrape($url);
+
+  $dom = new simple_html_dom();
+  $dom->load($html);
+
+  $i = 0;
+  $langs = array();
+  foreach($dom->find('/html/body/p/a') as $result) {
+        $lang = str_replace("/","",trim($result->href));
+        $langs[] = $lang;
+  }
+  return $langs;
+}
+
 ?>
