9bc73de22c24b94b81a8fa8d31527b097ff3ca51
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-16 00:00:46 +0200

    comment

--- core/src/main/scala/org/dbpedia/extraction/sources/Source.scala
+++ core/src/main/scala/org/dbpedia/extraction/sources/Source.scala
@@ -4,9 +4,10 @@ import scala.collection.immutable.Traversable
 
 /**
  * A source of wiki pages.
- * FIXME: get rid of this class. It has no purpose and makes APIs that use it less flexible. 
- * And: When you get rid of it, replace it by an Iterator, not a Traversable. We don't want
- * to keep its elements around any longer than we absolutely have to.
+ * FIXME: get rid of this class. It has no purpose except hasDefiniteSize = false and makes APIs 
+ * that use it less flexible. When we get rid of it, we should replace it by an Iterator or
+ * a Stream, not a Traversable. We don't want to keep its elements around any longer than we 
+ * absolutely have to.
  */
 trait Source extends Traversable[WikiPage]
 {
