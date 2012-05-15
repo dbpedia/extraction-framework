7f3d7102f94e6149ca54368ba6c875e1f90333de
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-16 01:50:58 +0200

    comment

--- core/src/main/scala/org/dbpedia/extraction/mappings/PropertyMapping.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/PropertyMapping.scala
@@ -3,6 +3,7 @@ package org.dbpedia.extraction.mappings
 import org.dbpedia.extraction.wikiparser.TemplateNode
 
 /**
- * Marker trait for mappings which map one or more properties of a specific class
+ * Marker trait for mappings which map one or more properties of a specific class.
+ * Necessary to make PropertyMappings distinguishable from other Mapping[TemplateNode] types.
  */
 trait PropertyMapping extends Mapping[TemplateNode]
