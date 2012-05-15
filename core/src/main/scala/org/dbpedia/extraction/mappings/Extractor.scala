a069f21a2d1e7d0e8ddbc0bfc39613a001f49a7b
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-16 01:50:58 +0200

    comment

--- core/src/main/scala/org/dbpedia/extraction/mappings/Extractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/Extractor.scala
@@ -5,5 +5,7 @@ import org.dbpedia.extraction.wikiparser._
 
 /**
  * Extractors are mappings that extract data from a PageNode.
+ * Necessary to get some type safety in CompositeExtractor: 
+ * Class[_ <: Extractor] can be checked at runtime, but Class[_ <: Mapping[PageNode]] can not.
  */
 trait Extractor extends Mapping[PageNode]
