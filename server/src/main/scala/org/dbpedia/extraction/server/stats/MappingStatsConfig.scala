7a75517d82d4a635f33f776555b2ed2212022da0
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-20 21:15:54 +0200

    removed unused import

--- server/src/main/scala/org/dbpedia/extraction/server/stats/MappingStatsConfig.scala
+++ server/src/main/scala/org/dbpedia/extraction/server/stats/MappingStatsConfig.scala
@@ -3,7 +3,6 @@ package org.dbpedia.extraction.server.stats
 import java.io.File
 
 import org.dbpedia.extraction.util.Language
-import org.dbpedia.extraction.wikiparser.impl.wikipedia.Namespaces
 import org.dbpedia.extraction.wikiparser.Namespace
 
 class MappingStatsConfig(statsDir : File, language: Language)
