85148ab15e06ba9b8d597ec30dbae2d16ca353ee
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-21 23:53:19 +0200

    fixed alphabetical order

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/DisambiguationExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/DisambiguationExtractorConfig.scala
@@ -10,12 +10,12 @@ object DisambiguationExtractorConfig
          "en" -> " (disambiguation)",
          "es" -> " (desambiguación)",
          "eu" -> " (argipena)",
+         "fr" -> " (homonymie)",
          "it" -> " (disambigua)",
          "ko" -> " (동음이의)",
          "pl" -> " (ujednoznacznienie)",
          "pt" -> " (desambiguação)",
-         "ru" -> " (значения)",
-         "fr" -> " (homonymie)"
+         "ru" -> " (значения)"
     )
 
     val supportedLanguages = disambiguationTitlePartMap.keySet
