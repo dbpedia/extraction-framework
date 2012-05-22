c07b55722ed6fdfaa3603e525cc6954853b8c312
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-22 22:00:47 +0200

    untangled ConfigLoader.scala a bit. Still much to do.

--- dump/src/main/scala/org/dbpedia/extraction/dump/extract/ConfigLoader.scala
+++ dump/src/main/scala/org/dbpedia/extraction/dump/extract/ConfigLoader.scala
@@ -1,25 +1,17 @@
 package org.dbpedia.extraction.dump.extract
 
-import org.dbpedia.extraction.destinations.formatters.{Formatter,TerseFormatter,TriXFormatter}
-import org.dbpedia.extraction.destinations.formatters.UriPolicy._
-import org.dbpedia.extraction.destinations.{Destination,FileDestination,CompositeDestination}
-import org.dbpedia.extraction.mappings._
-import scala.collection.immutable.ListMap
-import scala.collection.mutable.{HashMap,ArrayBuffer,HashSet}
+import org.dbpedia.extraction.destinations.{Dataset,Destination,FileDestination,CompositeDestination,MarkerDestination}
+import org.dbpedia.extraction.mappings.{Mappings,MappingsLoader,Redirects,RootExtractor,CompositeExtractor}
+import scala.collection.mutable.ArrayBuffer
 import java.util.Properties
 import java.io.{FileInputStream, InputStreamReader, File}
-import org.dbpedia.extraction.util.StringUtils._
 import org.dbpedia.extraction.util.{Language,Finder}
 import org.dbpedia.extraction.util.RichFile.toRichFile
-import java.net.{URI,URL}
+import java.net.URL
 import org.dbpedia.extraction.ontology.io.OntologyReader
-import org.dbpedia.extraction.ontology.Ontology
-import org.dbpedia.extraction.sources.{MemorySource, Source, XMLSource, WikiSource}
-import org.dbpedia.extraction.wikiparser._
-import org.dbpedia.extraction.destinations.Dataset
+import org.dbpedia.extraction.sources.{XMLSource,WikiSource}
+import org.dbpedia.extraction.wikiparser.{Namespace,PageNode,WikiParser}
 import org.dbpedia.extraction.dump.download.Download
-import scala.collection.JavaConversions.asScalaSet // implicit
-import org.dbpedia.extraction.destinations.MarkerDestination
 
 /**
  * Loads the dump extraction configuration.
@@ -29,7 +21,7 @@ import org.dbpedia.extraction.destinations.MarkerDestination
  */
 object ConfigLoader
 {
-    private var config : Config = null
+    private var config: Config = null
 
     /**
      * Loads the configuration and creates extraction jobs for all configured languages.
@@ -51,203 +43,6 @@ object ConfigLoader
         config.extractorClasses.keySet.view.map(createExtractionJob)
     }
     
-    private var ontologyFile : File = null
-
-    private var mappingsDir : File = null
-    
-    private var formats: List[String] = null
-    
-    private var requireComplete = false
-
-    private class Config(config : Properties)
-    {
-        // TODO: rewrite this, similar to download stuff:
-        // - Don't use java.util.Properties, allow multiple values for one key
-        // - Resolve config file names and load them as well
-        // - Use pattern matching to parse arguments
-        // - allow multiple config files, given on command line
-      
-        /** Dump directory */
-        val dumpDir = getFile("dir")
-        if (dumpDir == null) throw error("property 'dir' not defined.")
-        if (! dumpDir.exists) throw error("dir "+dumpDir+" does not exist")
-        
-        if(config.getProperty("require-download-complete") != null)
-          requireComplete = config.getProperty("require-download-complete").toBoolean
-
-        /** Local ontology file, downloaded for speed and reproducibility */
-        ontologyFile = getFile("ontology")
-
-        /** Local mappings files, downloaded for speed and reproducibility */
-        mappingsDir = getFile("mappings")
-        
-        /**
-         * Order is important here: First convert IRI to URI, then append '_' if necessary,
-         * then convert specific domain to generic domain. The second step must happen 
-         * after URI conversion (because a URI may need an underscore where a IRI doesn't), 
-         * and before the third step (because we need the specific domain to decide which 
-         * URIs should be made xml-safe).
-         * 
-         * In each tuple, the key is the policy name. The value is a policy-factory that
-         * takes a predicate. The predicate decides for which DBpedia URIs a policy
-         * should be applied. We pass a predicate to the policy-factory to get the policy.
-         */
-        private val policyFactories = Seq[(String, Predicate => Policy)] (
-          "uris" -> uris,
-          "xml-safe" -> xmlSafe,
-          "generic" -> generic
-        )
-
-        /**
-         * Parses a list of languages like "en,fr" or "*" or even "en,*,fr"
-         */
-        private def parsePredicate(languages: String): Predicate = {
-          
-          val codes = split(languages, ',').toSet
-          val domains = codes.map { code => if (code == "*") "*" else Language(code).dbpediaDomain }
-          
-          if (domains("*")) { uri => uri.getHost.equals("dbpedia.org") || uri.getHost.endsWith(".dbpedia.org") }
-          else { uri => domains(uri.getHost) }
-        }
-        
-        /**
-         * Parses a policy line like "uri-policy.main=uris:en,fr; generic:en"
-         */
-        private def parsePolicy(key: String): Policy = {
-          
-          val predicates = new HashMap[String, Predicate]()
-          
-          // parse all predicates
-          for (policy <- splitValue(key, ';')) {
-            split(policy, ':') match {
-              case List(key, languages) => {
-                require(! predicates.contains(key), "duplicate policy '"+key+"'")
-                predicates(key) = parsePredicate(languages)
-              }
-              case _ => throw error("invalid format: '"+policy+"'")
-            }
-          }
-          
-          require(predicates.nonEmpty, "found no URI policies")
-          
-          val policies = new ArrayBuffer[Policy]()
-          
-          // go through known policies in correct order, get predicate, and create policy
-          for ((key, factory) <- policyFactories; predicate <- predicates.remove(key)) {
-            policies += factory(predicate)
-          }
-          
-          require(predicates.isEmpty, "unknown URI policies "+predicates.keys.mkString("'","','","'"))
-          
-          // The resulting policy is the concatenation of all policies.
-          (iri, pos) => {
-            var result = iri
-            for (policy <- policies) result = policy(result, pos)
-            result
-          }
-        }
-        
-        // parse all URI policy lines
-        val policies = new HashMap[String, Policy]()
-        for (key <- config.stringPropertyNames) {
-          if (key.startsWith("uri-policy")) {
-            try policies(key) = parsePolicy(key)
-            catch { case e: Exception => throw error("invalid URI policy: '"+key+"="+config.getProperty(key)+"'", e) }
-          }
-        }
-        
-        private val formatters = Map[String, Policy => Formatter] (
-          "trix-triples" -> { new TriXFormatter(false, _) },
-          "trix-quads" -> { new TriXFormatter(true, _) },
-          "turtle-triples" -> { new TerseFormatter(false, true, _) },
-          "turtle-quads" -> { new TerseFormatter(true, true, _) },
-          "n-triples" -> { new TerseFormatter(false, false, _) },
-          "n-quads" -> { new TerseFormatter(true, false, _) }
-        )
-
-        // parse all format lines
-        val formats = new HashMap[String, Formatter]()
-        for (key <- config.stringPropertyNames) {
-          if (key.startsWith("format.")) {
-            
-            val suffix = key.substring("format.".length)
-            
-            val settings = splitValue(key, ';')
-            require(settings.length == 1 || settings.length == 2, "key '"+key+"' must have one or two values separated by ';' - file format and optional uri policy")
-            
-            val formatter = formatters.getOrElse(settings(0), throw error("first value for key '"+key+"' is '"+settings(0)+"' but must be one of "+formatters.keys.toSeq.sorted.mkString("'","','","'")))
-            
-            val policy =
-              if (settings.length == 1) identity
-              else policies.getOrElse(settings(1), throw error("second value for key '"+key+"' is '"+settings(1)+"' but must be a configured uri-policy, i.e. one of "+policies.keys.mkString("'","','","'")))
-            
-            formats(suffix) = formatter.apply(policy)
-          }
-        }
-
-        /** Languages */
-        // TODO: add special parameters, similar to download:
-        // extract=10000-:InfoboxExtractor,PageIdExtractor means all languages with at least 10000 articles
-        // extract=mapped:MappingExtractor means all languages with a mapping namespace
-        var languages = splitValue("languages", ',').map(Language)
-        if (languages.isEmpty) languages = Namespace.mappings.keySet.toList
-        languages = languages.sorted(Language.wikiCodeOrdering)
-
-        val extractorClasses = loadExtractorClasses()
-        
-        private def getFile(key: String): File = {
-          val value = config.getProperty(key)
-          if (value == null) null else new File(value)
-        }
-        
-        private def splitValue(key: String, sep: Char): List[String] = {
-          val values = config.getProperty(key)
-          if (values == null) List.empty
-          else split(values, sep)
-        }
-
-        private def split(value: String, sep: Char): List[String] = {
-          value.split("["+sep+"\\s]+", -1).map(_.trim).filter(_.nonEmpty).toList
-        }
-
-        /**
-         * Loads the extractors classes from the configuration.
-         *
-         * @return A Map which contains the extractor classes for each language
-         */
-        private def loadExtractorClasses() : Map[Language, List[Class[_ <: Extractor]]] =
-        {
-            //Load extractor classes
-            if(config.getProperty("extractors") == null) throw error("Property 'extractors' not defined.")
-            val stdExtractors = splitValue("extractors", ',').map(loadExtractorClass)
-
-            //Create extractor map
-            var extractors = ListMap[Language, List[Class[_ <: Extractor]]]()
-            for(language <- languages) extractors += ((language, stdExtractors))
-
-            //Load language specific extractors
-            val LanguageExtractor = """extractors\.(.*)""".r
-
-            for(LanguageExtractor(code) <- config.stringPropertyNames.toArray)
-            {
-                val language = Language(code)
-                if (extractors.contains(language))
-                {
-                    extractors += language -> (stdExtractors ::: splitValue("extractors."+code, ',').map(loadExtractorClass))
-                }
-            }
-
-            extractors
-        }
-
-        private def loadExtractorClass(name: String): Class[_ <: Extractor] = {
-          val className = if (! name.contains(".")) classOf[Extractor].getPackage.getName+'.'+name else name
-          // TODO: class loader of Extractor.class is probably wrong for some users.
-          classOf[Extractor].getClassLoader.loadClass(className).asSubclass(classOf[Extractor])
-        }
-    }
-
-
     private val parser = WikiParser()
     
     /**
@@ -262,19 +57,19 @@ object ConfigLoader
         //Extraction Context
         val context = new DumpExtractionContext
         {
-            def ontology : Ontology = _ontology
+            def ontology = _ontology
     
-            def commonsSource : Source = _commonsSource
+            def commonsSource = _commonsSource
     
-            def language : Language = lang
+            def language = lang
     
             private lazy val _mappingPageSource =
             {
                 val namespace = Namespace.mappings(language)
                 
-                if (mappingsDir != null && mappingsDir.isDirectory)
+                if (config.mappingsDir != null && config.mappingsDir.isDirectory)
                 {
-                    val file = new File(mappingsDir, namespace.getName(Language.Mappings).replace(' ','_')+".xml")
+                    val file = new File(config.mappingsDir, namespace.getName(Language.Mappings).replace(' ','_')+".xml")
                     XMLSource.fromFile(file, Language.Mappings).map(parser)
                 }
                 else
@@ -300,7 +95,7 @@ object ConfigLoader
                              title.namespace == Namespace.Category || title.namespace == Namespace.Template)
             }
             
-            def articlesSource : Source = _articlesSource
+            def articlesSource = _articlesSource
     
             private val _redirects =
             {
@@ -337,9 +132,9 @@ object ConfigLoader
     //language-independent val
     private lazy val _ontology =
     {
-        val ontologySource = if (ontologyFile != null && ontologyFile.isFile) 
+        val ontologySource = if (config.ontologyFile != null && config.ontologyFile.isFile)
         {
-          XMLSource.fromFile(ontologyFile, Language.Mappings)
+          XMLSource.fromFile(config.ontologyFile, Language.Mappings)
         } 
         else 
         {
@@ -362,14 +157,10 @@ object ConfigLoader
     }
     
     private def latestDate(finder: Finder[_]): String = {
-      val fileName = if (requireComplete) Download.Complete else "pages-articles.xml"
+      val fileName = if (config.requireComplete) Download.Complete else "pages-articles.xml"
       val dates = finder.dates(fileName)
-      if (dates.isEmpty) throw error("found no directory with file '"+finder.wikiName+"-[YYYYMMDD]-"+fileName+"'")
+      if (dates.isEmpty) throw new IllegalArgumentException("found no directory with file '"+finder.wikiName+"-[YYYYMMDD]-"+fileName+"'")
       dates.last
-    }
-    
-    private def error(message: String, cause: Throwable = null): IllegalArgumentException = {
-      new IllegalArgumentException(message, cause)
     }
     
 }
