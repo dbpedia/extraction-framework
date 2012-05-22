43931a395379b2b424bf87ce0337c8922777b47c
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-22 09:29:27 +0200

    use title.pageIri, don't roll your own

--- core/src/main/scala/org/dbpedia/extraction/mappings/WikiPageExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/WikiPageExtractor.scala
@@ -14,21 +14,19 @@ class WikiPageExtractor( context : {
                             def language : Language } ) extends Extractor
 {
     // We used foaf:page here, but foaf:isPrimaryTopicOf is probably better.
-    private val foafPageProperty = context.ontology.properties("foaf:isPrimaryTopicOf")
-    private val foafPrimaryTopicProperty = context.ontology.properties("foaf:primaryTopic")
-    private val dcLanguageProperty = context.ontology.properties("dc:language")
+    private val isPrimaryTopicOf = context.ontology.properties("foaf:isPrimaryTopicOf")
+    private val primaryTopic = context.ontology.properties("foaf:primaryTopic")
+    private val dcLanguage = context.ontology.properties("dc:language")
 
-    override def extract(node : PageNode, subjectUri : String, pageContext : PageContext): Seq[Quad] =
+    override def extract(page : PageNode, subjectUri : String, pageContext : PageContext): Seq[Quad] =
     {
-        if(node.title.namespace != Namespace.Main) return Seq.empty
+        if(page.title.namespace != Namespace.Main) return Seq.empty
         
         val quads = new ArrayBuffer[Quad]()
 
-        val objectLink = "http://" + context.language.wikiCode + ".wikipedia.org/wiki/" + node.root.title.encoded
-
-        quads += new Quad(context.language, DBpediaDatasets.LinksToWikipediaArticle, subjectUri, foafPageProperty,  objectLink, node.sourceUri)
-        quads += new Quad(context.language, DBpediaDatasets.LinksToWikipediaArticle, objectLink, foafPrimaryTopicProperty, subjectUri, node.sourceUri)
-        quads += new Quad(context.language, DBpediaDatasets.LinksToWikipediaArticle, objectLink, dcLanguageProperty,  context.language.wikiCode, node.sourceUri)
+        quads += new Quad(context.language, DBpediaDatasets.LinksToWikipediaArticle, subjectUri, isPrimaryTopicOf,  page.title.pageIri, page.sourceUri)
+        quads += new Quad(context.language, DBpediaDatasets.LinksToWikipediaArticle, page.title.pageIri, primaryTopic, subjectUri, page.sourceUri)
+        quads += new Quad(context.language, DBpediaDatasets.LinksToWikipediaArticle, page.title.pageIri, dcLanguage, context.language.wikiCode, page.sourceUri)
 
         quads
     }
