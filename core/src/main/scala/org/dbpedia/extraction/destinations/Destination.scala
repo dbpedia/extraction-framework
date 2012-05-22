57fcdbc0cb326b64ebc5a6516dbd7bab66a91fd0
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-22 17:46:32 +0200

    don't implement close()

--- core/src/main/scala/org/dbpedia/extraction/destinations/Destination.scala
+++ core/src/main/scala/org/dbpedia/extraction/destinations/Destination.scala
@@ -1,7 +1,7 @@
 package org.dbpedia.extraction.destinations
 
 /**
- * Represents an abstraction over a destination of RDF statements.
+ * A destination for RDF quads.
  *
  * Implementing classes of this trait must override the write method.
  * Optionally, the close method can be overriden in order to finalize the destination.
@@ -9,13 +9,13 @@ package org.dbpedia.extraction.destinations
  */
 trait Destination
 {
-    /**
-     * Writes a new statement to this destination.
-     */
-    def write(graph : Seq[Quad]): Unit
+  /**
+   * Writes a new statement to this destination.
+   */
+  def write(graph : Seq[Quad]): Unit
 
-    /**
-     * Closes this destination.
-     */
-    def close() : Unit = {}
+  /**
+   * Closes this destination.
+   */
+  def close(): Unit
 }
