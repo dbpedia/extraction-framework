1fae85c8deed8aa1bb3b3132a3cc87d81e27272e
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-22 08:57:24 +0200

    merge(): find values more efficiently and simply; re-use expensive DatatypeFactory; implement hashCode and equals

--- core/src/main/scala/org/dbpedia/extraction/util/Date.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/Date.scala
@@ -4,10 +4,10 @@ import org.dbpedia.extraction.ontology.datatypes.Datatype
 import javax.xml.datatype.DatatypeFactory
 import org.dbpedia.extraction.ontology.RdfNamespace
 
-class Date (val year: Option[Int] = None, val month: Option[Int] = None, val day: Option[Int] = None, val datatype : Datatype)
-     extends Ordered[Date]
+class Date (val year: Option[Int] = None, val month: Option[Int] = None, val day: Option[Int] = None, val datatype: Datatype)
+extends Ordered[Date]
 {
-    private val calendar = DatatypeFactory.newInstance().newXMLGregorianCalendar()
+    private val calendar = Date.datatypeFactory.newXMLGregorianCalendar()
 
     require(year.isEmpty || year.get != 0, "year must not be 0")
     require(month.isEmpty || month.get != 0, "month must not be 0")
@@ -57,20 +57,30 @@ class Date (val year: Option[Int] = None, val month: Option[Int] = None, val day
     // FIXME: this "xsd:" thing is ugly. Datatype should contain its base uri.
     require(RdfNamespace.XSD.prefix+":"+calendar.getXMLSchemaType.getLocalPart == datatype.name, "invalid date "+year.getOrElse("")+"-"+month.getOrElse("")+"-"+day.getOrElse("")+" for "+datatype.name)
 
-    override def compare(that : Date) : Int = calendar.compare(that.calendar)
+    override def compare(that: Date) : Int = this.calendar.compare(that.calendar)
+    
+    override def equals(other: Any): Boolean = other match {
+      case that: Date => this.calendar.equals(that.calendar)
+      case _ => false
+    }
+    
+    override def hashCode: Int = this.calendar.hashCode
     
     override def toString = calendar.toXMLFormat
 }
 
 object Date
 {
+    // store the result of this expensive call
+    private val datatypeFactory = DatatypeFactory.newInstance()
+    
     def merge(dates : List[Date], datatype : Datatype) : Date =
     {
         require(! dates.isEmpty, "dates are required")
-
-        val year  = dates.map(_.year).reduceLeft(_ orElse _)
-        val month = dates.map(_.month).reduceLeft(_ orElse _)
-        val day   = dates.map(_.day).reduceLeft(_ orElse _)
+        
+        val year  = dates.find(_.year.isDefined).map(_.year.get)
+        val month = dates.find(_.month.isDefined).map(_.month.get)
+        val day   = dates.find(_.day.isDefined).map(_.day.get)
 
         new Date(year, month, day, datatype)
     }
