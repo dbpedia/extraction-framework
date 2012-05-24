f3fee395695af934c2df0e9de1bcadc2870bad33
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-24 23:26:11 +0200

    Condition: call matches() in extract(). Redundant if Condition is used in a ConditionalMapping, but in the future, a Condition may also be used in a CompositeMapping.

--- core/src/main/scala/org/dbpedia/extraction/mappings/ConditionalMapping.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/ConditionalMapping.scala
@@ -10,23 +10,23 @@ class ConditionalMapping(
 )
 extends Mapping[TemplateNode]
 {
-    override val datasets = cases.flatMap(_.datasets).toSet ++ defaultMappings.flatMap(_.datasets).toSet
+  override val datasets = cases.flatMap(_.datasets).toSet ++ defaultMappings.flatMap(_.datasets).toSet
 
-    override def extract(node: TemplateNode, subjectUri : String, pageContext : PageContext) : Seq[Quad] =
+  override def extract(node: TemplateNode, subjectUri : String, pageContext : PageContext) : Seq[Quad] =
+  {
+    for(condition <- cases)
     {
-      for(condition <- cases)
-      {
-        if (condition.matches(node)) {
-          val graph = condition.extract(node, subjectUri, pageContext)
-          // template mapping sets instance URI
-          val instanceURI = node.annotation(TemplateMapping.INSTANCE_URI_ANNOTATION)
-            .getOrElse(throw new IllegalArgumentException("missing instance URI"))
-            .asInstanceOf[String]
-          return graph ++ defaultMappings.flatMap(_.extract(node, instanceURI, pageContext))
-        }
+      if (condition.matches(node)) {
+        val graph = condition.extract(node, subjectUri, pageContext)
+        // template mapping sets instance URI
+        val instanceURI = node.annotation(TemplateMapping.INSTANCE_URI_ANNOTATION)
+          .getOrElse(throw new IllegalArgumentException("missing instance URI"))
+          .asInstanceOf[String]
+        return graph ++ defaultMappings.flatMap(_.extract(node, instanceURI, pageContext))
       }
-      Seq.empty
     }
+    Seq.empty
+  }
 }
 
 class ConditionMapping(
@@ -37,36 +37,48 @@ class ConditionMapping(
 ) 
 extends Mapping[TemplateNode]
 {
-    /** Check if templateProperty is defined */
-    require(operator == "otherwise" || templateProperty != null, "templateProperty must be defined")
-    /** Check if given operator is supported */
-    require(List("isSet", "equals", "contains", "otherwise").contains(operator), "Invalid operator: " + operator +". Supported operators: isSet, equals, contains, otherwise")
-    /** Check if value is defined */
-    require(operator == "otherwise" || operator == "isSet" || value != null, "Value must be defined")
+  /** Check if templateProperty is defined */
+  require(operator == "otherwise" || templateProperty != null, "templateProperty must be defined")
+  
+  /** Check if given operator is supported */
+  require(List("isSet", "equals", "contains", "otherwise").contains(operator), "Invalid operator: " + operator +". Supported operators: isSet, equals, contains, otherwise")
+  
+  /** Check if value is defined */
+  require(operator == "otherwise" || operator == "isSet" || value != null, "Value must be defined")
 
-    override val datasets = mapping.datasets
+  override val datasets = mapping.datasets
 
-    def extract(node : TemplateNode, subjectUri : String, pageContext : PageContext) : Seq[Quad] =
-    {
-       mapping.extract(node, subjectUri, pageContext)
-    }
+  override def extract(node : TemplateNode, subjectUri : String, pageContext : PageContext) : Seq[Quad] =
+  {
+    // Note: CompositeMapping will call extract() without calling matches() first, so we 
+    // have to check matches() here. If we are part of a ConditionalMapping, this call of matches() 
+    // will be redundant, but it's cheap, so it's not a problem.
+    if (matches(node)) mapping.extract(node, subjectUri, pageContext)
+    else Seq.empty
+  }
 
-    def matches(node : TemplateNode) : Boolean =
-    {
-      if (operator == "otherwise") true
-      else {
-        val property = node.property(templateProperty).getOrElse(return false)
-        val propertyText = StringParser.parse(property).getOrElse("").toLowerCase.trim
+  def matches(node : TemplateNode) : Boolean =
+  {
+    if (operator == "otherwise") true
+    else {
+      
+      // Note: if you add operators whose evaluation may be expensive, you should probably
+      // add a constructor parameter that tells us if the call to matches() in extract() can
+      // be omitted. This constructor argument should be true if this object is part of 
+      // a ConditionalMapping (which calls matches() before extract()) and false otherwise.
+      val property = node.property(templateProperty).getOrElse(return false)
+      val propertyText = StringParser.parse(property).getOrElse("").toLowerCase.trim
 
-        operator match
-        {
-            case "isSet" => ! propertyText.isEmpty
-            // FIXME: toLowerCase must use correct language locale
-            case "equals" => propertyText == value.toLowerCase
-            // FIXME: toLowerCase must use correct language locale
-            case "contains" => propertyText.contains(value.toLowerCase)
-            case _ => false
-        }
+      operator match
+      {
+          case "isSet" => ! propertyText.isEmpty
+          // FIXME: toLowerCase must use correct language locale
+          case "equals" => propertyText == value.toLowerCase
+          // FIXME: toLowerCase must use correct language locale
+          case "contains" => propertyText.contains(value.toLowerCase)
+          case _ => false
       }
     }
+  }
+  
 }
