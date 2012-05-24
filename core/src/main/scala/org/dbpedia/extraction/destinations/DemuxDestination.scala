7721a094358c8d6558f7a08308b5ab5c4cdf7cd7
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-24 04:42:04 +0200

    super long comment.

--- core/src/main/scala/org/dbpedia/extraction/destinations/DemuxDestination.scala
+++ core/src/main/scala/org/dbpedia/extraction/destinations/DemuxDestination.scala
@@ -6,7 +6,16 @@ import collection.mutable.HashMap
  * A destination which groups quads by dataset and writes them to different destinations -
  * in other words, it de-multiplexes the quad stream (hence the name). 
  * 
- * This class is thread-safe.
+ * This class is thread-safe. It's actually a bit too thread-safe: it would be nice if multiple
+ * threads could execute write() concurrently, because they may well be writing to different
+ * target destinations and won't get into each others way. Only the call getOrElseUpdate() 
+ * needs to be synchronized really. But: the possibility that yet another thread may call
+ * close() makes things much harder. In practice, close() will probably be called by a master
+ * thread after all slave threads finished calling write(). But in theory, the calls may happen
+ * concurrently, and we must make sure that close() is called on all target destinations.
+ * That means that when close() is called, we cannot let any thread execute write().
+ * Read-write-locks may be the way... But anyway, it probably won't really be a problem.
+ * Usually, all destinations will access the same storage and that will be the bottleneck.
  * 
  * @param factory Called when a dataset is encountered for the first time, produces a destination. 
  * All quads for the dataset are written to that destination. If it returns null for a dataset, 
