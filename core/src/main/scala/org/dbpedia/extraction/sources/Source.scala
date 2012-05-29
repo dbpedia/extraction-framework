90d75a2386e2a09b8985374df375c126c71c9130
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-05-30 00:19:01 +0200

    comment

--- core/src/main/scala/org/dbpedia/extraction/sources/Source.scala
+++ core/src/main/scala/org/dbpedia/extraction/sources/Source.scala
@@ -4,10 +4,15 @@ import scala.collection.immutable.Traversable
 
 /**
  * A source of wiki pages.
- * FIXME: get rid of this class. It has no purpose except hasDefiniteSize = false and makes APIs 
- * that use it less flexible. When we get rid of it, we should replace it by an Iterator or
- * a Stream, not a Traversable. We don't want to keep its elements around any longer than we 
- * absolutely have to.
+ * TODO: do we need this class? Methods that have a paramater with this type are not as flexible
+ * as they could be, for example they cannot be called with List(page). The only advantage of
+ * this class seems to be the definition hasDefiniteSize = false, which can easily be added to
+ * any Traversable implementations that actually need it. We should clearly document in which
+ * cases hasDefiniteSize = false is useful or necessary. The Scala documentation doesn't help
+ * much here. Traversable also defines all kinds of methods - for example size() - that call
+ * foreach() and iterate over the whole collection, which for most of our sources is a huge
+ * waste. Even calling head() causes a big overhead, for example a file or a network connection
+ * is opened. Is there a more appropriate Scala type?
  */
 trait Source extends Traversable[WikiPage]
 {
