4d71116a6dde76e184579e08c563da87833a7b58
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-06-02 04:04:00 +0200

    comment

--- core/src/main/scala/org/dbpedia/extraction/util/FileLike.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/FileLike.scala
@@ -1,5 +1,8 @@
 package org.dbpedia.extraction.util
 
+/**
+ * Allows common handling of java.io.File and java.nio.file.Path
+ */
 abstract class FileLike[T <% FileLike[T]] {
   
   def resolve(name: String) : T
