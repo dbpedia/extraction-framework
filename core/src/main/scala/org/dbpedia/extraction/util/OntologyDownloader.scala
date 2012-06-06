8efb0faf8db74386709868b910e69e240ff9bc73
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-06-06 02:50:00 +0200

    also save OWL file

--- core/src/main/scala/org/dbpedia/extraction/util/OntologyDownloader.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/OntologyDownloader.scala
@@ -1,14 +1,15 @@
 package org.dbpedia.extraction.util
 
-import java.io.File
-
-import org.dbpedia.extraction.ontology.io.OntologyReader
+import java.io.{File,FileOutputStream,OutputStreamWriter}
+import org.dbpedia.extraction.ontology.Ontology
+import org.dbpedia.extraction.ontology.io.{OntologyReader,OntologyOWLWriter}
 import org.dbpedia.extraction.sources.XMLSource
 import org.dbpedia.extraction.wikiparser.Namespace
 
 /**
  * Download ontology classes and properties from http://mappings.dbpedia.org/ and transform them
- * into the format of the dump files (because XMLSource understands that format). 
+ * into the format of the dump files (because XMLSource understands that format). Also save the
+ * result as OWL.
  */
 object OntologyDownloader {
   
@@ -16,27 +17,40 @@ object OntologyDownloader {
     
   def main(args: Array[String]) : Unit =
   {
-    require(args != null && args.length == 1, "expected one argument for ontology target file")
-    val file = new File(args(0))
-    download(file)
-    // load it to test it
-    load(file)
+    require(args != null && args.length == 2, "expected two arguments: target file for wikitext XML dump, target file for OWL format.")
+    val dumpFile = new File(args(0))
+    val owlFile = new File(args(1))
+    download(dumpFile)
+    val ontology = load(dumpFile)
+    save(ontology, owlFile)
   }
   
-  def download(file : File) : Unit =
+  def download(dumpFile: File): Unit =
   {
     val nanos = System.nanoTime
-    println("downloading ontology from "+apiUrl+" to "+file)
-    new WikiDownloader(apiUrl).download(file, Namespace.OntologyClass, Namespace.OntologyProperty)
-    println("downloaded ontology from "+apiUrl+" to "+file+" in "+((System.nanoTime - nanos) / 1000000000F)+" seconds")
+    println("downloading ontology from "+apiUrl+" to "+dumpFile)
+    new WikiDownloader(apiUrl).download(dumpFile, Namespace.OntologyClass, Namespace.OntologyProperty)
+    println("downloaded ontology from "+apiUrl+" to "+dumpFile+" in "+((System.nanoTime - nanos) / 1000000000F)+" seconds")
   }
     
-  def load(file : File) : Unit =
+  def load(dumpFile: File): Ontology =
   {
     val nanos = System.nanoTime
-    println("loading ontology from "+file)
-    new OntologyReader().read(XMLSource.fromFile(file, Language.Mappings))
-    println("loaded ontology from "+file+" in "+((System.nanoTime - nanos) / 1000000000F)+" seconds")
+    println("loading ontology from "+dumpFile)
+    val ontology = new OntologyReader().read(XMLSource.fromFile(dumpFile, Language.Mappings))
+    println("loaded ontology from "+dumpFile+" in "+((System.nanoTime - nanos) / 1000000000F)+" seconds")
+    ontology
+  }
+  
+  def save(ontology: Ontology, owlFile: File): Unit =
+  {
+    val nanos = System.nanoTime
+    println("saving ontology to "+owlFile)
+    val xml = new OntologyOWLWriter().write(ontology)
+    val writer = new OutputStreamWriter(new FileOutputStream(owlFile), "UTF-8")
+    try writer.write(xml.toString)
+    finally writer.close()
+    println("saved ontology to "+owlFile+" in "+((System.nanoTime - nanos) / 1000000000F)+" seconds")
   }
   
 }
