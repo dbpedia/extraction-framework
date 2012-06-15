b7d51fecdeded62634dfe6e08c7eb44748863ccd
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-06-15 03:34:08 +0200

    comments, cosmetics

--- core/src/main/scala/org/dbpedia/extraction/util/Workers.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/Workers.scala
@@ -42,7 +42,7 @@ object SimpleWorkers {
    * }
    */
   def apply[T <: AnyRef](proc: T => Unit): Workers[T] = {
-    apply(1)(proc)
+    apply(defaultThreads, defaultThreads)(proc)
   }
 }
   
@@ -87,32 +87,46 @@ object ResourceWorkers {
    * }
    */
   def apply[T <: AnyRef](factory: => Worker[T]): Workers[T] = {
-    apply(1)(factory)
+    apply(defaultThreads, defaultThreads)(factory)
   }
 }
 
+/**
+ * Constants for workers.
+ */
 object Workers {
+  
+  /**
+   * Sentinel object that signals to the threads that the queue is empty.
+   */
   private[util] val sentinel = new Object()
+  
+  /**
+   * By default, use one thread per logical processor.
+   */
   private[util] val defaultThreads = Runtime.getRuntime().availableProcessors()
 }
   
+/**
+ * A simple fixed size thread-pool.
+ * @param threads number of threads in pool
+ * @param queueLength max length of work queue
+ * @param factory called during initialization of this class to create a worker for each thread
+ */
 class Workers[T <: AnyRef](threads: Int, queueLength: Int, factory: => Worker[T]) {
-  
-  def this(queueDepth: Int, factory: => Worker[T]) = this(defaultThreads, defaultThreads * queueDepth, factory)
-  
-  def this(factory: => Worker[T]) = this(1, factory)
   
   private val queue = new ArrayBlockingQueue[AnyRef](queueLength)
   
   private val workers =
   for (i <- 0 until threads) yield
-  new Thread(getClass.getName+"-thread-"+i) {
+  new Thread() {
+    val worker = factory
     override def run(): Unit = {
-      val worker = factory
       worker.init()
       try {
         while(true) {
           val value = queue.take()
+          // if we find the sentinel, we're done
           if (value eq sentinel) return
           worker.process(value.asInstanceOf[T])
         }
@@ -122,17 +136,29 @@ class Workers[T <: AnyRef](threads: Int, queueLength: Int, factory: => Worker[T]
     }
   }
   
+  /**
+   * Start all threads. Each thread will initialize its worker.
+   */
   final def start(): Unit = {
     for (worker <- workers) worker.start()
   }
   
+  /**
+   * Add a value to the queue. A thread will take the value and let its worker process it.
+   * If queue is full and all threads are busy, wait until a thread becomes available.
+   */
   final def process(value: T): Unit = {
     if (value == null) throw new NullPointerException("value")
     queue.put(value)
   }
   
+  /**
+   * Stop all threads and wait for them to finish. Each thread will destroy its worker.
+   */
   final def stop(): Unit = {
+    // enqueue one sentinel per thread - each thread removes one
     for (worker <- workers) queue.put(sentinel)
+    // wait for the threads to find the sentinels and finish
     for (worker <- workers) worker.join()
   }
     
