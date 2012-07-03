d237c5c1e7ee2b67dd173098a164a39930967385
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-03 21:43:49 +0200

    cosmetics

--- core/src/main/scala/org/dbpedia/extraction/util/RichString.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/RichString.scala
@@ -89,7 +89,7 @@ class RichString(str : String)
      * Similar to java.lang.String.replaceAll() and scala.util.matching.Regex.replaceAllIn(),
      * but the replacement can be chosen more flexibly and this method should be more efficient.
      */
-    def replaceBy(pattern: Pattern, append: (StringBuilder, Matcher) => _): String = {
+    def replaceBy(pattern: Pattern, append: (StringBuilder, Matcher) => Unit): String = {
       val matcher = pattern.matcher(str)
       if (! matcher.find()) return str
       val sb = new StringBuilder
