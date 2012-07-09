f021d6cf0e8bbdc9e7bc4015363451233193c25a
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-09 18:08:23 +0200

    bugfix: "Cat_main" -> "Cat main"

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
@@ -4,7 +4,7 @@ object TopicalConceptsExtractorConfig
 {
 
     //FIXME name conflict between categories might exist between languages. add language-specific maps
-    val catMainTemplates = Set("Cat_main",
+    val catMainTemplates = Set("Cat main",
                                "Artigo principal", // for pt.wikipedia.org
                                "AP",               // for es.wikipedia.org
                                "Article principal" // for fr.wikipedia.org
