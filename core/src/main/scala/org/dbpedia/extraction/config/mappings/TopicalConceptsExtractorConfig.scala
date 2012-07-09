0bbcbfc1c4c00431a99d8fb49b958e62b8571819
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-09 16:08:26 +0200

    "Article principal" for fr.wikipedia.org

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
@@ -6,7 +6,8 @@ object TopicalConceptsExtractorConfig
     //FIXME name conflict between categories might exist between languages. add language-specific maps
     val catMainTemplates = Set("Cat_main",
                                "Artigo principal", // for pt.wikipedia.org
-                               "AP"                // for es.wikipedia.org
+                               "AP",               // for es.wikipedia.org
+                               "Article principal" // for fr.wikipedia.org
                             )
 
 }
