c8ab18cfb6e54162ec2aaed1bf52b76322fc7397
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-13 21:05:02 +0200

    moved ByteLogger and CountingInputStream from org.dbpedia.extraction.dump.download to org.dbpedia.extraction.util

--- core/src/main/scala/org/dbpedia/extraction/util/ByteLogger.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/ByteLogger.scala
@@ -1,21 +1,20 @@
-package org.dbpedia.extraction.dump.download
+package org.dbpedia.extraction.util
 
 /**
- * Logs read bytes.
- * @length length of input
- * @step approximate number of bytes read between progress logs
- * @pretty if true, re-use one line for output, otherwise use a new line for each log. 
+ * Logs read bytes. Meant to be used with CountingInputStream.
+ * @param length length of input
+ * @param step approximate number of bytes read between progress logs
+ * @param pretty if true, re-use one line for output, otherwise use a new line for each log. 
  * Pretty printing works when logging to a console, but usuaully not in files.
+ * @param nanos System.nanoTime used as start time
  */
-class ByteLogger(length : Long, step : Long, pretty : Boolean) extends ((Long, Boolean) => Unit)
+class ByteLogger(length: Long, step: Long, pretty: Boolean, nanos: Long = System.nanoTime) extends ((Long, Boolean) => Unit)
 {
   require(step > 0, "step must be > 0 but is "+step)
   
-  private var nanos = System.nanoTime
+  private var next: Long = step
   
-  private var next : Long = step
-  
-  def apply( bytes : Long, close : Boolean ) : Unit =
+  def apply(bytes: Long, close: Boolean): Unit =
   {
     if (close || bytes >= next)
     {
@@ -28,7 +27,7 @@ class ByteLogger(length : Long, step : Long, pretty : Boolean) extends ((Long, B
     }
   }
   
-  private def formatBytes( bytes : Long ) : String =
+  private def formatBytes(bytes: Long): String =
   {
     if (bytes <  0) "? B"
     else if (bytes < 1024) bytes+" B"
@@ -37,7 +36,7 @@ class ByteLogger(length : Long, step : Long, pretty : Boolean) extends ((Long, B
     else (bytes / 1073741824F)+" GB"
   }
   
-  private def formatMillis( millis : Long ) : String =
+  private def formatMillis(millis: Long): String =
   {
     val secs = millis / 1000
     if (secs < 60) millis / 1000F+" seconds"
@@ -45,12 +44,12 @@ class ByteLogger(length : Long, step : Long, pretty : Boolean) extends ((Long, B
     else zeros(secs / 3600)+":"+zeros(secs % 3600 / 60)+":"+zeros(secs % 60)+" hours"
   }
   
-  private def formatRate(bytes : Long, millis : Long) : String =
+  private def formatRate(bytes: Long, millis: Long): String =
   {
     if (millis == 0) "? B/s"
     else if (bytes / millis < 1024) (bytes / 1.024F / millis)+" KB/s"
     else (bytes / 1048.576F / millis)+" MB/s"
   }
   
-  private def zeros( num : Long ) : String = if (num < 10) "0"+num else num.toString
+  private def zeros(num: Long): String = if (num < 10) "0"+num else num.toString
 }
