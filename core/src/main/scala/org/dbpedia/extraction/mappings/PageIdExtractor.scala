ec3b3303a31f201ea1d492995f1bbbe14dd79482
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-17 17:41:45 +0200

    comment: "FIXME: does this make sense? We should probably use the DBpedia resource URI as subject"; use QuadBuilder

--- core/src/main/scala/org/dbpedia/extraction/mappings/PageIdExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/PageIdExtractor.scala
@@ -1,17 +1,20 @@
 package org.dbpedia.extraction.mappings
 
-import org.dbpedia.extraction.destinations.{DBpediaDatasets, Quad}
+import org.dbpedia.extraction.destinations.{DBpediaDatasets,Quad,QuadBuilder}
 import org.dbpedia.extraction.wikiparser.PageNode
 import org.dbpedia.extraction.ontology.Ontology
 import org.dbpedia.extraction.util.Language
 
 /**
- * Extracts page ids to articles.
+ * Extracts page ids of articles, e.g.
+ * <http://en.wikipedia.org/wiki/Foo> <http://dbpedia.org/ontology/wikiPageID> "123456"^^<xsd:integer> .
+ * 
+ * FIXME: does this make sense? We should probably use the DBpedia resource URI as subject.
  */
 class PageIdExtractor (
-  context : {
-    def ontology : Ontology
-    def language : Language
+  context: {
+    def ontology: Ontology
+    def language: Language
   }
 )
 extends Extractor
@@ -22,10 +25,11 @@ extends Extractor
 
   override val datasets = Set(DBpediaDatasets.PageIds)
 
+  private val quad = QuadBuilder(context.language, DBpediaDatasets.PageIds, wikiPageIdProperty, context.ontology.datatypes("xsd:integer")) _
+
   override def extract(page: PageNode, subjectUri: String, pageContext: PageContext): Seq[Quad] =
   {
     val objectLink = page.title.pageIri
-
-    Seq(new Quad(context.language, DBpediaDatasets.PageIds, objectLink, wikiPageIdProperty, page.id.toString, page.sourceUri, context.ontology.datatypes("xsd:integer")))
+    Seq(quad(objectLink, page.id.toString, page.sourceUri))
   }
 }
