de66366ef2947c711cebab254551b4f59b6d5b70
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-22 23:01:32 +0200

    renamed Appender.appendCodePoint to Appender.append, added comments

--- core/src/main/java/org/dbpedia/util/text/Appender.java
+++ core/src/main/java/org/dbpedia/util/text/Appender.java
@@ -1,9 +1,20 @@
 package org.dbpedia.util.text;
 
+/**
+ * Appends Unicode strings or code points to a {@link StringBuilder}.
+ */
 public interface Appender {
   
-  public void appendCodePoint(StringBuilder builder, int codePoint);
+  /**
+   * @param builder may or may not be null, depending on use case.
+   * @param code code point to append
+   */
+  public void append(StringBuilder builder, int code);
 
+  /**
+   * @param builder may or may not be null, depending on use case.
+   * @param str String to append
+   */
   public void append(StringBuilder builder, String str);
   
 }
