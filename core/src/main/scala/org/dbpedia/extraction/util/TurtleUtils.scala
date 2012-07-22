c2ba175682510ce3f53d374290728c317c283ee2
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-22 21:12:21 +0200

    added escapeTurtle() for single code point

--- core/src/main/scala/org/dbpedia/extraction/util/TurtleUtils.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/TurtleUtils.scala
@@ -30,24 +30,34 @@ object TurtleUtils {
     {
       val c = input.codePointAt(offset)
       offset += Character.charCount(c)
-
-      // TODO: use a lookup table for c <= 0xA0? c <= 0xFF?
-           if (c == '\\') sb append "\\\\"
-      else if (c == '\"') sb append "\\\""
-      else if (c == '\n') sb append "\\n"
-      else if (c == '\r') sb append "\\r";
-      else if (c == '\t') sb append "\\t"
-      else if (c >= 0x0020 && c < 0x007F) sb append c.toChar
-      else if (turtle && c >= 0x00A0 && c <= 0xFFFF) sb append c.toChar
-      else if (turtle && c >= 0x10000) sb appendCodePoint c
-      else if (c <= 0xFFFF) appendHex(sb, c, 'u', 4)
-      else appendHex(sb, c, 'U', 8)
+      escapeTurtle(sb, c, turtle)
     }
     
     sb
   }
   
-  private def appendHex(sb: StringBuilder, c: Int, u: Char, d: Int) {
+  /**
+   * Escapes a Unicode code point according to Turtle / N-Triples format.
+   * @param sb must not be null
+   * @param c Unicode code point
+   * @param turtle if true, non-ASCII characters are not escaped (allowed by Turtle); 
+   * if false, non-ASCII characters are escaped (required by N-Triples / N-Quads).
+   */
+  def escapeTurtle(sb: StringBuilder, c: Int, turtle: Boolean): StringBuilder = {
+    // TODO: use a lookup table for c <= 0xA0? c <= 0xFF?
+         if (c == '\\') sb append "\\\\"
+    else if (c == '\"') sb append "\\\""
+    else if (c == '\n') sb append "\\n"
+    else if (c == '\r') sb append "\\r"
+    else if (c == '\t') sb append "\\t"
+    else if (c >= 0x0020 && c < 0x007F) sb append c.toChar
+    else if (turtle && c >= 0x00A0 && c <= 0xFFFF) sb append c.toChar
+    else if (turtle && c >= 0x10000) sb appendCodePoint c
+    else if (c <= 0xFFFF) appendHex(sb, 'u', c, 4)
+    else appendHex(sb, 'U', c, 8)
+  }
+
+  private def appendHex(sb: StringBuilder, u: Char, c: Int, d: Int): StringBuilder = {
     sb append "\\" append u
     NumberUtils.intToHex(sb, c, d) 
   }
