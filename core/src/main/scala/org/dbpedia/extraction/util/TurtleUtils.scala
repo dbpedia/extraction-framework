684968f6273fe7ae511c0e209b25099f0bd6355f
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-25 00:43:36 +0200

    refactoring: move stuff from object TurtleUtils to class TurtleEscaper

--- core/src/main/scala/org/dbpedia/extraction/util/TurtleUtils.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/TurtleUtils.scala
@@ -11,7 +11,9 @@ object TurtleUtils {
    * if false, non-ASCII characters are escaped (required by N-Triples / N-Quads).
    */
   def escapeTurtle(input: String, turtle: Boolean): String = {
-    escapeTurtle(new StringBuilder, input, turtle).toString
+    val sb = new StringBuilder
+    new TurtleEscaper(sb, turtle).escapeTurtle(input)
+    sb.toString
   }
   
   /**
@@ -22,17 +24,7 @@ object TurtleUtils {
    * if false, non-ASCII characters are escaped (required by N-Triples / N-Quads).
    */
   def escapeTurtle(sb: StringBuilder, input: String, turtle: Boolean): StringBuilder = {
-
-    val length = input.length
-
-    var offset = 0
-    while (offset < length)
-    {
-      val code = input.codePointAt(offset)
-      offset += Character.charCount(code)
-      escapeTurtle(sb, code, turtle)
-    }
-    
+    new TurtleEscaper(sb, turtle).escapeTurtle(input)
     sb
   }
   
@@ -44,6 +36,40 @@ object TurtleUtils {
    * if false, non-ASCII characters are escaped (required by N-Triples / N-Quads).
    */
   def escapeTurtle(sb: StringBuilder, code: Int, turtle: Boolean): StringBuilder = {
+    new TurtleEscaper(sb, turtle).escapeTurtle(code)
+    sb
+  }
+  
+}
+
+/**
+ * Escapes a Unicode string according to Turtle / N-Triples format.
+ * @param sb must not be null
+ * @param turtle if true, non-ASCII characters are not escaped (allowed by Turtle); 
+ * if false, non-ASCII characters are escaped (required by N-Triples / N-Quads).
+ */
+class TurtleEscaper(sb: StringBuilder, turtle: Boolean) {
+  
+  /**
+   * Escapes a Unicode string according to Turtle / N-Triples format.
+   * @param input must not be null
+   */
+  def escapeTurtle(input: String): Unit = {
+    val length = input.length
+    var index = 0
+    while (index < length)
+    {
+      val code = input.codePointAt(index)
+      index += Character.charCount(code)
+      escapeTurtle(code)
+    }
+  }
+  
+  /**
+   * Escapes a Unicode code point according to Turtle / N-Triples format.
+   * @param c Unicode code point
+   */
+  def escapeTurtle(code: Int) = {
     // TODO: use a lookup table for c <= 0xA0? c <= 0xFF?
          if (code == '\\') sb append "\\\\"
     else if (code == '\"') sb append "\\\""
@@ -53,11 +79,11 @@ object TurtleUtils {
     else if (code >= 0x0020 && code < 0x007F) sb append code.toChar
     else if (turtle && code >= 0x00A0 && code <= 0xFFFF) sb append code.toChar
     else if (turtle && code >= 0x10000) sb appendCodePoint code
-    else if (code <= 0xFFFF) appendHex(sb, 'u', code, 4)
-    else appendHex(sb, 'U', code, 8)
+    else if (code <= 0xFFFF) appendHex('u', code, 4)
+    else appendHex('U', code, 8)
   }
 
-  private def appendHex(sb: StringBuilder, esc: Char, code: Int, digits: Int): StringBuilder = {
+  private def appendHex(esc: Char, code: Int, digits: Int): StringBuilder = {
     sb append "\\" append esc
     NumberUtils.intToHex(sb, code, digits) 
   }
