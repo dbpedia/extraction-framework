622e79151e5037d9da219b01fc8a9264d25d6c3f
Jona Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2012-07-25 21:09:29 +0200

    write type counts to files, redirecting stdout doesn't work (probably a Maven bug/feature?)

--- scripts/src/main/scala/org/dbpedia/extraction/scripts/CountTypes.scala
+++ scripts/src/main/scala/org/dbpedia/extraction/scripts/CountTypes.scala
@@ -5,16 +5,18 @@ import java.io.File
 import org.dbpedia.extraction.ontology.RdfNamespace.fullUri
 import org.dbpedia.extraction.ontology.DBpediaNamespace.ONTOLOGY
 import org.dbpedia.extraction.ontology.RdfNamespace
+import org.dbpedia.extraction.util.RichFile.wrapFile
 import scala.collection.immutable.SortedMap
 import scala.collection.mutable
 import scala.collection.mutable.HashMap
 import scala.collection.Map
+import IOUtils.write
 
-class Counter(var num: Int = 0)
+class Counter(var num: Long = 0L)
 
 /**
  * Example call:
- * ../run CountTypes /data/dbpedia instance-types .ttl.gz 10000-
+ * ../run CountTypes /data/dbpedia instance-types .ttl.gz instance-types-counted.txt true 10000-
  */
 object CountTypes {
   
@@ -24,12 +26,14 @@ object CountTypes {
   
   def main(args: Array[String]): Unit = {
     
-    require(args != null && args.length >= 4, 
-      "need at least four args: "+
+    require(args != null && args.length >= 5,
+      "need at least five args: "+
       /*0*/ "base dir, "+
-      /*1*/ "name of input dataset (e.g. 'instance-types'), "+
-      /*2*/ "input file suffix (e.g. '.nt.gz', '.ttl', '.ttl.bz2'), " +
-      /*3*/ "languages or article count ranges (e.g. 'en,fr' or '10000-')")
+      /*1*/ "input dataset name (e.g. 'instance-types'); "+
+      /*2*/ "input file suffix (e.g. '.nt.gz', '.ttl', '.ttl.bz2'); " +
+      /*3*/ "output text file name (e.g. 'instance-types-counted.txt' - one file per language dir, plus one file for total count in base dir); " +
+      /*4*/ "boolean flag: if true, write total type count to file in base dir, otherwise don't; " +
+      /*5*/ "languages or article count ranges (e.g. 'en,fr' or '10000-')")
     
     val baseDir = new File(args(0))
     
@@ -41,12 +45,17 @@ object CountTypes {
     val suffix = args(2)
     require(suffix.nonEmpty, "no input file suffix")
     
-    val languages = parseLanguages(baseDir, args.drop(3))
+    val output = args(3)
+    require(output.nonEmpty, "no output file name")
+    
+    val total = args(4).toBoolean
+    
+    val languages = parseLanguages(baseDir, args.drop(5))
     require(languages.nonEmpty, "no languages")
     
     val rdfType = RdfNamespace.RDF.append("type")
     
-    val totalTypes = new HashMap[String, Counter]()
+    val totalTypes = if (total) new HashMap[String, Counter]() else null
     
     for (language <- languages) {
       val languageTypes = new HashMap[String, Counter]()
@@ -55,11 +64,11 @@ object CountTypes {
         if (quad.datatype != null) throw new IllegalArgumentException("expected object uri, found object literal: "+quad)
         if (quad.predicate != rdfType) throw new IllegalArgumentException("expected object uri, found object literal: "+quad)
         countType(quad.value, languageTypes)
-        countType(quad.value, totalTypes)
+        if (total) countType(quad.value, totalTypes)
       }
-      printTypes(language.wikiCode+" "+input, languageTypes)
+      printTypes(finder.find(output), language.wikiCode+" "+input, languageTypes)
     }
-    printTypes("total", totalTypes)
+    if (total) printTypes(new File(baseDir, output), "total", totalTypes)
     
   }
   
@@ -67,13 +76,15 @@ object CountTypes {
     types.getOrElseUpdate(uri, new Counter).num += 1
   }
   
-  private def printTypes(tag: String, types: Map[String, Counter]): Unit = {
+  private def printTypes(file: File, tag: String, types: Map[String, Counter]): Unit = {
     val sorted = SortedMap(types.toArray: _*)
-    println(tag)
-    for ((name, counter) <- sorted) {
-      println(name+"\t"+counter.num)
+    val writer = write(file)
+    try {
+      for ((name, counter) <- sorted) {
+        writer.write(name+"\t"+counter.num+"\n")
+      }
     }
-    println()
+    finally writer.close()
   }
   
 }
