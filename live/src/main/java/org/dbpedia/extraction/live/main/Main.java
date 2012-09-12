c5253a16f675e5b7562d3fb0ce97eba7cc2b4120
Dimitris Kontokostas <jimkont@gmail.com>
2012-09-12 15:39:02 +0300

    Add preliminary support to handle shutdown gracefully; Needs more work...

--- live/src/main/java/org/dbpedia/extraction/live/main/Main.java
+++ live/src/main/java/org/dbpedia/extraction/live/main/Main.java
@@ -1,6 +1,7 @@
 package org.dbpedia.extraction.live.main;
 
 
+import org.apache.log4j.Logger;
 import org.dbpedia.extraction.live.core.LiveOptions;
 import org.dbpedia.extraction.live.feeder.OAIFeeder;
 import org.dbpedia.extraction.live.feeder.OAIFeederMappings;
@@ -12,9 +13,10 @@ import org.dbpedia.extraction.live.publisher.Publisher;
 import org.dbpedia.extraction.live.publisher.PublishingData;
 import org.dbpedia.extraction.live.statistics.Statistics;
 import org.dbpedia.extraction.live.util.DateUtil;
+import org.dbpedia.extraction.live.util.ExceptionUtil;
 import org.dbpedia.extraction.live.util.Files;
 
-import java.io.*;
+import java.io.File;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.util.TreeMap;
@@ -25,8 +27,9 @@ import java.util.concurrent.PriorityBlockingQueue;
 //import org.dbpedia.extraction.wikiparser.*;
 
 
-public class Main
-{
+public class Main {
+    private static Logger logger = Logger.getLogger(Main.class);
+
     private static final int NUMBER_OF_RECENTLY_UPDATED_INSTANCES = 20;
 
 
@@ -47,56 +50,94 @@ public class Main
     //it will be just deleted immediately without any further processing.
     public static TreeMap<Long, Boolean> existingPagesTree = new TreeMap<Long, Boolean>();
 
-	public static void authenticate(final String username, final String password)
-	{
-		Authenticator.setDefault(new Authenticator() {
-		    @Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-		        return new PasswordAuthentication(username,
-		        								  password.toCharArray());
-		    }
-		});
-	}
+    // TODO make these non-static
+    private static OAIFeederMappings feederMappings = null;
+    private static OAIFeeder feederLive = null;
+    private static OAIFeeder feederUnmodified = null;
+    private static Statistics statistics = null;
 
-	public static void main(String[] args)
-		throws Exception
-	{
+    public static void authenticate(final String username, final String password) {
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username,
+                        password.toCharArray());
+            }
+        });
+    }
+
+    // TODO this needs more work (later)
+    public static void stopLive() {
+
+        if (feederMappings != null)
+            feederMappings.stopFeeder();
+        if (feederLive != null)
+            feederLive.stopFeeder();
+        if (feederUnmodified != null)
+            feederUnmodified.stopFeeder();
+        if (statistics != null)
+            statistics.stopStatistics();
+    }
+
+    // TODO this needs more work (later)
+    public static void startLive() {
+        try {
+
+            Publisher publisher = new Publisher("Publisher", 4);
+
+            //All feeders, one for live update, one for mapping affected pages, and the last one is for unmodified pages
+            feederMappings = new OAIFeederMappings("FeederMappings", Thread.MIN_PRIORITY, Priority.MappingPriority,
+                    LiveOptions.options.get("mappingsOAIUri"), LiveOptions.options.get("mappingsBaseWikiUri"), LiveOptions.options.get("mappingsOaiPrefix"),
+                    2000, 1000, LiveOptions.options.get("uploaded_dump_date"), 0,
+                    LiveOptions.options.get("working_directory"));
+            feederMappings.startFeeder();
+
+
+            feederLive = new OAIFeeder("FeederLive", Thread.NORM_PRIORITY, Priority.LivePriority,
+                    LiveOptions.options.get("oaiUri"), LiveOptions.options.get("baseWikiUri"), LiveOptions.options.get("oaiPrefix"),
+                    3000, 1000, LiveOptions.options.get("uploaded_dump_date"), 0,
+                    LiveOptions.options.get("working_directory"));
+            feederLive.startFeeder();
+
+            feederUnmodified = new OAIFeeder("FeederUnmodified", Thread.MIN_PRIORITY, Priority.UnmodifiedPagePriority,
+                    LiveOptions.options.get("oaiUri"), LiveOptions.options.get("baseWikiUri"), LiveOptions.options.get("oaiPrefix"),
+                    30000, 1000, LiveOptions.options.get("uploaded_dump_date"), DateUtil.getDuration1MonthMillis(),
+                    LiveOptions.options.get("working_directory"));
+            feederUnmodified.startFeeder();
+
+            PageProcessor processor = new PageProcessor("Page processing thread", 8);
+
+            PublishedDataCompressor compressor = new PublishedDataCompressor("PublishedDataCompressor", Thread.MIN_PRIORITY);
+
+            statistics = new Statistics(LiveOptions.options.get("statisticsFilePath"), NUMBER_OF_RECENTLY_UPDATED_INSTANCES,
+                    DateUtil.getDuration1MinMillis(), 2 * DateUtil.getDuration1MinMillis());
+            statistics.startStatistics();
+        }
+        catch (Exception exp){
+            logger.error(ExceptionUtil.toString(exp));
+        }
+    }
+
+    public static void main(String[] args)
+            throws Exception {
 
 
 
         Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
         authenticate("dbpedia", Files.readFile(new File("pw.txt")).trim());
 
-        Publisher publisher = new Publisher("Publisher", 4);
+        startLive();
 
-        //All feeders, one for live update, one for mapping affected pages, and the last one is for unmodified pages
-        OAIFeederMappings feederMappings = new OAIFeederMappings("FeederMappings", Thread.MIN_PRIORITY, Priority.MappingPriority,
-                LiveOptions.options.get("mappingsOAIUri"), LiveOptions.options.get("mappingsBaseWikiUri"), LiveOptions.options.get("mappingsOaiPrefix"),
-                2000, 1000, LiveOptions.options.get("uploaded_dump_date"), 0,
-                LiveOptions.options.get("working_directory"));
-        feederMappings.startFeeder();
+        // TODO this needs more work (later)
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                logger.warn("Received exit signal, trying to exit graceful...");
+                stopLive();
+            }
+        });
 
 
-        OAIFeeder feederLive = new OAIFeeder("FeederLive", Thread.NORM_PRIORITY, Priority.LivePriority,
-                LiveOptions.options.get("oaiUri"), LiveOptions.options.get("baseWikiUri"), LiveOptions.options.get("oaiPrefix"),
-                3000, 1000, LiveOptions.options.get("uploaded_dump_date"), 0,
-                LiveOptions.options.get("working_directory"));
-        feederLive.startFeeder();
-
-        OAIFeeder feederUnmodified = new OAIFeeder("FeederUnmodified", Thread.MIN_PRIORITY, Priority.UnmodifiedPagePriority,
-                LiveOptions.options.get("oaiUri"), LiveOptions.options.get("baseWikiUri"), LiveOptions.options.get("oaiPrefix"),
-                30000, 1000, LiveOptions.options.get("uploaded_dump_date"), DateUtil.getDuration1MonthMillis(),
-                LiveOptions.options.get("working_directory"));
-        feederUnmodified.startFeeder();
-
-        PageProcessor processor = new PageProcessor("Page processing thread", 8);
-
-        PublishedDataCompressor compressor = new PublishedDataCompressor("PublishedDataCompressor", Thread.MIN_PRIORITY);
-
-        Statistics statistics = new Statistics(LiveOptions.options.get("statisticsFilePath"),NUMBER_OF_RECENTLY_UPDATED_INSTANCES,
-                DateUtil.getDuration1MinMillis(),2*DateUtil.getDuration1MinMillis());
-        statistics.startStatistics();
-
-	}
+    }
 
 }
