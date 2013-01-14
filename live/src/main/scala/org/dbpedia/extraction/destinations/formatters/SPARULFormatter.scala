1dac0363200682ccc6fbf18b73ba01532a9e2d9c
Dimitris Kontokostas <jimkont@gmail.com>
2013-01-14 20:29:46 +0100

    Add SPARUL Formatter for Live

---
+++ live/src/main/scala/org/dbpedia/extraction/destinations/formatters/SPARULFormatter.scala
@@ -0,0 +1,16 @@
+package org.dbpedia.extraction.destinations.formatters
+
+import UriPolicy._
+
+/**
+ * TODO: add functionality - the comments could contain more useful info
+ *
+ * @param policies Mapping from URI positions (as defined in UriPolicy) to URI policy functions.
+ *                 Must have five (UriPolicy.POSITIONS) elements. If null, URIs will not be modified.
+ */
+class SPARULFormatter(insOrDel: Boolean, graph: String, policies: Array[Policy] = null)
+  extends TerseFormatter(false, false, policies) {
+  override def header = (if (insOrDel) "INSERT DATA INTO" else "DELETE DATA FROM") + " <" + graph + "> {\n"
+
+  override def footer = " } "
+}
