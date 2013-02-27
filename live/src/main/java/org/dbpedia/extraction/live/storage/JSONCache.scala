91fa63f2d6aa3d1967041ec71b0fb423cf58cc29
Dimitris Kontokostas <jimkont@gmail.com>
2013-02-27 18:41:04 +0200

    Store updateCount in cache

--- live/src/main/java/org/dbpedia/extraction/live/storage/JSONCache.scala
+++ live/src/main/java/org/dbpedia/extraction/live/storage/JSONCache.scala
@@ -71,12 +71,13 @@ class JSONCache(pageID: Long, pageTitle: String) {
   }
 
   def updateCache(json: String, subjects: String, diff: String): Boolean = {
+    val updatedTimes = if ( cacheObj == null) "0" else (cacheObj.updatedTimes + 1).toString
     // Check wheather to update or insert
     if (cacheExists) {
-      return JDBCUtil.execPrepared(DBpediaSQLQueries.getJSONCacheUpdate, Array[String](this.pageTitle, "0",  json, subjects, diff,  "" + this.pageID))
+      return JDBCUtil.execPrepared(DBpediaSQLQueries.getJSONCacheUpdate, Array[String](this.pageTitle, updatedTimes,  json, subjects, diff,  "" + this.pageID))
     }
     else
-      return JDBCUtil.execPrepared(DBpediaSQLQueries.getJSONCacheInsert, Array[String]("" + this.pageID, this.pageTitle, "0",  json, subjects, diff))
+      return JDBCUtil.execPrepared(DBpediaSQLQueries.getJSONCacheInsert, Array[String]("" + this.pageID, this.pageTitle, updatedTimes,  json, subjects, diff))
   }
 
   private def initCache {
