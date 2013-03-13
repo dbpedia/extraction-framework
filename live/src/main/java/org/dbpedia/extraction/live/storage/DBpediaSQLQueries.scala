276ceb65edbe5bc1bf9ef74e665d8b0741498425
Dimitris Kontokostas <jimkont@gmail.com>
2013-03-13 09:24:44 +0200

    Optimize query to use the index

--- live/src/main/java/org/dbpedia/extraction/live/storage/DBpediaSQLQueries.scala
+++ live/src/main/java/org/dbpedia/extraction/live/storage/DBpediaSQLQueries.scala
@@ -26,7 +26,7 @@ object DBpediaSQLQueries {
   }
 
   def getJSONCacheUnmodified: String = {
-    "SELECT pageID, updated FROM DBPEDIALIVE_CACHE WHERE datediff(updated,now()) <= ? ORDER BY updated ASC LIMIT ? "
+    "SELECT pageID, updated FROM DBPEDIALIVE_CACHE WHERE updated <= now() - INTERVAL ? DAY ORDER BY updated ASC LIMIT ? "
   }
 
   def getJSONCacheUpdateError: String = {
