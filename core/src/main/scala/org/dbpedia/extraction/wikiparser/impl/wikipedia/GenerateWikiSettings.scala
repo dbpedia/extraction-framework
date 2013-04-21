51208862a0dfa118d54554d592c03bb721993541
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2013-04-21 15:18:35 +0300

    comment about alternative langlist file URL
    langlist was unavailable for some days. It's back, but if it goes away again, we may use a copy in git.

--- core/src/main/scala/org/dbpedia/extraction/wikiparser/impl/wikipedia/GenerateWikiSettings.scala
+++ core/src/main/scala/org/dbpedia/extraction/wikiparser/impl/wikipedia/GenerateWikiSettings.scala
@@ -53,6 +53,11 @@ object GenerateWikiSettings {
     val languageMap = new LinkedHashMap[String, String]()
     
     // Note: langlist is sometimes not correctly sorted (done by hand), but no problem for us.
+    // langlist was unavailable for several days in April 2013. Reported by Omri Oren:
+    // https://github.com/dbpedia/extraction-framework/issues/37
+    // It's back, thanks to Omri and Krinkle (Wikimedia). If it goes away again, we may use the copy in git:
+    // https://gerrit.wikimedia.org/r/gitweb?p=operations/mediawiki-config.git;a=blob_plain;f=langlist
+    // I don't really trust that long and ugly URL though, so I will leave the old URL for now. JC 2013-04-21
     val source = Source.fromURL("http://noc.wikimedia.org/conf/langlist")(Codec.UTF8)
     var languages = try source.getLines.toList finally source.close
     languages = "mappings" :: "commons" :: "wikidata" :: languages
