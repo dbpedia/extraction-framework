19b5d1b1179c206bb4f70538d6f11bb8775068cd
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2013-04-21 18:29:47 +0200

    Import.scala should be able to handle zipped files

--- dump/src/main/scala/org/dbpedia/extraction/dump/sql/Import.scala
+++ dump/src/main/scala/org/dbpedia/extraction/dump/sql/Import.scala
@@ -1,6 +1,6 @@
 package org.dbpedia.extraction.dump.sql
 
-import java.io.File
+import java.io._
 import org.dbpedia.extraction.sources.XMLSource
 import org.dbpedia.extraction.util.{Finder,Language,WikiInfo}
 import org.dbpedia.extraction.util.ConfigUtils.parseLanguages
@@ -12,6 +12,9 @@ import scala.collection.immutable.SortedSet
 import org.dbpedia.extraction.dump.download.Download
 import java.util.Properties
 import scala.io.Source
+import org.apache.commons.compress.compressors.bzip2._
+import java.util.zip._
+import scala.io.Codec.UTF8
 
 object Import {
   
@@ -21,9 +24,10 @@ object Import {
     val tablesFile = new File(args(1))
     val url = args(2)
     val requireComplete = args(3).toBoolean
+    val fileName = args(4)
     
     // Use all remaining args as keys or comma or whitespace separated lists of keys
-    var languages = parseLanguages(baseDir, args.drop(4))
+    var languages = parseLanguages(baseDir, args.drop(5))
     
     val source = Source.fromFile(tablesFile)(Codec.UTF8)
     val tables =
@@ -40,15 +44,15 @@ object Import {
       for (language <- languages) {
         
         val finder = new Finder[File](baseDir, language, "wiki")
-        val tagFile = if (requireComplete) Download.Complete else "pages-articles.xml"
+        val tagFile = if (requireComplete) Download.Complete else fileName
         val date = finder.dates(tagFile).last
-        val file = finder.file(date, "pages-articles.xml")
+        val file = finder.file(date, fileName)
         
         val database = finder.wikiName
         
         println("importing pages in namespaces "+namespaceList+" from "+file+" to database "+database+" on server URL "+url)
         
-        val source = XMLSource.fromFile(file, language, title => namespaces.contains(title.namespace))
+        val source = XMLSource.fromReader(reader(file), language, title => namespaces.contains(title.namespace))
         
         val stmt = conn.createStatement()
         try {
@@ -66,5 +70,32 @@ object Import {
     
   }
   
+  // TODO: The rest is copied from ConfigLoader.scala. We should find a central place for the 
+  // auto-unzip stuff. Maybe in core, but I don't like the added dependency on commons-compress.
+  
+  private def reader(file: File): () => Reader = {
+    val unzip = unzipper(file.getName)
+    () => new InputStreamReader(unzip(new FileInputStream(file)), UTF8)
+  }
+
+  /**
+   * @return stream zipper function
+   */
+  private def unzipper(name: String): InputStream => InputStream = {
+    unzippers.getOrElse(suffix(name), identity)
+  }
+  
+  /**
+   * @return file suffix
+   */
+  private def suffix(name: String): String = {
+    name.substring(name.lastIndexOf('.') + 1)
+  }
+  
+  private val unzippers = Map[String, InputStream => InputStream] (
+    "gz" -> { new GZIPInputStream(_) }, 
+    "bz2" -> { new BZip2CompressorInputStream(_, true) } 
+  )
+    
 }
 
