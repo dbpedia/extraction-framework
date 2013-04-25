4f6ba6a2c2ab0a98e622aed7588e912758a5be6a
Julien Plu <julien-plu@data.lirmm.fr>
2013-04-25 15:17:31 +0200

    Code cleaning
    - throw an exception if the dump analysed is not the french one
    - remove the "hard" URIs creation by a more cleaner way
    - remove the datatype because it was already precised with the dbpedia ontology

--- core/src/main/scala/org/dbpedia/extraction/mappings/fr/PopulationExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/fr/PopulationExtractor.scala
@@ -12,39 +12,43 @@ import org.dbpedia.extraction.mappings._
  * Extracts links to the official homepage of an instance.
  */
 class PopulationExtractor(
-  context : {
-    def ontology : Ontology
-    def language : Language
-    def redirects : Redirects
-  }
+    context : {
+        def ontology : Ontology
+        def language : Language
+    	def redirects : Redirects
+    }
 )
-extends Extractor
-{
-  private val populationProperty = context.ontology.properties("populationTotal")
+extends Extractor {
+    private val populationProperty = context.ontology.properties("populationTotal")
+    private val populationRegex = """pop=(\d+)""".r
+    override val datasets = Set(DBpediaDatasets.FrenchPopulation)
 
-  private val populationRegex = """pop=(\d+)""".r
+    override def extract(page: PageNode, subjectUri: String, pageContext: PageContext): Seq[Quad] =
+    {
+	if (context.language.wikiCode == "fr") {
+            if(page.title.namespace != Namespace.Template || page.isRedirect || !page.title.decoded.contains("évolution population") || page.title.decoded.contains("Discussion") || page.title.decoded.contains("Modèles")) return Seq.empty
+  		println(page.title.decoded) 
+    	        for (node <- page.children) {
+	            if (node.toWikiText.contains("|pop=")) {
+		        populationRegex.findAllIn(node.toWikiText).matchData foreach {
+			    m =>
+			    val city = page.title.decoded.split("/")(1).replace(" ", "_")
+			
+			    if (!isAllDigits(city)) {
+		    	        val newUri = context.language.resourceUri.append(city)
+			    
+			        return Seq (new Quad(context.language, DBpediaDatasets.FrenchPopulation, newUri, populationProperty, m.group(1), node.sourceUri))
+			    }
+		        }
+	            }
+                }
 
-  override val datasets = Set(DBpediaDatasets.FrenchPopulation)
-
-  override def extract(page: PageNode, subjectUri: String, pageContext: PageContext): Seq[Quad] =
-  {
-    if(page.title.namespace != Namespace.Template || page.isRedirect || !page.title.decoded.contains("évolution population") || page.title.decoded.contains("Discussion")) return Seq.empty
-   
-    for (node <- page.children) {
-	if (node.toWikiText.contains("|pop=")) {
-		populationRegex.findAllIn(node.toWikiText).matchData foreach {
-			m =>
-			val city = page.title.decoded.split("/")(1).replace(" ", "_")
-			if (!isAllDigits(city)) {
-				val newUri = "http://fr.dbpedia.org/resource/" + city
-				return Seq (new Quad(context.language, DBpediaDatasets.FrenchPopulation, newUri, populationProperty, m.group(1), node.sourceUri, context.ontology.datatypes("xsd:nonNegativeInteger")))
-			}
-		}
+                Seq.empty
+        }
+	else {
+	    throw new Exception("The PopulationExtractor is only made for the french Wikipedia dump")
 	}
     }
-
-    Seq.empty
-  }
-
-  private def isAllDigits(x: String) = x.matches("^\\d*$")
+    
+    private def isAllDigits(x: String) = x.matches("^\\d*$")
 }
