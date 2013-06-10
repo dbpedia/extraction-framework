c8068e57291f47821115847d3c789e866c375b0a
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2013-06-10 03:09:50 +0200

    Italian topic fixes (mammadori)

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
@@ -8,13 +8,14 @@ object TopicalConceptsExtractorConfig
     // Don't change this else if you know how it is done.
 
     val catMainTemplates = Set("Cat main",
-      "مزيد"   ,// for ar.wikipedia.org
-                               "Infocat", "Infocatm", // for ca.wikipedia.org
-                               "Catmore",          // for el.wikipedia.org
-                               "AP",               // for es.wikipedia.org
-                               "Nagusia",          // for eu.wikipedia.org
-                               "Article principal",// for fr.wikipedia.org
-                               "Artigo principal", // for pt.wikipedia.org
-                               "Основная статья по теме категории", "Catmain" //for ru.wikipedia.org
+      "مزيد"   ,// ar
+                               "Infocat", "Infocatm", // ca
+                               "Catmore",          // el
+                               "AP",               // es
+                               "Nagusia",          // eu
+                               "Article principal",// fr
+                               "Voce principale", "torna a",  // it
+                               "Artigo principal", // pt
+                               "Основная статья по теме категории", "Catmain" // ru
                             )
 }
