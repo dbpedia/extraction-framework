b28dbacc23c5955e955f5816b9cfbbd1aecee029
Dimitris Kontokostas <jimkont@gmail.com>
2013-09-02 20:27:44 +0300

    Add map of equivalent Classes / Properties in Ontology

--- core/src/main/scala/org/dbpedia/extraction/ontology/io/OntologyReader.scala
+++ core/src/main/scala/org/dbpedia/extraction/ontology/io/OntologyReader.scala
@@ -72,10 +72,12 @@ class OntologyReader
                 val name = getName(page.title, _.capitalize(page.title.language.locale))
 
                 ontologyBuilder.classes ::= loadClass(name, templateNode)
+                // Fill the equivalentClass map
 
-                for(specificProperty <- loadSpecificProperties(name, templateNode))
+              for(specificProperty <- loadSpecificProperties(name, templateNode))
                 {
                     ontologyBuilder.specializedProperties ::= specificProperty
+                    // To check for equivalent Property Map
                 }
             }
             else if(templateName == OntologyReader.OBJECTPROPERTY_NAME || templateName == OntologyReader.DATATYPEPROPERTY_NAME)
@@ -85,6 +87,7 @@ class OntologyReader
                 for(property <- loadOntologyProperty(name, templateNode))
                 {
                     ontologyBuilder.properties ::= property
+                    // Fill the equivalentProperty map
                 }
             }
             // TODO: read datatypes
@@ -298,6 +301,8 @@ class OntologyReader
         var properties = List[PropertyBuilder]()
         var datatypes = List[Datatype]()
         var specializedProperties = List[SpecificPropertyBuilder]()
+        var equivalentPropertiesMap = Map[OntologyProperty,Set[OntologyProperty]] ()
+        var equivalentClassesMap = Map[OntologyProperty,Set[OntologyProperty]] ()
 
         def build() : Ontology  =
         {
@@ -308,7 +313,9 @@ class OntologyReader
             new Ontology( classes.flatMap(_.build(classMap)).map(c => (c.name, c)).toMap,
                           properties.flatMap(_.build(classMap, typeMap)).map(p => (p.name, p)).toMap,
                           datatypes.map(t => (t.name, t)).toMap,
-                          specializedProperties.flatMap(_.build(classMap, propertyMap, typeMap)).toMap )
+                          specializedProperties.flatMap(_.build(classMap, propertyMap, typeMap)).toMap,
+                          equivalentPropertiesMap,
+                          equivalentClassesMap)
         }
     }
 
