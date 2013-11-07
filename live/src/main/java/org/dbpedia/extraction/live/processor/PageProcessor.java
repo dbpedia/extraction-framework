6bb35c23ed00084292623cb9ef86b12f734bec64
Dimitris Kontokostas <jimkont@gmail.com>
2013-11-07 08:54:59 +0200

    log page deletions

--- live/src/main/java/org/dbpedia/extraction/live/processor/PageProcessor.java
+++ live/src/main/java/org/dbpedia/extraction/live/processor/PageProcessor.java
@@ -66,8 +66,10 @@ public class PageProcessor extends Thread{
                 if (page.getPriority() == LiveQueuePriority.MappingPriority) {
                     LiveExtractionConfigLoader.reload(page.getStatQueueAdd());
                 }
-                if (page.isDeleted() == true)
+                if (page.isDeleted() == true) {
                     JSONCache.deleteCacheItem(page.getItemID(),LiveExtractionConfigLoader.policies());
+                    logger.info("Deleted page with ID: " + page.getItemID() + " (" + page.getItemName() + ")");
+                }
                 else
                     processPage(page);
             }
