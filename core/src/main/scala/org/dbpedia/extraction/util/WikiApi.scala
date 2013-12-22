4a5dd9ef5c9a1577ef88c761a2fec1ea6d701d08
Dimitris Kontokostas <jimkont@gmail.com>
2013-12-22 20:33:41 +0200

    bugfix provided by @ninniuz

--- core/src/main/scala/org/dbpedia/extraction/util/WikiApi.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/WikiApi.scala
@@ -53,7 +53,7 @@ class WikiApi(url: URL, language: Language)
         // -> "generator" instead of "list" and "gapnamespace" instead of "apnamespace" ("gap" is for "generator all pages")
  
         //Retrieve list of pages
-        val response = query("?action=query&format=xml&list=allpages&apfrom=" + fromPage + "&aplimit=" + pageListLimit + "&apnamespace=" + namespace.code)
+        val response = query("?action=query&format=xml&list=allpages&apfrom=" + URLEncoder.encode(fromPage, "UTF-8") + "&aplimit=" + pageListLimit + "&apnamespace=" + namespace.code)
 
         //Extract page ids
         val pageIds = for(p <- response \ "query" \ "allpages" \ "p") yield (p \ "@pageid").head.text.toLong
