22e68f65bac4f400358f28a8eb4651fd5bf3bdc9
Dimitris Kontokostas <jimkont@gmail.com>
2014-01-24 13:14:35 +0200

    fix comment

--- core/src/main/scala/org/dbpedia/extraction/mappings/JsonNodeExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/JsonNodeExtractor.scala
@@ -4,7 +4,7 @@ import org.dbpedia.extraction.destinations.Quad
 import org.dbpedia.extraction.wikiparser._
 
 /**
- * Extractors are mappings that extract data from a PageNode.
+ * Extractors are mappings that extract data from a JsonNode.
  * Necessary to get some type safety in CompositeExtractor: 
  * Class[_ <: Extractor] can be checked at runtime, but Class[_ <: Mapping[PageNode]] can not.
  */
