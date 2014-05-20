c4b28000f31262d9fc8f8497a2a89f329f317a95
Magnus <magnus@FB10MK.local>
2014-05-20 19:55:41 +0200

    minor

--- live/src/main/java/org/dbpedia/extraction/live/storage/DBpediaSQLQueries.scala
+++ live/src/main/java/org/dbpedia/extraction/live/storage/DBpediaSQLQueries.scala
@@ -10,15 +10,15 @@ object DBpediaSQLQueries {
     * JSON Cache Queries (select, update, insert, delete)
     * */
   def getJSONCacheSelect: String = {
-    "SELECT timesUpdated, json, subjects FROM DBPEDIALIVE_CACHE WHERE pageID = ? "
+    "SELECT timesUpdated, json, subjects FROM DBPEDIALIVE_CACHE WHERE pageID = ?"
   }
 
   def getJSONCacheInsert: String = {
-    "INSERT INTO DBPEDIALIVE_CACHE (pageID, title, updated, timesUpdated, json, subjects, diff) VALUES ( ?, ? , now() , ? , ? , ? , ?  ) "
+    "INSERT INTO DBPEDIALIVE_CACHE (pageID, title, updated, timesUpdated, json, subjects, diff) VALUES ( ?, ? , now() , ? , ? , ? , ?  )"
   }
 
   def getJSONCacheUpdate: String = {
-    "UPDATE DBPEDIALIVE_CACHE SET title = ?, updated = now(), timesUpdated = ?, json = ?, subjects = ?, diff = ? WHERE pageID = ? "
+    "UPDATE DBPEDIALIVE_CACHE SET title = ?, updated = now(), timesUpdated = ?, json = ?, subjects = ?, diff = ? WHERE pageID = ?"
   }
 
   def getJSONCacheDelete: String = {
@@ -26,11 +26,11 @@ object DBpediaSQLQueries {
   }
 
   def getJSONCacheUnmodified: String = {
-    "SELECT pageID, updated FROM DBPEDIALIVE_CACHE WHERE updated <= now() - INTERVAL ? DAY ORDER BY updated ASC LIMIT ? "
+    "SELECT pageID, updated FROM DBPEDIALIVE_CACHE WHERE updated <= now() - INTERVAL ? DAY ORDER BY updated ASC LIMIT ?"
   }
 
   def getJSONCacheUpdateError: String = {
-    "UPDATE DBPEDIALIVE_CACHE SET error = ?, updated = now(), timesUpdated = timesUpdated + 1 WHERE pageID = ? "
+    "UPDATE DBPEDIALIVE_CACHE SET error = ?, updated = now(), timesUpdated = timesUpdated + 1 WHERE pageID = ?"
   }
 
 }
