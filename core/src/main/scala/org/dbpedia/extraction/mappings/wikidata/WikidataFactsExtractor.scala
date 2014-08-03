c376849a0f5c07f28ccc78f0522dd9d8d63f1d19
alismayilov <alismayilov@gmail.com>
2014-08-03 20:57:55 +0200

    WikidataFactsExtractor only string facts extraction

--- core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataFactsExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataFactsExtractor.scala
@@ -1,12 +1,18 @@
 package org.dbpedia.extraction.mappings
 
+import java.util.Collections
+
 import org.dbpedia.extraction.ontology._
 import org.dbpedia.extraction.util.Language
 import org.dbpedia.extraction.destinations.{Quad, DBpediaDatasets}
 import org.dbpedia.extraction.wikiparser.{JsonNode, PageNode}
+import org.json.JSONObject
+import org.wikidata.wdtk.datamodel.implementation.DataObjectFactoryImpl
+import org.wikidata.wdtk.datamodel.interfaces.DataObjectFactory
 import collection.mutable.ArrayBuffer
 import scala.language.reflectiveCalls
-
+import scala.collection.JavaConversions._
+import scala.util.parsing.json.JSON
 
 
 /**
@@ -36,10 +42,30 @@ class WikidataFactsExtractor(
   // this is where we will store the output
   override val datasets = Set(DBpediaDatasets.WikidataFacts)
 
-  override def extract(page : JsonNode, subjectUri : String, pageContext : PageContext): Seq[Quad] =
-  {
+  override def extract(page : JsonNode, subjectUri : String, pageContext : PageContext): Seq[Quad] = {
     // This array will hold all the triples we will extract
     val quads = new ArrayBuffer[Quad]()
+    for ((statementGroup) <- page.wikiDataItem.getStatementGroups) {
+      val claim = statementGroup.getStatements().get(0).getClaim()
+      val propertyId = claim.getMainSnak().getPropertyId().toString().replace("(PropertyId)", "")
+      try {
+        val mainSnak: JSONObject = new JSONObject(claim.getMainSnak())
+        val value = JSON.parseFull(mainSnak.get("value").toString())
+        value match {
+          case Some(map: Map[String, Any]) => {
+              println(map.get("string"))
+              map.get("string") match {
+               case Some(s) => quads += new Quad(context.language, DBpediaDatasets.WikidataFacts, subjectUri, propertyId, s.toString(), page.wikiPage.sourceUri, context.ontology.datatypes("xsd:string"))
+               case _ =>
+             }
+            }
+            case _ =>
+          }
+     }
+     catch {
+       case _:IndexOutOfBoundsException =>
+     }
+    }
     quads
   }
 }
