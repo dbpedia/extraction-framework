0bd1a4a77acdbbc9b8e276817a9dac269dd91481
alismayilov <alismayilov@gmail.com>
2014-08-03 20:52:07 +0200

    WikidataFactsExtractor

--- core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataDescriptionExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataDescriptionExtractor.scala
@@ -10,33 +10,32 @@ import scala.collection.mutable.ArrayBuffer
 /**
  * Created by ali on 7/29/14.
  */
-
-class WikidataAliasExtractor (
-                               context : {
-                                 def ontology : Ontology
-                                 def language : Language
-                               }
-                               )
+class WikidataDescriptionExtractor (
+                                     context : {
+                                       def ontology : Ontology
+                                       def language : Language
+                                     }
+                                     )
   extends JsonNodeExtractor {
   // Here we define all the ontology predicates we will use
   private val isPrimaryTopicOf = context.ontology.properties("foaf:isPrimaryTopicOf")
   private val primaryTopic = context.ontology.properties("foaf:primaryTopic")
   private val dcLanguage = context.ontology.properties("dc:language")
-  private val aliasProperty = context.ontology.properties("alias")
+  private val aliasProperty = context.ontology.properties("description")
 
 
   // this is where we will store the output
-  override val datasets = Set(DBpediaDatasets.WikidataAlias)
+  override val datasets = Set(DBpediaDatasets.WikidataDescription)
 
   override def extract(page: JsonNode, subjectUri: String, pageContext: PageContext): Seq[Quad] = {
     // This array will hold all the triples we will extract
     val quads = new ArrayBuffer[Quad]()
 
-    for ((lang, value) <- page.wikiDataItem.getAliases) {
-      val alias = value.toString().replace("(" + lang + ")", "").replace("[","").replace("]","").trim()
+    for ((lang, value) <- page.wikiDataItem.getDescriptions()) {
+      val description = value.toString().replace("(" + lang + ")", "").trim()
       Language.get(lang) match
       {
-        case Some(dbpedia_lang) => quads += new Quad(dbpedia_lang, DBpediaDatasets.WikidataAlias, subjectUri, aliasProperty,alias.toString(), page.wikiPage.sourceUri, context.ontology.datatypes("rdf:langString"))
+        case Some(dbpedia_lang) => quads += new Quad(dbpedia_lang, DBpediaDatasets.WikidataDescription, subjectUri, aliasProperty,description, page.wikiPage.sourceUri, context.ontology.datatypes("rdf:langString"))
         case _=>
       }
     }
---
+++ core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataFactsExtractor.scala
@@ -0,0 +1,45 @@
+package org.dbpedia.extraction.mappings
+
+import org.dbpedia.extraction.ontology._
+import org.dbpedia.extraction.util.Language
+import org.dbpedia.extraction.destinations.{Quad, DBpediaDatasets}
+import org.dbpedia.extraction.wikiparser.{JsonNode, PageNode}
+import collection.mutable.ArrayBuffer
+import scala.language.reflectiveCalls
+
+
+
+/**
+* Extracts Wikidata claims
+* on the form of
+* value triples:
+* <http://wikidata.dbpedia.org/resouce/Q64> <http://www.wikidata.org/entity/P625> "33.3333333 -123.433333333"
+* URI triples
+* <http://wikidata.dbpedia.org/resouce/Q64> <http://www.wikidata.org/entity/P625> <wikidata.dbpedia.org/resource/Q223>
+*
+*/
+class WikidataFactsExtractor(
+                         context : {
+                           def ontology : Ontology
+                           def language : Language
+                         }
+                         )
+  extends JsonNodeExtractor
+{
+  // Here we define all the ontology predicates we will use
+  private val isPrimaryTopicOf = context.ontology.properties("foaf:isPrimaryTopicOf")
+  private val primaryTopic = context.ontology.properties("foaf:primaryTopic")
+  private val dcLanguage = context.ontology.properties("dc:language")
+  private val labelProperty = context.ontology.properties("rdfs:label")
+
+
+  // this is where we will store the output
+  override val datasets = Set(DBpediaDatasets.WikidataFacts)
+
+  override def extract(page : JsonNode, subjectUri : String, pageContext : PageContext): Seq[Quad] =
+  {
+    // This array will hold all the triples we will extract
+    val quads = new ArrayBuffer[Quad]()
+    quads
+  }
+}
