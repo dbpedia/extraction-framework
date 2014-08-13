78d8c3d24448dd9babd09b51df6d58560663b2e0
gaurav <gaurav@ggvaidya.com>
2014-08-12 20:24:57 -0600

    Added dbo:hasAnnotation and dbo:asWikiText (and updated ontology to include them).

--- core/src/main/scala/org/dbpedia/extraction/mappings/ImageAnnotationExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/ImageAnnotationExtractor.scala
@@ -11,6 +11,8 @@ import org.dbpedia.extraction.util.StringUtils._
 /**
  * Extracts image annotations created using the Image Annotator gadget
  * (https://commons.wikimedia.org/wiki/Help:Gadget-ImageAnnotator)
+ *
+ * This uses the W3C Media Fragments 1.0: http://www.w3.org/TR/2012/REC-media-frags-20120925/ 
  */
 class ImageAnnotationExtractor (
   context : {
@@ -20,8 +22,12 @@ class ImageAnnotationExtractor (
 )
 extends PageNodeExtractor
 {
-  private val descriptionProperty = context.ontology.properties("description")
   private val logger = Logger.getLogger(classOf[ImageAnnotationExtractor].getName)
+
+  /* Properties */
+  private val hasAnnotationProperty = context.ontology.properties("hasAnnotation")
+  private val descriptionProperty = context.ontology.properties("description")
+  private val asWikiTextProperty = context.ontology.properties("asWikiText")
 
   override val datasets = Set(DBpediaDatasets.ImageAnnotations)
 
@@ -30,9 +36,6 @@ extends PageNodeExtractor
     // Only Commons files please.
     if(context.language != Language.Commons || pageNode.title.namespace != Namespace.File)
         return Seq.empty
-
-    // TODO: extend this to include templates inside templates?
-    // Not if none of the other extractors does this.
 
     // Get a list of every ImageNote on this page.
     val imageNoteStarts = pageNode.children.filter({
@@ -46,7 +49,7 @@ extends PageNodeExtractor
         case _ => false
     })
 
-    // Zip the two lists so that we end up with a datastructure of
+    // Zip the two lists so that we end up with a Seq of
     // corresponding (ImageNote, ImageNoteEnd) pairs.
     val imageNoteQuads = imageNoteStarts.zip(imageNoteEnds).flatMap(pair => { 
         pair match {
@@ -63,21 +66,55 @@ extends PageNodeExtractor
                 val indexStart = pageNode.children.indexOf(noteStart)
                 val indexEnd = pageNode.children.indexOf(noteEnd)
 
+                // Build a URI for a portion of this image.
+                // Since all these are required properties, we'll just throw
+                // RuntimeExceptions if any of them are missing.
+                def getImageNodePropertyIntValue(key: String):Int = {
+                    var str = ""
+                    try {
+                        str = noteStart.property(key).get.children.map(_.toPlainText).mkString("")
+                        str.toInt
+                    } catch {
+                        case ex: NoSuchElementException =>
+                            throw new RuntimeException("Template ImageNote missing property '" + key + "' in " + subjectUri)
+                        case ex: NumberFormatException =>
+                            throw new RuntimeException("Property '" + key + "' has a non-numerical value '" + str + "', only numerical values are accepted.")
+                    }
+                }
+
+                // Get the x, y, w, h and build a URL for that section of the
+                // image using the W3 Media Fragments recommendation at
+                // http://www.w3.org/TR/2012/REC-media-frags-20120925/
+                val annotation_x = getImageNodePropertyIntValue("x")
+                val annotation_y = getImageNodePropertyIntValue("y")
+                val annotation_w = getImageNodePropertyIntValue("w")
+                val annotation_h = getImageNodePropertyIntValue("h")
+                val annotation_url = ExtractorUtils.getFileURL(pageNode.title.encoded, context.language) +
+                    s"#xywh=pixel:$annotation_x,$annotation_y,$annotation_w,$annotation_h"
+
                 // Get every node between the start and end template nodes,
                 // and convert it into WikiText and plaintext.
                 val contents = if(indexEnd - indexStart < 1) Seq.empty
                     else pageNode.children.slice(indexStart + 1, indexEnd)
-                val contents_plaintext = contents.map(_.toPlainText).mkString(" ")
-                val contents_wikitext = contents.map(_.toWikiText).mkString(" ")
+                val contents_plaintext = contents.map(_.toPlainText).mkString("").trim
+                val contents_wikitext = (noteStart +: contents :+ noteEnd).map(_.toWikiText).mkString("").trim
 
-                // TODO: generate a quad to indicate than an annotation exists.
+                // Generate a quad to indicate than an annotation exists.
+                val quad_annotation = new Quad(
+                    context.language,
+                    DBpediaDatasets.ImageAnnotations,
+                    subjectUri,
+                    hasAnnotationProperty,
+                    annotation_url,
+                    pageNode.sourceUri
+                )
 
                 // Generate one quad each for plaintext and wikitext content.
                 val quads_contents = Seq(
                     new Quad(
                         context.language, 
                         DBpediaDatasets.ImageAnnotations, 
-                        subjectUri, 
+                        annotation_url, 
                         descriptionProperty, 
                         contents_plaintext, 
                         pageNode.sourceUri
@@ -85,14 +122,14 @@ extends PageNodeExtractor
                     new Quad(
                         context.language, 
                         DBpediaDatasets.ImageAnnotations, 
-                        subjectUri, 
-                        descriptionProperty, 
+                        annotation_url, 
+                        asWikiTextProperty, 
                         contents_wikitext, 
                         pageNode.sourceUri
                     )
                 )
 
-                quads_contents
+                quad_annotation +: quads_contents
             }
 
             case _ => throw new RuntimeException("Pair ('" + pair._1 + "', '" + pair._2 + "') detected unexpectedly.")
