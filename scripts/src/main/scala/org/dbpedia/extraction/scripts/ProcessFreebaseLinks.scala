8ccb56de20dfacf7df5f32fb1e82e40535c671f8
Daniel Fleischhacker <daniel@informatik.uni-mannheim.de>
2014-08-13 21:52:40 +0200

    Adapt Freebase sameAs link generation to new format
    RDF format of Freebase RDF dumps has changed from the
    format assumed in this script. This commit adapts the
    parsing code to work again

--- scripts/src/main/scala/org/dbpedia/extraction/scripts/ProcessFreebaseLinks.scala
+++ scripts/src/main/scala/org/dbpedia/extraction/scripts/ProcessFreebaseLinks.scala
@@ -26,17 +26,17 @@ object ProcessFreebaseLinks
     require(args != null && args.length == 1 && args(0).nonEmpty, "missing required argument: config file name")
 
     val config = loadConfig(args(0), "UTF-8")
-    
+
     val baseDir = getValue(config, "base-dir", true)(new File(_))
     if (! baseDir.exists) throw error("dir "+baseDir+" does not exist")
-    
+
     val input = getString(config, "input", true)
     val inputFile = new File(baseDir, input)
-    
+
     val output = getString(config, "output", true)
-    
+
     val languages = parseLanguages(baseDir, getStrings(config, "languages", ',', true))
-    
+
     val formats = parseFormats(config, "uri-policy", "format")
 
     // destinations for all languages
@@ -44,11 +44,11 @@ object ProcessFreebaseLinks
       // This seems to be the only way to avoid creating an Option object during map lookup.
       override def default(k: String): Destination = null
     }
-    
+
     for (language <- languages) {
       val finder = new Finder[File](baseDir, language, "wiki")
       val date = finder.dates().last
-      
+
       val formatDestinations = new ArrayBuffer[Destination]()
       for ((suffix, format) <- formats) {
         val file = finder.file(date, output+'.'+suffix)
@@ -56,20 +56,20 @@ object ProcessFreebaseLinks
       }
       destinations(language.wikiCode) = new CompositeDestination(formatDestinations.toSeq: _*)
     }
-    
+
     val undefined = new TreeSet[String]()
-    
+
     // I didn't find a way to create a singleton Seq without a lot of copying, so we re-use this array.
     // It's silly, but I don't want to be an accomplice in Scala's wanton disregard of efficiency.
     val quads = new ArrayBuffer[Quad](1)
     // Initialize first element
     quads += null
-    
+
     val startNanos = System.nanoTime
     err.println("reading Freebase file "+input+", writing DBpedia files...")
-    
+
     destinations.values.foreach(_.open())
-    
+
     // Freebase files are huge - use Long to count lines, not Int
     var lineCount = 0L
     var linkCount = 0L
@@ -92,44 +92,51 @@ object ProcessFreebaseLinks
         if (lineCount % 1000000 == 0) logRead("line", lineCount, startNanos)
       }
     }
-    
+
     destinations.values.foreach(_.close())
-    
+
     logRead("line", lineCount, startNanos)
     logRead("link", linkCount, startNanos)
-    
+
     err.println("links for these languages were found but not written because configuration didn't include them: "+undefined.mkString("[",",","]"))
   }
 
 /*
 
-The lines that we are interested in look exactly like this:
+The lines that we are interested in look exactly like this: (as of August 2014)
 
-ns:m.0104jp	ns:common.topic.topic_equivalent_webpage	<http://pt.wikipedia.org/wiki/Buna>.
+<http://rdf.freebase.com/ns/m.01c7c8>  <http://rdf.freebase.com/ns/common.topic.topic_equivalent_webpage>  <http://en.wikipedia.org/wiki/USS_George_Washington_(CVN-73)>   .
 
 Fields are separated by tabs, not spaces. We simply extract the MID, the language and the title,
 create DBpedia and Freebase URIs and create a Quad from them.
 
 There are similar lines that we currently don't care about:
 
-ns:m.0104jp	ns:common.topic.topic_equivalent_webpage	<http://pt.wikipedia.org/wiki/index.html?curid=1409578>.
+<http://rdf.freebase.com/ns/m.01c7c8>  <http://rdf.freebase.com/ns/common.topic.topic_equivalent_webpage> <http://pt.wikipedia.org/wiki/index.html?curid=1409578>.
+
 
 Wikipedia titles always start with capital letters, so we can exclude these lines because their
 titles start with lower-case 'i'.
 
 */
   
-  private val prefix = "ns:m."
-  private val midStart = prefix.length
+  private val part1 = "<http://rdf.freebase.com/ns/m."
+  private val midStart = part1.length
   
-  private val infix = "\tns:common.topic.topic_equivalent_webpage\t<http://"
-  private val infixLength = infix.length
+  private val predicatePart = "<http://rdf.freebase.com/ns/common.topic.topic_equivalent_webpage>"
+  private val predicatePartLength = predicatePart.length
+
+  private val preLanguageCode = "<http://"
+  private val preLanguageCodeLength = preLanguageCode.length
+
+  private val wikipediaUriInfix = ".wikipedia.org/wiki/"
+  private val wikipediaUriInfixLength = wikipediaUriInfix.length
   
-  private val wikipedia = ".wikipedia.org/wiki/"
-  private val wikipediaLength = wikipedia.length
-  
-  private val suffix = ">."
+  private val suffix = ">"
   private val suffixLength = suffix.length
+
+  private val lineTerminator = "."
+  private val lineTerminatorLength = lineTerminator.length
   
   private val sameAs = RdfNamespace.OWL.append("sameAs")
 
@@ -138,29 +145,39 @@ titles start with lower-case 'i'.
   
   private def parseLink(line: String): Quad = {
     
-    if (! line.startsWith(prefix)) return null
+    if (! line.startsWith(part1)) return null
     
-    val midEnd = line.indexOf('\t', midStart)
+    val midEnd = line.indexOf('>', midStart)
     if (midEnd == -1) return null
+
+    val withoutSubject = line.substring(midEnd + 1).trim
+
+    if (! withoutSubject.startsWith(predicatePart)) return null
+
+    val objectPart = withoutSubject.substring(predicatePartLength + 1).trim()
+
+    val langStart = objectPart.indexOf(preLanguageCode)
+    if (langStart == -1) return null
     
-    if (! line.startsWith(infix, midEnd)) return null
-    val langStart = midEnd + infixLength
-    
-    val langEnd = line.indexOf('.', langStart)
+    val langEnd = objectPart.indexOf('.', langStart + preLanguageCodeLength)
     if (langEnd == -1) return null
     
-    if (! line.startsWith(wikipedia, langEnd)) return null
-    val titleStart = langEnd + wikipediaLength
+    if (! objectPart.startsWith(wikipediaUriInfix, langEnd)) return null
+    val titleStart = langEnd + wikipediaUriInfixLength
     
     // exclude ".../wiki/index.html?curid=..." lines
-    if (line.charAt(titleStart) == 'i') return null
+    if (objectPart.charAt(titleStart) == 'i') return null
     
-    if (! line.endsWith(suffix)) return null
-    val titleEnd = line.length - suffixLength
+    if (! objectPart.endsWith(lineTerminator)) return null
+    val withoutLineTerminator = objectPart.substring(titleStart, objectPart.length - lineTerminatorLength).trim()
+
+    if (!withoutLineTerminator.endsWith(suffix)) return null
+
+    val titleEnd = withoutLineTerminator.length - suffixLength
     
     val mid = line.substring(midStart, midEnd)
-    val lang = line.substring(langStart, langEnd)
-    var title = line.substring(titleStart, titleEnd)
+    val lang = objectPart.substring(langStart + preLanguageCodeLength, langEnd)
+    var title = withoutLineTerminator.substring(0, titleEnd)
     
     // Some Freebase URIs are percent-encoded - let's decode them
     title = UriDecoder.decode(title)
@@ -183,5 +200,4 @@ titles start with lower-case 'i'.
   private def error(message: String, cause: Throwable = null): IllegalArgumentException = {
     new IllegalArgumentException(message, cause)
   }
-    
 }
