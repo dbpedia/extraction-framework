59489085a507fa9e3cb72b8c6d83659db3f712c3
Daniel Fleischhacker <daniel@informatik.uni-mannheim.de>
2014-08-20 16:59:26 +0200

    Add language code to generic domain URI for existing abstract

--- scripts/src/main/scala/org/dbpedia/extraction/scripts/GenerateListOfExistingAbstracts.scala
+++ scripts/src/main/scala/org/dbpedia/extraction/scripts/GenerateListOfExistingAbstracts.scala
@@ -16,12 +16,13 @@ import scala.collection.mutable.HashMap
  */
 object GenerateListOfExistingAbstracts {
   def main(args: Array[String]) {
-    require(args != null && args.length >= 3,
+    require(args != null && args.length >= 4,
       "need at least three args: " +
         /*0*/ "base dir, " +
         /*1*/ "input suffix (e.g., .nt.gz), " +
         /*2*/ "output file name (e.g., existing-abtracts.tsv), " +
-        /*3*/ "languages or article count ranges (e.g. 'en,fr' or '10000-')")
+        /*3*/ "language code of generic domain (e.g., en), " +
+        /*4*/ "languages or article count ranges (e.g. 'en,fr' or '10000-')")
 
     val baseDir = new File(args(0))
 
@@ -29,7 +30,9 @@ object GenerateListOfExistingAbstracts {
 
     val outputFile = new File(args(2))
 
-    val languages = parseLanguages(baseDir, args.drop(3))
+    val genericDomain = args(3)
+
+    val languages = parseLanguages(baseDir, args.drop(4))
     require(languages.nonEmpty, "no languages")
 
     val writer = new BufferedWriter(new FileWriter(outputFile))
@@ -38,7 +41,14 @@ object GenerateListOfExistingAbstracts {
     for (language <- languages) {
       val finder = new DateFinder[File](baseDir, language)
       QuadReader.readQuads(finder, "long-abstracts" + suffix, auto=true) { quad =>
-        writer.write(s"${language.wikiCode}\t${quad.subject}\n")
+        val languageUri =
+        if (quad.subject.startsWith("http://dbpedia.org")) {
+          quad.subject.replace("http://dbpedia.org", s"http://$genericDomain.dbpedia.org")
+        }
+        else {
+          quad.subject
+        }
+        writer.write(s"${language.wikiCode}\t$languageUri\n")
       }
     }
 
