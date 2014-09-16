299ce9ce565b6244e2387f41fb1d5aba8e925812
alismayilov <alismayilov@gmail.com>
2014-09-16 12:39:53 +0200

    some changes according to commets

--- core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataLLExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/wikidata/WikidataLLExtractor.scala
@@ -29,37 +29,40 @@ class WikidataLLExtractor(
   // Here we define all the ontology predicates we will use
   private val sameAsProperty = context.ontology.properties("owl:sameAs")
 
-
-  // this is where we will store the output
-  override val datasets = for (lang <-Namespace.mappings.keySet) yield new Dataset("wikidata-ll-"+lang.wikiCode)
+  private val mappingLanguages = Namespace.mappings.keySet
+  private val datasetMap:Map[String,Dataset]= (for (lang <-mappingLanguages) yield (lang.wikiCode -> new Dataset("wikidata-ll-"+lang.wikiCode)))(collection.breakOut)
+  override val datasets = datasetMap.values.toSet
 
   override def extract(page : JsonNode, subjectUri : String, pageContext : PageContext): Seq[Quad] =
   {
     // This array will hold all the triples we will extract
     val quads = new ArrayBuffer[Quad]()
 
-    for (lang<-Namespace.mappings.keySet) {
-      for ((wikidataLang, siteLink) <- page.wikiDataItem.getSiteLinks) {
+    for ((wikidataLang, siteLink) <- page.wikiDataItem.getSiteLinks) {
+      val lang1 = wikidataLang.toString().replace("wiki", "")
+      if (datasetMap.keys.contains(lang1)) {
         for ((wikidataLang2, siteLink2) <- page.wikiDataItem.getSiteLinks) {
-          if (wikidataLang != wikidataLang2) {
-            val l1 = wikidataLang.toString().replace("wiki", "")
-            val l2 = wikidataLang2.toString().replace("wiki", "")
-            if (lang.wikiCode == l1) {
-              val sitelink1 = WikiTitle.parse(siteLink.getPageTitle().toString(), lang)
-              Language.get(l2) match {
-                case Some(dbpedia_lang) => {
-                  val sitelink2 = WikiTitle.parse(siteLink2.getPageTitle().toString(), dbpedia_lang)
-                  val dataset = Map(lang.wikiCode -> new Dataset("wikidata-ll-"+lang.wikiCode))
-                  quads += new Quad(context.language, dataset(lang.wikiCode), sitelink1.resourceIri,
-                    sameAsProperty, sitelink2.resourceIri, page.wikiPage.sourceUri,null)
+          val lang2 = wikidataLang2.toString().replace("wiki", "")
+          if (datasetMap.keys.contains(lang2) & lang1 != lang2) {
+            Language.get(lang1) match {
+              case Some(dbpedia_lang1) => {
+                Language.get(lang2) match {
+                  case Some(dbpedia_lang2) => {
+                    val sitelink1 = WikiTitle.parse(siteLink.getPageTitle().toString(), dbpedia_lang1)
+                    val sitelink2 = WikiTitle.parse(siteLink2.getPageTitle().toString(), dbpedia_lang2)
+                    quads += new Quad(context.language, datasetMap(lang1), sitelink1.resourceIri,
+                      sameAsProperty, sitelink2.resourceIri, page.wikiPage.sourceUri, null)
+                  }
+                  case _ =>
                 }
-                case _ =>
               }
+              case _ =>
             }
           }
         }
       }
     }
+
     quads
   }
 }
