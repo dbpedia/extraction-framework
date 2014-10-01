2b0fa79da6a9324bca5e06a70609ab1e550a714c
Dimitris Kontokostas <jimkont@gmail.com>
2014-10-01 10:40:50 +0300

    simplify SPARUL Builder and use terse builder directly

--- live/src/main/scala/org/dbpedia/extraction/destinations/formatters/SPARULFormatter.scala
+++ live/src/main/scala/org/dbpedia/extraction/destinations/formatters/SPARULFormatter.scala
@@ -3,13 +3,14 @@ package org.dbpedia.extraction.destinations.formatters
 import UriPolicy._
 
 /**
- * TODO: add functionality - the comments could contain more useful info
+ * SPARUL triple blocks are Turtle-compatible so we modify only the heafer / footer
+ * and use a Turtle builder for the rest
  *
  * @param policies Mapping from URI positions (as defined in UriPolicy) to URI policy functions.
  *                 Must have five (UriPolicy.POSITIONS) elements. If null, URIs will not be modified.
  */
 class SPARULFormatter(insOrDel: Boolean, graph: String, policies: Array[Policy] = null)
-  extends TripleFormatter(() => new SPARULBuilder(policies))  {
+  extends TripleFormatter(() =>new TerseBuilder(false, true, policies))  {
   override def header = (if (insOrDel) "INSERT DATA INTO" else "DELETE DATA FROM") + " <" + graph + "> {\n"
 
   override def footer = " } "
