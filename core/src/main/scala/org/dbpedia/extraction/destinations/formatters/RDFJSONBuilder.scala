83785aa1dd471d12113754ee4b3dc228f815cf3a
feroshjacob <feroshjacob@gmail.com>
2014-10-28 21:54:39 -0400

    scala.util.parsong.jsob is deprecated and removed in Scala 2.11, JSONFormat.quoteString to a local quoteString method

--- core/src/main/scala/org/dbpedia/extraction/destinations/formatters/RDFJSONBuilder.scala
+++ core/src/main/scala/org/dbpedia/extraction/destinations/formatters/RDFJSONBuilder.scala
@@ -1,6 +1,6 @@
 package org.dbpedia.extraction.destinations.formatters
 
-import scala.util.parsing.json.JSONFormat
+
 import UriPolicy._
 
 /*
@@ -72,7 +72,33 @@ class RDFJSONBuilder(policies: Array[Policy] = null)
    * Escapes a Unicode string according to JSON Format
    */
   private def escape(input: String): RDFJSONBuilder = {
-    sb append JSONFormat.quoteString(input)
+    
+    /*
+     * Below code is from scala-library-2.9.1/scala/util/parsing/json/Parser.scala.html
+     * 
+     */
+      def quoteString (s : String) : String =
+    s.map {
+      case '"'  => "\\\""
+      case '\\' => "\\\\"
+      case '/'  => "\\/" 
+      case '\b' => "\\b"
+      case '\f' => "\\f"
+      case '\n' => "\\n"
+      case '\r' => "\\r"
+      case '\t' => "\\t"
+      /* We'll unicode escape any control characters. These include:
+       * 0x0 -> 0x1f  : ASCII Control (C0 Control Codes)
+       * 0x7f         : ASCII DELETE
+       * 0x80 -> 0x9f : C1 Control Codes
+       *
+       * Per RFC4627, section 2.5, we're not technically required to
+       * encode the C1 codes, but we do to be safe.
+       */
+      case c if ((c >= '\u0000' && c <= '\u001f') || (c >= '\u007f' && c <= '\u009f')) => "\\u%04x".format(c: Int)
+      case c => c
+    }.mkString
+    sb append quoteString(input)
     this
   }
 
