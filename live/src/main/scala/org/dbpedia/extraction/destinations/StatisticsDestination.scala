4ece4600c1cf41b421b7939b8efd6e2387dcf4cf
Andre Pereira <adrapereira@gmail.com>
2015-07-07 18:10:46 +0100

    Add statistics destination in extraction

--- live/src/main/scala/org/dbpedia/extraction/destinations/StatisticsDestination.scala
+++ live/src/main/scala/org/dbpedia/extraction/destinations/StatisticsDestination.scala
@@ -1,13 +1,33 @@
 package org.dbpedia.extraction.destinations
 
+import org.slf4j.LoggerFactory
+
+import scala.collection.Seq
+
 /**
  * Writes extraction results to statistics
  */
 class StatisticsDestination extends LiveDestination{
 
+  private val logger = LoggerFactory.getLogger(classOf[StatisticsDestination].getName)
+
+  private var addedTriples = 0
+  private var unmodifiedTriples = 0
+  private var now = System.currentTimeMillis
+
+  /**
+   * Opens this destination. This method should only be called once during the lifetime
+   * of a destination, and it should not be called concurrently with other methods of this class.
+   */
   def open() {}
 
-  def write(extractor: String, hash: String, graphAdd: Seq[Quad], graphRemove: Seq[Quad], graphUnmodified: Seq[Quad]) {}
+  override def write(extractor: String, hash: String, graphAdd: Seq[Quad], graphRemove: Seq[Quad], graphUnmodified: Seq[Quad]) {
+    addedTriples += graphAdd.length
+    unmodifiedTriples += graphUnmodified.length
+  }
 
-  def close() {}
+  override def close = {
+    val total = addedTriples + unmodifiedTriples
+    logger.info("produced " + total  + " in " + (System.currentTimeMillis - now) + "ms.")
+  }
 }
