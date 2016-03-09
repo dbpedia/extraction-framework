2dbf19e440397e652ef7628047d39b5a0d69d523
chile12 <markus.freudenberg@gmail.com>
2016-03-09 21:00:23 +0100

    added post processing for statistics

--- scripts/src/main/bash/collectStats.sh
+++ scripts/src/main/bash/collectStats.sh
@@ -4,13 +4,12 @@
 # publish these files alongside the data and use the html example (directory wiki)
 # on how to integrate the stats on the drupal wiki pages
 
-# run this directly from $DBPPREFIX/extraction-framework/scripts
 # for more information about the parameters have a look at the classes in /statistics
 
-../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "short_abstracts" stats-instances-cd.json canonicalized donotlist donotlist
-../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "short_abstracts" stats-instances-ld.json localized donotlist donotlist
-../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "mappingbased_literals,mappingbased_objects,geo_coordinates_mappingbased,instance_types,instance_types_transitive" stats-mappingbased.json canonicalized listproperties donotlist
-../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "mappingbased_literals,mappingbased_objects,geo_coordinates_mappingbased,instance_types,instance_types_transitive" stats-raw-infoboxes.json canonicalized listproperties donotlist
-../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "instance_types,instance_types_transitive" stats-type-statistics.json canonicalized donotlistprops listobjects
+../../../../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "short_abstracts" stats-instances-cd.json canonicalized donotlist donotlist
+../../../../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "short_abstracts" stats-instances-ld.json localized donotlist donotlist
+../../../../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "mappingbased_literals,mappingbased_objects,geo_coordinates_mappingbased,instance_types,instance_types_transitive" stats-mappingbased.json canonicalized listproperties donotlist
+../../../../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "mappingbased_literals,mappingbased_objects,geo_coordinates_mappingbased,instance_types,instance_types_transitive" stats-raw-infoboxes.json canonicalized listproperties donotlist
+../../../../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "instance_types,instance_types_transitive" stats-type-statistics.json canonicalized donotlistprops listobjects
 
-../run PostProcessinfStats /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 stats-instances-cd.json stats-instances-ld.json stats-mappingbased.json stats-raw-infoboxes.json stats-type-statistics.json stats-general-stats.json stats-properties-stats.json stats-types-stats.json
+../../../../run PostProcessinfStats /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 stats-instances-cd.json stats-instances-ld.json stats-mappingbased.json stats-raw-infoboxes.json stats-type-statistics.json stats-general-stats.json stats-properties-stats.json stats-types-stats.json
