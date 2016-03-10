7dfbb512d66ddbf308debc38a127661d14a73d81
chile12 <markus.freudenberg@gmail.com>
2016-03-10 18:28:46 +0100

    added post processing for statistics

--- scripts/src/main/bash/collectStats.sh
+++ scripts/src/main/bash/collectStats.sh
@@ -9,7 +9,7 @@
 ../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "short_abstracts" stats-instances-cd.json canonicalized donotlist donotlist
 ../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "short_abstracts" stats-instances-ld.json localized donotlist donotlist
 ../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "mappingbased_literals,mappingbased_objects,geo_coordinates_mappingbased,instance_types,instance_types_transitive" stats-mappingbased.json canonicalized listproperties donotlist
-../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "mappingbased_literals,mappingbased_objects,geo_coordinates_mappingbased,instance_types,instance_types_transitive" stats-raw-infoboxes.json canonicalized listproperties donotlist
+../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "infobox_properties" stats-raw-infoboxes.json canonicalized listproperties donotlist
 ../run TypeStatistics /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 .ttl.bz2 "instance_types,instance_types_transitive" stats-type-statistics.json canonicalized donotlistprops listobjects
 
-../run PostProcessinfStats /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 stats-instances-cd.json stats-instances-ld.json stats-mappingbased.json stats-raw-infoboxes.json stats-type-statistics.json stats-general-stats.json stats-properties-stats.json stats-types-stats.json
+../run PostProcessinfStats /media/bigone/25TB/www/downloads.dbpedia.org/2015-10 stats-instances-ld.json stats-instances-cd.json stats-mappingbased.json stats-raw-infoboxes.json stats-type-statistics.json stats-general-stats.json stats-properties-stats.json stats-types-stats.json
