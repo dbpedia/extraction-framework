9d399688a4f8550abea1e60d4aa72ffed0118a01
chile12 <markus.freudenberg@gmail.com>
2016-04-07 13:16:44 +0200

    data turncation problem when title is too long for import

--- dump/src/main/scala/org/dbpedia/extraction/dump/sql/Importer.scala
+++ dump/src/main/scala/org/dbpedia/extraction/dump/sql/Importer.scala
@@ -1,5 +1,6 @@
 package org.dbpedia.extraction.dump.sql
 
+import com.mysql.jdbc.MysqlDataTruncation
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException
 import org.dbpedia.extraction.sources.Source
 import org.dbpedia.extraction.util.StringUtils
@@ -142,6 +143,7 @@ class Importer(conn: Connection) {
     catch {
       // throw our own exception that our XML parser won't catch
       case icv: MySQLIntegrityConstraintViolationException =>       //catch unique key violations (which will occur...)
+      case dce: MysqlDataTruncation =>                              //catch if data is too long for a column
       case sqle: SQLException => throw new ImporterException(sqle)
     }
     finally stmt.close
