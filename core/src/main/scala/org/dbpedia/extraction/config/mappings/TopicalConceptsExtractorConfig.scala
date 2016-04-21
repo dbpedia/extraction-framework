9faff2b635817e07300763c10ced00ba8cbda5dd
Fumihiro Kato <fumi@fumi.me>
2016-04-21 19:16:19 +0900

    Added Japanese config for TopicalConceptsExtractor. Japanese Wikipedia seems to use Template:Catmore for this purpose.

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/TopicalConceptsExtractorConfig.scala
@@ -10,7 +10,7 @@ object TopicalConceptsExtractorConfig
     val catMainTemplates = Set(
       "مزيد"   ,// ar
                                "Infocat", "Infocatm", // ca
-                               "Catmore", // el
+                               "Catmore", // el,ja
                                "Cat main", // en
                                "AP", // es
                                "Nagusia", // eu
