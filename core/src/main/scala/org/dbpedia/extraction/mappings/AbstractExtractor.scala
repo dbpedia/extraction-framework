52a2aa59f52962d43b7c9409971447b40313d709
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-05-21 10:54:04 +0200

    Refactor AbstractExtractor to be more configurable and language aware

--- core/src/main/scala/org/dbpedia/extraction/mappings/AbstractExtractor.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/AbstractExtractor.scala
@@ -37,7 +37,7 @@ class AbstractExtractor(
 extends PageNodeExtractor
 {
     //TODO make this configurable
-    protected def apiUrl: String = "https://en.wikipedia.org/w/api.php"
+    protected def apiUrl: String = "http://localhost:8008/mediawiki/api.php"
 
     private val maxRetries = 5
 
---
+++ live/src/main/scala/org/dbpedia/extraction/mappings/AbstractExtractorWikipedia.scala
@@ -0,0 +1,20 @@
+package org.dbpedia.extraction.mappings
+
+import org.dbpedia.extraction.ontology.Ontology
+import org.dbpedia.extraction.util.Language
+
+/**
+  * Overwrites the apiUrl property for the AbstractExtractor in the core module to work with configurable Wikipedias.
+  * @author Lukas Faber, Stephan Haarmann, Sebastian Serth
+  * date 21.05.2016.
+  */
+class AbstractExtractorWikipedia(
+  context : {
+    def ontology : Ontology
+    def language : Language
+  })
+  extends AbstractExtractor (context)
+{
+
+  override def apiUrl: String = "https://" + context.language.wikiCode + ".wikipedia.org/w/api.php"
+}
