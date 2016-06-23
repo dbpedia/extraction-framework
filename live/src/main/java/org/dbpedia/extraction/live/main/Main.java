106b19f382df3e3397be070e89c6182210b4a638
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-06-23 12:07:56 +0200

    Reformat code in Main.java (live extraction) to match code style

--- live/src/main/java/org/dbpedia/extraction/live/main/Main.java
+++ live/src/main/java/org/dbpedia/extraction/live/main/Main.java
@@ -58,27 +58,27 @@ public class Main {
         JDBCUtil.execSQL("SET names utf8");
 
         if (Boolean.parseBoolean(LiveOptions.options.get("feeder.rcstream.enabled"))) {
-            feeders.add(new RCStreamFeeder("RCStreamFeeder", LiveQueuePriority.LivePriority,
-                    LiveOptions.options.get("uploaded_dump_date"), LiveOptions.options.getComment("working_directory"),
-                    LiveOptions.options.get("feeder.rcstream.room")));
+            feeders .add(new RCStreamFeeder("RCStreamFeeder", LiveQueuePriority.LivePriority,
+                LiveOptions.options.get("uploaded_dump_date"), LiveOptions.options.getComment("working_directory"),
+                LiveOptions.options.get("feeder.rcstream.room")));
         }
 
         if (Boolean.parseBoolean(LiveOptions.options.get("feeder.mappings.enabled")) == true) {
             long pollInterval = Long.parseLong(LiveOptions.options.get("feeder.mappings.pollInterval"));
             long sleepInterval = Long.parseLong(LiveOptions.options.get("feeder.mappings.sleepInterval"));
             feeders .add( new OAIFeederMappings("FeederMappings", LiveQueuePriority.MappingPriority,
-                    LiveOptions.options.get("mappingsOAIUri"), LiveOptions.options.get("mappingsBaseWikiUri"), LiveOptions.options.get("mappingsOaiPrefix"),
-                    pollInterval, sleepInterval, LiveOptions.options.get("uploaded_dump_date"),
-                    LiveOptions.options.get("working_directory")));
+                LiveOptions.options.get("mappingsOAIUri"), LiveOptions.options.get("mappingsBaseWikiUri"), LiveOptions.options.get("mappingsOaiPrefix"),
+                pollInterval, sleepInterval, LiveOptions.options.get("uploaded_dump_date"),
+                LiveOptions.options.get("working_directory")));
         }
 
         if (Boolean.parseBoolean(LiveOptions.options.get("feeder.live.enabled")) == true) {
             long pollInterval = Long.parseLong(LiveOptions.options.get("feeder.live.pollInterval"));
             long sleepInterval = Long.parseLong(LiveOptions.options.get("feeder.live.sleepInterval"));
             feeders .add( new OAIFeeder("FeederLive", LiveQueuePriority.LivePriority,
-                    LiveOptions.options.get("oaiUri"), LiveOptions.options.get("baseWikiUri"), LiveOptions.options.get("oaiPrefix"),
-                    pollInterval, sleepInterval, LiveOptions.options.get("uploaded_dump_date"),
-                    LiveOptions.options.get("working_directory")));
+                LiveOptions.options.get("oaiUri"), LiveOptions.options.get("baseWikiUri"), LiveOptions.options.get("oaiPrefix"),
+                pollInterval, sleepInterval, LiveOptions.options.get("uploaded_dump_date"),
+                LiveOptions.options.get("working_directory")));
         }
 
         if (Boolean.parseBoolean(LiveOptions.options.get("feeder.unmodified.enabled")) == true) {
@@ -87,8 +87,8 @@ public class Main {
             int threshold = Integer.parseInt(LiveOptions.options.get("feeder.unmodified.threshold"));
             long sleepTime = Long.parseLong(LiveOptions.options.get("feeder.unmodified.sleepTime"));
             feeders .add( new UnmodifiedFeeder("FeederUnmodified", LiveQueuePriority.UnmodifiedPagePriority,
-                    minDaysAgo, chunk, threshold, sleepTime,
-                    LiveOptions.options.get("uploaded_dump_date"), LiveOptions.options.get("working_directory")));
+                minDaysAgo, chunk, threshold, sleepTime,
+                LiveOptions.options.get("uploaded_dump_date"), LiveOptions.options.get("working_directory")));
         }
 
         int threads = Integer.parseInt(LiveOptions.options.get("ProcessingThreads"));
