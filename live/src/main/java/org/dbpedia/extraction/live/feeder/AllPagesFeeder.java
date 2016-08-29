7d526cfb15856fc7dd50bc0b0777081c68639124
Sebastian Serth <Sebastian.Serth@student.hpi.de>
2016-08-29 14:52:27 +0200

    Refactor AllPagesFeeder to use WikiApi with XML

--- live/src/main/java/org/dbpedia/extraction/live/feeder/AllPagesFeeder.java
+++ live/src/main/java/org/dbpedia/extraction/live/feeder/AllPagesFeeder.java
@@ -4,16 +4,21 @@ import org.dbpedia.extraction.live.core.LiveOptions;
 import org.dbpedia.extraction.live.queue.LiveQueueItem;
 import org.dbpedia.extraction.live.queue.LiveQueuePriority;
 import org.dbpedia.extraction.live.util.DateUtil;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.dbpedia.extraction.live.util.ExceptionUtil;
+import org.dbpedia.extraction.util.Language;
+import org.dbpedia.extraction.util.WikiApi;
+import scala.Tuple3;
+import scala.collection.immutable.Seq;
+import scala.xml.Node;
 
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author Lukas Faber, Stephan Haarmann, Sebastian Serth
@@ -21,9 +26,9 @@ import java.util.Date;
  */
 public class AllPagesFeeder extends Feeder {
 
+    private final Language language = Language.apply(LiveOptions.options.get("language"));
+    private WikiApi api;
     private boolean isFinished = false;
-    private String query_base =
-            "?action=query&format=json&list=allpages&aplimit=500&apnamespace=%d&continue=%s&apcontinue=%s";
     private String continueString = "-||";
     private String continueTitle = "";
     private ArrayList<Integer> allowedNamespaces = new ArrayList<>();
@@ -32,6 +37,12 @@ public class AllPagesFeeder extends Feeder {
     public AllPagesFeeder(String feederName, LiveQueuePriority queuePriority, String defaultStartTime,
                           String folderBasePath) {
         super(feederName, queuePriority, defaultStartTime, folderBasePath);
+        try {
+            api = new WikiApi(new URL(language.apiUri()), language);
+        } catch (MalformedURLException exp) {
+            logger.error(ExceptionUtil.toString(exp), exp);
+        }
+
         for (String namespace : LiveOptions.options.get("feeder.allpages.allowedNamespaces").split("\\s*,\\s*")) {
             allowedNamespaces.add(Integer.parseInt(namespace));
         }
@@ -46,37 +57,22 @@ public class AllPagesFeeder extends Feeder {
     protected Collection<LiveQueueItem> getNextItems() {
         ArrayList<LiveQueueItem> queue = new ArrayList<LiveQueueItem>();
         if (!isFinished) {
-            JSONObject response = queryAllPagesAPI();
-            if (response != null) {
-                JSONArray pages = response.getJSONObject("query").getJSONArray("allpages");
-                for (Object pageObject : pages) {
-                    JSONObject page = (JSONObject) pageObject;
-                    queue.add(new LiveQueueItem(page.getInt("pageid"), page.getString("title"), DateUtil.transformToUTC(new Date()), false, ""));
-                }
-                if (response.has("continue")) {
-                    continueString = response.getJSONObject("continue").getString("continue");
-                    setContinueTitle(response.getJSONObject("continue").getString("apcontinue"));
-                } else {
-                    goToNextNamespace();
-                }
+            List<Node> pageList = queryAllPagesAPI();
+            for (Node page : pageList) {
+                //queue.add(new LiveQueueItem(Long.parseLong(page.$bslash$at("pageid")), page.$bslash$at("title"), DateUtil.transformToUTC(new Date()), false, ""));
+            }
+            if (continueTitle.isEmpty()) {
+                goToNextNamespace();
             }
         }
         return queue;
     }
 
-    private JSONObject queryAllPagesAPI() {
-        String apiURL = LiveOptions.options.get("feeder.allpages.wikiapi");
-        URL url = null;
-        try {
-            url = new URL(apiURL + String.format(query_base, allowedNamespaces.get(currentNamespace), continueString, continueTitle));
-            InputStream responseStream = url.openConnection().getInputStream();
-            java.util.Scanner s = new java.util.Scanner(responseStream).useDelimiter("\\A");
-            return new JSONObject(s.next());
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
-        goToNextNamespace();
-        return null;
+    private List<Node> queryAllPagesAPI() {
+        Tuple3<String, String, Seq<Node>> result = api.retrieveAllPagesPerNamespace(allowedNamespaces.get(currentNamespace), continueString, continueTitle);
+        setContinueTitle(result._1());
+        setContinueString(result._2());
+        return scala.collection.JavaConversions.seqAsJavaList(result._3());
     }
 
     private void goToNextNamespace() {
@@ -89,11 +85,15 @@ public class AllPagesFeeder extends Feeder {
         continueTitle = "";
     }
 
-    public void setContinueTitle(String continueTitle) {
+    private void setContinueTitle(String continueTitle) {
         try {
             this.continueTitle = URLEncoder.encode(continueTitle, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        } catch (UnsupportedEncodingException exp) {
+            logger.error(ExceptionUtil.toString(exp), exp);
         }
+    }
+
+    private void setContinueString(String continueString) {
+        this.continueString = continueString;
     }
 }
