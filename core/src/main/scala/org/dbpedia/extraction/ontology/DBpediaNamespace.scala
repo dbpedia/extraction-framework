49b40187d040586cf34e1d6cec28ef9a21862159
chile12 <markus.freudenberg@gmail.com>
2016-11-16 23:25:49 +0100

    DataIDGeneratorUpdate

--- core/src/main/scala/org/dbpedia/extraction/ontology/DBpediaNamespace.scala
+++ core/src/main/scala/org/dbpedia/extraction/ontology/DBpediaNamespace.scala
@@ -4,22 +4,17 @@ import java.lang.StringBuilder
 
 import org.dbpedia.extraction.util.StringUtils.{escape, replacements}
 
-class DBpediaNamespace private (namespace: String) extends RdfNamespace(null, namespace, true) {
+class DBpediaNamespace (namespace: String) extends RdfNamespace(null, namespace, true) {
   
   override protected def append(sb: StringBuilder, suffix: String): Unit = {
     escape(sb, suffix, DBpediaNamespace.iriEscapes)
   }
 
   override def toString: String = namespace
+
 }
 
 object DBpediaNamespace {
-
-  val ONTOLOGY = ns("http://dbpedia.org/ontology/")
-  val DATATYPE = ns("http://dbpedia.org/datatype/")
-  val DATASET = ns("http://dbpedia.org/dataset/")
-  val EXTRACTOR = ns("http://dbpedia.org/extractor/")
-  val TRANSFORM = ns("http://dbpedia.org/transform/")
 
   // for this list of characters, see RFC 3987 and https://sourceforge.net/mailarchive/message.php?msg_id=28982391
   private val iriEscapes = {
@@ -33,5 +28,11 @@ object DBpediaNamespace {
   private def ns(namespace: String): DBpediaNamespace = {
     new DBpediaNamespace(namespace)
   }
+
+  val ONTOLOGY = ns("http://dbpedia.org/ontology/")
+  val DATATYPE = ns("http://dbpedia.org/datatype/")
+  val DATASET = ns("http://dbpedia.org/dataset/")
+  val EXTRACTOR = ns("http://dbpedia.org/extractor/")
+  val TRANSFORM = ns("http://dbpedia.org/transform/")
 
 }
