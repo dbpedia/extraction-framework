e8cc4fcd1a06eddb0395a1a4cf7da0f2fe7cd415
wmaroy <maroy.wouter@gmail.com>
2017-07-04 16:55:21 +0200

    updated RML Formatter: initial support for conditionals

--- core/src/main/scala/org/dbpedia/extraction/mappings/rml/translation/formatter/RMLFormatter.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/rml/translation/formatter/RMLFormatter.scala
@@ -19,8 +19,9 @@ object RMLFormatter extends Formatter {
       val triplesMapPart = getTriplesMapPart(model, base)
       val subjectMapPart = getSubjectMapPart(model, base)
       val mappingsPart = getAllMappings(model, base)
+      val conditionalsPart = getAllConditionalMappings(model, base)
       val functionsPart = getFunctions(model, base)
-      print(Seq(prefixes, triplesMapPart, subjectMapPart, functionsPart, mappingsPart).reduce((first, second) => first.concat('\n' + second)))
+      print(Seq(prefixes, triplesMapPart, subjectMapPart, functionsPart, mappingsPart, conditionalsPart).reduce((first, second) => first.concat('\n' + second)))
       triplesMapPart
     } catch {
       case x : Exception => x.printStackTrace(); null
@@ -86,11 +87,38 @@ object RMLFormatter extends Formatter {
     statements.asScala.map(statement => {
 
       val predicateObjectMap = statement.getObject
-      val properties = predicateObjectMap.asResource().listProperties()
-      getMapping(properties, base)
+      if(!hasConditions(predicateObjectMap.asResource())) {
+        val properties = predicateObjectMap.asResource().listProperties()
+        getMapping(properties, base)
+      } else {
+        "" // skip conditionals here
+      }
 
     }).reduce((first, second) => first.concat("\n" + second)) // so return all concatenated mappings
 
+  }
+
+  private def getAllConditionalMappings(model: RMLModel, base : String) : String = {
+
+    // Get the normal (non-conditional) mappings first
+    val triplesMapResource = model.triplesMap.resource
+    val statements = triplesMapResource.listProperties(model.model.createProperty(RdfNamespace.RR.namespace + "predicateObjectMap")).toList
+
+    val tuple = statements.asScala.map(statement => {
+
+      val predicateObjectMap = statement.getObject
+      if(hasConditions(predicateObjectMap.asResource())) {
+        getConditionalMapping(predicateObjectMap.asResource(), base)
+      } else {
+        ("","") // skip conditionals here
+      }
+
+    }).filter(tuple => !tuple._1.equals("") && !tuple._2.equals(""))
+      .reduce((first, second) => (first._1.concat("\n" + second._1), first._2.concat("\n" + second._2))) // so return all concatenated mappings
+
+    val heading = "########\n\n#### Conditional Mappings\n"
+
+    heading + tuple._1.concat("\n" + tuple._2)
   }
 
   /**
@@ -125,6 +153,71 @@ object RMLFormatter extends Formatter {
 
     heading + predicateObjectMapString + offset + functionTermMapString
   }
+
+  /**
+    * Retrieves all necessary constructs for a conditional mapping
+    *
+    * Returns a tuple: ._1 = PredicateObjectMaps itself, ._2 = FunctionTermMaps from the conditional function
+    *
+    * @param resource
+    * @param base
+    * @return
+    */
+  private def getConditionalMapping(resource : Resource, base : String) : (String, String) = {
+    if(hasConditions(resource)) {
+      val current_condition = getCondition(resource, base)
+      if(hasFallback(resource)) {
+        val fallbackMap = getFallbackMap(resource)
+        val other_conditions = getConditionalMapping(fallbackMap, base)
+        (current_condition._1 + offset + other_conditions._1, current_condition._2 + offset + other_conditions._2)
+      } else {
+        (getResourceString(resource,base), "")
+      }
+    } else {
+      (getResourceString(resource,base), "")
+    }
+  }
+
+  /**
+    * Returns crml:equalCondition and all other necessary constructs
+    *
+    * @param resource
+    * @param base
+    * @return
+    */
+  private def getCondition(resource : Resource, base : String) : (String, String) = {
+    val property = resource.getModel.getProperty(RdfNamespace.CRML.namespace + "equalCondition")
+    val functionPOM = getResourceString(resource.getPropertyResourceValue(property).asResource(), base)
+    val conditionPOM = getResourceString(resource, base)
+    (conditionPOM, functionPOM)
+  }
+
+  /**
+    * Returns a string with all the properties of a resource in Turtle, prefixed, without prefixes
+    *
+    * @param resource
+    * @param base
+    * @return
+    */
+  private def getResourceString(resource : Resource, base : String) : String = {
+    val statements = resource.listProperties().toList.asScala.toArray
+    val modelWrapper = createModelWrapper(statements)
+    val turtle = modelWrapper.writeAsTurtle(base)
+    removePrefixes(turtle)
+  }
+
+  /**
+    * Retrieves a crml:FallbackMap
+    *
+    * @param resource
+    * @return
+    */
+  private def getFallbackMap(resource: Resource) : Resource = {
+    val property = resource.getModel.getProperty(RdfNamespace.CRML.namespace + "fallbackMap")
+    resource.getPropertyResourceValue(property)
+  }
+
+
 
   /**
     * Retrieves all function rr:PredicateObjectMaps
@@ -290,6 +383,17 @@ object RMLFormatter extends Formatter {
   }
 
   /**
+    * Checks if given resource contains a crml:fallbackMap
+    *
+    * @param resource
+    * @return
+    */
+  private def hasFallback(resource: Resource) : Boolean = {
+    val property = resource.getModel.createProperty(RdfNamespace.CRML.namespace + "fallbackMap")
+    resource.hasProperty(property)
+  }
+
+  /**
     * Retrievs the object map resource of a resource
     *
     * @param resource
@@ -298,6 +402,17 @@ object RMLFormatter extends Formatter {
   private def getObjectMap(resource: Resource) : Resource = {
     val objectMapProperty = resource.getModel.getProperty(RdfNamespace.RR.namespace + "objectMap")
     resource.getProperty(objectMapProperty).getObject.asResource()
+  }
+
+  /**
+    * Checks if given resource contains crml:equalCondition
+    *
+    * @param resource
+    * @return
+    */
+  private def hasConditions(resource : Resource) : Boolean = {
+    val property = resource.getModel.createProperty(RdfNamespace.CRML.namespace + "equalCondition")
+    resource.hasProperty(property)
   }
 
 
@@ -329,5 +444,18 @@ object RMLFormatter extends Formatter {
 
     result
 
+  }
+
+  /**
+    * Creates a new Model Wrapper with prefixes and statements
+    *
+    * @param statements
+    * @return
+    */
+  private def createModelWrapper(statements : Array[Statement]) = {
+    val freshModel = new ModelWrapper
+    freshModel.insertRDFNamespacePrefixes()
+    freshModel.model.add(statements)
+    freshModel
   }
 }
