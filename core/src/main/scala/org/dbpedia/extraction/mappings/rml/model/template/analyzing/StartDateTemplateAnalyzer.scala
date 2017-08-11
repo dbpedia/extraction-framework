97a042d5b2dd7b4e83771c74a647fc556a3c6873
wmaroy <maroy.wouter@gmail.com>
2017-08-11 18:28:36 +0200

    StartDate and EndDate template analyzers updated

--- core/src/main/scala/org/dbpedia/extraction/mappings/rml/model/template/analyzing/StartDateTemplateAnalyzer.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/rml/model/template/analyzing/StartDateTemplateAnalyzer.scala
@@ -3,13 +3,13 @@ package org.dbpedia.extraction.mappings.rml.model.template.analyzing
 import java.util.logging.Logger
 
 import org.dbpedia.extraction.mappings.rml.model.resource.{RMLFunctionTermMap, RMLPredicateObjectMap, RMLUri}
-import org.dbpedia.extraction.mappings.rml.model.template.Template
-import org.dbpedia.extraction.ontology.Ontology
+import org.dbpedia.extraction.mappings.rml.model.template.{StartDateTemplate, Template}
+import org.dbpedia.extraction.ontology.{Ontology, RdfNamespace}
 
 /**
   * Created by wmaroy on 11.08.17.
   */
-class StartDateTemplateAnalyzer(ontology: Ontology) extends TemplateAnalyzer {
+class StartDateTemplateAnalyzer(ontology: Ontology) extends AbstractTemplateAnalyzer(ontology) {
 
   val logger = Logger.getGlobal
 
@@ -20,7 +20,10 @@ class StartDateTemplateAnalyzer(ontology: Ontology) extends TemplateAnalyzer {
     val ftm = pom.objectMap.asInstanceOf[RMLFunctionTermMap]
     val fn = ftm.getFunction
 
-    null
+    val ontologyProperty = loadProperty(pom.rrPredicate)
+    val property = fn.references(RdfNamespace.DBF.namespace + "startDatePropertyParameter")
+
+    StartDateTemplate(property, ontologyProperty)
 
   }
 
