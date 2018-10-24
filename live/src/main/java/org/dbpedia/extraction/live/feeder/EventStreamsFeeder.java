a15a9546e4f4edf7d59dc5d5edd0c8b646a8b389
Lena <lena.schindler@posteo.de>
2018-10-24 19:28:28 +0200

    reformat

--- live/src/main/java/org/dbpedia/extraction/live/feeder/EventStreamsFeeder.java
+++ live/src/main/java/org/dbpedia/extraction/live/feeder/EventStreamsFeeder.java
@@ -3,31 +3,32 @@ package org.dbpedia.extraction.live.feeder;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+
 import org.dbpedia.extraction.live.queue.LiveQueueItem;
 import org.dbpedia.extraction.live.queue.LiveQueuePriority;
 import org.dbpedia.extraction.live.util.DateUtil;
 
 
-public class EventStreamsFeeder extends Feeder{
+public class EventStreamsFeeder extends Feeder {
 
 
-  public EventStreamsFeeder(String feederName,
-      LiveQueuePriority queuePriority,
-      String defaultStartTime, String folderBasePath) {
-    super(feederName, queuePriority, defaultStartTime, folderBasePath);
-  }
+    public EventStreamsFeeder(String feederName,
+                              LiveQueuePriority queuePriority,
+                              String defaultStartTime, String folderBasePath) {
+        super(feederName, queuePriority, defaultStartTime, folderBasePath);
+    }
 
-  @Override
-  protected void initFeeder() {
+    @Override
+    protected void initFeeder() {
 
-  }
+    }
 
-  @Override
-  protected Collection<LiveQueueItem> getNextItems() {
-    ArrayList<LiveQueueItem> queue = new ArrayList<>();
+    @Override
+    protected Collection<LiveQueueItem> getNextItems() {
+        ArrayList<LiveQueueItem> queue = new ArrayList<>();
 
-    queue.add(new LiveQueueItem(0, "Frances Broaddus-Crutchfield", DateUtil.transformToUTC(1538755501), false, ""));
+        queue.add(new LiveQueueItem(0, "Frances Broaddus-Crutchfield", DateUtil.transformToUTC(1538755501), false, ""));
 
-    return queue;
-  }
+        return queue;
+    }
 }
