65773249ea54220d23fda5a9d92b73bbac7f8adb
Lena <lena.schindler@posteo.de>
2018-11-19 20:17:28 +0100

    EventStremsFeeder adapted in order to make use of the EventStreamsHelper

--- live/src/main/java/org/dbpedia/extraction/live/feeder/EventStreamsFeeder.java
+++ live/src/main/java/org/dbpedia/extraction/live/feeder/EventStreamsFeeder.java
@@ -2,33 +2,50 @@ package org.dbpedia.extraction.live.feeder;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
-
+import org.dbpedia.extraction.live.core.LiveOptions;
 import org.dbpedia.extraction.live.queue.LiveQueueItem;
 import org.dbpedia.extraction.live.queue.LiveQueuePriority;
-import org.dbpedia.extraction.live.util.DateUtil;
+import org.dbpedia.utils.sse.EventStreamsHelper;
 
+
+//https://stream.wikimedia.org/v2/stream/recentchange
 
 public class EventStreamsFeeder extends Feeder {
 
+    private static Collection<LiveQueueItem> queueItemCollection;
+    private ArrayList<Integer> allowedNamespaces;
+    private String language;
 
     public EventStreamsFeeder(String feederName,
                               LiveQueuePriority queuePriority,
                               String defaultStartTime, String folderBasePath) {
         super(feederName, queuePriority, defaultStartTime, folderBasePath);
+        queueItemCollection = new ArrayList<>();
+        allowedNamespaces = new ArrayList<>();
+        for (String namespace : LiveOptions.options.get("feeder.eventstreams.allowedNamespaces").split("\\s*,\\s*")) {
+            allowedNamespaces.add(Integer.parseInt(namespace));
+        }
+        language = LiveOptions.options.get("language");
     }
+
 
     @Override
     protected void initFeeder() {
-
+        EventStreamsHelper helper = new EventStreamsHelper(language, allowedNamespaces);
+        helper.eventStreamsClient();
     }
 
     @Override
     protected Collection<LiveQueueItem> getNextItems() {
-        ArrayList<LiveQueueItem> queue = new ArrayList<>();
+        Collection <LiveQueueItem> returnQueueItemCollection;
+        synchronized (this){
+            returnQueueItemCollection = queueItemCollection;
+            queueItemCollection = new ArrayList<>();
+        }
+        return returnQueueItemCollection;
+    }
 
-        queue.add(new LiveQueueItem(0, "Frances Broaddus-Crutchfield", DateUtil.transformToUTC(1538755501), false, ""));
-
-        return queue;
+    public static synchronized void addQueueItemCollection(LiveQueueItem item){
+        queueItemCollection.add(item);
     }
 }
