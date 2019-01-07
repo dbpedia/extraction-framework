ff37547ea019d1ab1c2fb27114066fdfe2e6e8ee
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-01-07 18:51:18 +0100

    added errorlog of rapper

--- scripts/src/main/bash/sortandrap.sh
+++ scripts/src/main/bash/sortandrap.sh
@@ -4,9 +4,12 @@ if [ ! -d "tmpfolder" ]; then
     mkdir -p tmpfolder
 fi
 
+LOG=tmpfolder/logfile
+echo -n "" > $LOG
+
 for file in */src/main/databus/*/*.ttl.bz2; do
         echo "processing $file ...";
-        lbzip2 -dc $file | LC_ALL=C sort --parallel=8 -u -T tmpfolder | rapper -i ntriples -O - - file | lbzip2 > tmpfile;
+        lbzip2 -dc $file | LC_ALL=C sort --parallel=8 -u -T tmpfolder | rapper -i ntriples -O - - file 2>>$LOG | lbzip2 > tmpfile;
         echo "finished processing $file";
         mv tmpfile $file;
 done
