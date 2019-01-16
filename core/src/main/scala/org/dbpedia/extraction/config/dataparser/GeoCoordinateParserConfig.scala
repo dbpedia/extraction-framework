17d7493da474a3e7fab6fb8622bbad9e01756fcb
Anubhav Ujjawal <anubhavujjawal@gmail.com>
2019-01-16 10:56:57 +0530

    Extended scalesMap for en, fixed typos in comments
    - Extended en scalesMap in ParserUtilsConfig.scala
    - Fixed typos in comments in GeoCoordinateParserConfig.scala

--- core/src/main/scala/org/dbpedia/extraction/config/dataparser/GeoCoordinateParserConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/dataparser/GeoCoordinateParserConfig.scala
@@ -10,14 +10,14 @@ object GeoCoordinateParserConfig
                                  //"coor dms/new", "coor dec", "coor/new", "coor dms/archive001",
                                  //"coord/conversion", "coord/templates", "location dec"
     
-    //map latitude letters used in languages to the ones used in English ("E" for East and "W" for West) 
+    //map longitude letters used in languages to the ones used in English ("E" for East and "W" for West) 
     val longitudeLetterMap = Map(
         "de" -> Map("E" -> "E", "O" -> "E", "W" -> "W"),
         "en" -> Map("E" -> "E", "W" -> "W"),
         "fr" -> Map("E" -> "E", "O" -> "W", "W" -> "W")
     )
 
-    //map longitude letters used in languages to the ones used in English ("N" for North and "S" for South)
+    //map latitude letters used in languages to the ones used in English ("N" for North and "S" for South)
     val latitudeLetterMap = Map(
         "en" -> Map("N" -> "N", "S" -> "S")
     )
