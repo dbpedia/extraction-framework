807eab5d5801c58b4ec8ba72183ddd3059669ab0
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-08-16 16:56:20 +0200

    first version including shacl

--- dump/src/test/scala/org/dbpedia/extraction/dump/MinidumpTests.scala
+++ dump/src/test/scala/org/dbpedia/extraction/dump/MinidumpTests.scala
@@ -1,11 +1,13 @@
 package org.dbpedia.extraction.dump
 
-import java.io.File
+import java.io.{File, FileInputStream}
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.concurrent.ConcurrentLinkedQueue
 
 import org.apache.commons.io.FileUtils
+import org.apache.jena.rdf.model.{Model, ModelFactory}
+import org.apache.jena.riot.{RDFDataMgr, RDFLanguages}
 import org.apache.spark.sql.{SQLContext, SparkSession}
 import org.dbpedia.extraction.config.Config
 import org.dbpedia.extraction.dump.extract.{ConfigLoader, Extraction}
@@ -21,37 +23,53 @@ class MinidumpTests extends FunSuite with BeforeAndAfterAll {
   /**
     * in src/test/resources/
     */
-  val extractionConfig = "extraction.minidump.properties"
-  val pageArticlesMinidump = "mini-enwiki.xml.bz2"
-  val wikiMasque= "enwiki"
+  val date = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime)
 
-  var dumpPath = ""
+  //Workaround to get resource files in Scala 2.11
+  val classLoader =   getClass.getClassLoader
+  val config = new Config(classLoader.getResource("extraction.minidump.properties").getFile)
+  val minidumpURL = classLoader.getResource("mini-enwiki.xml.bz2")
+  val ciTestFile = classLoader.getResource("dbpedia-specific-ci-tests.ttl").getFile
+  val custom_SHACL_testFile = classLoader.getResource("custom-shacl-tests.ttl").getFile
+  val ciTestModel: Model = ModelFactory.createDefaultModel()
+
+  val dbpedia_ontologyFile = classLoader.getResource("dbpedia.owl").getFile
+
+  val wikiMasque= "enwiki"
+  val minidumpwikiDirectory = new File(config.dumpDir, s"$wikiMasque/$date/")
+  val dumpDirectory = new File(config.dumpDir, s"$wikiMasque/$date/")
+  //val testModelFile: File = new File("../dump/src/test/resources/new_release_based_ci_tests_draft.ttl")
+
+
+  //val extractionConfig = "extraction.minidump.properties"
+  //val pageArticlesMinidump = "mini-enwiki.xml.bz2"
 
   override def beforeAll() {
 
+    /**
+      * check ttl file for CI here
+      */
+
+    println("Loading triggers and validators")
+
+    RDFDataMgr.read(ciTestModel, new FileInputStream(ciTestFile),RDFLanguages.TURTLE)
+
+
+
+
     println("Extracting Minidump")
-
-    val date = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime)
-
-    //Workaround to get resource files in Scala 2.11
-    val classLoader = getClass.getClassLoader
-
-    val configURL = classLoader.getResource(extractionConfig)
-    val config = new Config(configURL.getFile)
-
-    val minidumpwikiDirectory = new File(config.dumpDir, s"$wikiMasque/$date/")
-    dumpPath = minidumpwikiDirectory.getAbsolutePath
+    // create directories
     minidumpwikiDirectory.mkdirs()
-
-    val minidumpURL = classLoader.getResource(pageArticlesMinidump)
-
+    // copy dumps
     FileUtils.copyFile(
       new File(minidumpURL.getFile),
       new File(minidumpwikiDirectory, s"$wikiMasque-$date-pages-articles-multistream.xml.bz2")
     )
+    /**
+      * extraction
+       */
 
     val configLoader = new ConfigLoader(config)
-
     val parallelProcesses = if(config.runJobsInParallel) config.parallelProcesses else 1
     val jobsRunning = new ConcurrentLinkedQueue[Future[Unit]]()
     //Execute the extraction jobs one by one
@@ -71,10 +89,25 @@ class MinidumpTests extends FunSuite with BeforeAndAfterAll {
       Thread.sleep(1000)
   }
 
+
+  test("RDFUnit SHACL"){
+    val filesToBeValidated = dumpDirectory.listFiles.filter(_.isFile).filter(_.toString.endsWith(".ttl.bz2")).toList
+    println(filesToBeValidated)
+
+    val dbpedia_ont: Model = ModelFactory.createDefaultModel()
+    RDFDataMgr.read(dbpedia_ont, new FileInputStream(dbpedia_ontologyFile),RDFLanguages.RDFXML)
+
+    val custom_SHACL_tests: Model = ModelFactory.createDefaultModel()
+    RDFDataMgr.read(custom_SHACL_tests, new FileInputStream(custom_SHACL_testFile),RDFLanguages.TURTLE)
+
+    assert(dbpedia_ont.size()>0)
+    assert(custom_SHACL_tests.size()>0)
+
+
+  }
+
   test("IRI Coverage Tests") {
 
-    val classLoader = getClass.getClassLoader
-    val pathToTestModelFilePath = classLoader.getResource("dbpedia-specific-ci-tests.ttl")
 
     val hadoopHomeDir = new File("./.haoop/")
     hadoopHomeDir.mkdirs()
@@ -89,8 +122,8 @@ class MinidumpTests extends FunSuite with BeforeAndAfterAll {
     val sqlContext: SQLContext = sparkSession.sqlContext
 
     val eval = ValidationExecutor.testIris(
-      pathToFlatTurtleFile =  s"$dumpPath/*.ttl.bz2",
-      pathToTestCases = pathToTestModelFilePath.getFile
+      pathToFlatTurtleFile =  s"${dumpDirectory.getAbsolutePath}/*.ttl.bz2",
+      pathToTestCases = ciTestFile
 
     )(sqlContext)
 
