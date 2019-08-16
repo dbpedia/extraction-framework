0917118307cab71f961ab81af05c518216873ea4
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-08-16 11:55:26 +0200

    created multilingual architecture

--- dump/src/test/bash/createMinidump.sh
+++ dump/src/test/bash/createMinidump.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-LIST=uris.lst
-URIS=`cat $LIST`
-TARGET="../resources/mini-enwiki.xml"
+LANG="en fr"
 
-# copy header
-cp head.xml "$TARGET"
-
-for i in $URIS ; do
-	echo "" >> "$TARGET"
-	curl $i | xmlstarlet sel -N x="http://www.mediawiki.org/xml/export-0.10/"  -t -c "//x:page" >> $TARGET
-	echo "" >> "$TARGET"
+for l in ${LANG} ; do
+	TARGET="../resources/mini-"$l"wiki.xml"
+	# copy header
+	cp head.xml "$TARGET"
+    for u in `cat "uris-$l.lst"` ; do
+		echo "" >> "$TARGET"
+		echo $TARGET
+		curl $u  | xmlstarlet sel -N x="http://www.mediawiki.org/xml/export-0.10/"  -t -c "//x:page" >> $TARGET
+		echo "" >> "$TARGET"
+	done
+	echo "</mediawiki>\n" >> $TARGET
+	cat "$TARGET" | lbzip2 > "$TARGET.bz2"
+	rm $TARGET
 done
 
-echo "</mediawiki>\n" >> $TARGET
-
-cat "$TARGET" | lbzip2 > "$TARGET.bz2"
-rm $TARGET
 # curl $FIRST > $TMPFOLDER/main2.xml
 #xmlstarlet sel -N x="http://www.mediawiki.org/xml/export-0.10/" -t -c "//page" main2.xml
 #  xmlstarlet ed  -N x="http://www.mediawiki.org/xml/export-0.10/" --subnode "/x:mediawiki/x:siteinfo" --type elem -n "newsubnode" -v "" head.xml
