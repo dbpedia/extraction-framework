f3c73bc5f6f77972a556f56e0c0fd4e9d5cead43
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-08-28 00:57:31 +0200

    finished

--- dump/src/test/scala/org/dbpedia/extraction/dump/MinidumpTests.scala
+++ dump/src/test/scala/org/dbpedia/extraction/dump/MinidumpTests.scala
@@ -88,8 +88,8 @@ class MinidumpTests extends FunSuite with BeforeAndAfterAll {
        */
     val jobsRunning = new ConcurrentLinkedQueue[Future[Unit]]()
 
-    //extract(mappingsConfig,jobsRunning)
-    //extract(genericConfig,jobsRunning)
+    extract(mappingsConfig,jobsRunning)
+    extract(genericConfig,jobsRunning)
     extract (nifAbstractConfig, jobsRunning)
 
     def extract (config: Config, jobsRunning:ConcurrentLinkedQueue[Future[Unit]]) = {
