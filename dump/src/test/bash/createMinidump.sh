ebc5d730130dd634d6a3071c5f7d9114306604f9
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-09-19 18:14:20 +0200

    towards multilingual tests

--- dump/src/test/bash/createMinidump.sh
+++ dump/src/test/bash/createMinidump.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
 
-LANG="en fr de nl"
+
+# sort the file
+sort -u -o uris.lst uris.lst 
+
+# detect languages
+LANG=`sed 's|^https://||;s|\.wikipedia.org.*||' uris.lst | sort -u` 
+
+
 
 for l in ${LANG} ; do
-	TARGET="../resources/minidumps/"$l"/wiki.xml"
+	echo "LANGUAGE $l"
+	PAGES=`grep "$l.wikipedia.org" uris.lst | sed 's|wikipedia.org/wiki/|wikipedia.org/wiki/Special:Export/|' `
 	# copy header
+	TARGET="../resources/minidumps/"$l"/wiki.xml"
+	echo "TARGET: $TARGET"
 	cp head.xml "$TARGET"
-    for u in `cat "uris-$l.lst"` ; do
+	# process pages
+	for p in ${PAGES}; do
+		echo "PAGE: $p"
 		echo "" >> "$TARGET"
-		echo $TARGET
-		curl $u  | xmlstarlet sel -N x="http://www.mediawiki.org/xml/export-0.10/"  -t -c "//x:page" >> $TARGET
+		
+		curl -L $p  | xmlstarlet sel -N x="http://www.mediawiki.org/xml/export-0.10/"  -t -c "//x:page" >> $TARGET
 		echo "" >> "$TARGET"
 	done
 	echo "</mediawiki>\n" >> $TARGET
 	cat "$TARGET" | lbzip2 > "$TARGET.bz2"
 	rm $TARGET
-done
+	 
+done 
 
 # curl $FIRST > $TMPFOLDER/main2.xml
 #xmlstarlet sel -N x="http://www.mediawiki.org/xml/export-0.10/" -t -c "//page" main2.xml
