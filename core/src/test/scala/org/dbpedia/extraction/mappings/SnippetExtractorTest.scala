e8cdec02f9db77d06bb9140d21b217ad7a350098
kurzum <kurzum@github.com>
2020-03-30 07:35:08 +0200

    experimented with snippet test

--- core/src/test/scala/org/dbpedia/extraction/mappings/SnippetExtractorTest.scala
+++ core/src/test/scala/org/dbpedia/extraction/mappings/SnippetExtractorTest.scala
@@ -10,12 +10,35 @@ import org.scalatest.{FlatSpec, Matchers, PrivateMethodTester}
 import scala.language.reflectiveCalls
 
 /**
-  *
-  */
+ *
+ */
 @RunWith(classOf[JUnitRunner])
 class SnippetExtractorTest extends FlatSpec with Matchers with PrivateMethodTester {
+  private val parser = WikiParser.getInstance()
 
-  "InfoboxMappingsExtractor" should """return correct property id's for YM infobox""" in {
+  private val context = new {
+    def ontology = InfoboxMappingsExtractorTest.context.ontology;
+    def language = Language.English;
+    def redirects = new Redirects(Map())
+  }
+
+
+  //TODO LANGUAGE IS NOT USED CORRECTLY
+  private def extractPageNode(input: String, extractor: PageNodeExtractor, lang:Language): Seq[Quad] = {
+    val title = "TestPage"
+    val subjectUri = "http://dbpedia.org/resource/" + title
+    val page = new WikiPage(WikiTitle.parse("TestPage", lang), input)
+
+    extractor.extract(page.pageNode.get, subjectUri)
+      .map(quad => quad.copy(dataset = null, context = null, language = if (quad.datatype != null && quad.datatype.endsWith("langString")) quad.datatype else null)) // remove dataset
+
+  }
+/* TESTS */
+
+  private val homepageExtractor: PageNodeExtractor = new HomepageExtractor(context)
+  private val infoboxExtractor: PageNodeExtractor = new InfoboxExtractor(context)
+
+  "HomepageExtractor" should """return correct homepage for external links""" in {
 
     val lang = Language.English
 
@@ -27,32 +50,41 @@ class SnippetExtractorTest extends FlatSpec with Matchers with PrivateMethodTest
       homepageExtractor)
 
 
-      val answer: Seq[Quad] = Seq(
-       Quad.unapply("<http://dbpedia.org/resource/TestPage> <http://xmlns.com/foaf/0.1/homepage> <http://www.homepage.com> ." )
-      ).map(_.get)
 
-    (quads) should be (answer)
+    val answer: Seq[Quad] = Seq(
+      Quad.unapply("<http://dbpedia.org/resource/TestPage> <http://xmlns.com/foaf/0.1/homepage> <http://www.homepage.com> .")
+    ).map(_.get)
+
+    (quads) should be(answer)
   }
 
-  private val parser = WikiParser.getInstance()
+  "InfoboxExtractor" should """recent bug""" in {
 
-  private val context = new {
-    def ontology = InfoboxMappingsExtractorTest.context.ontology;
-    def language = Language.English;
-    def redirects = new Redirects(Map())
+    //https://en.wikipedia.org/wiki/Abbas_Gharabaghi
+    val wiki =
+      """
+        |{{Infobox Officeholder
+        || birth_place         = [[Tabriz]], Iran
+        || birth_place2         = [[Tabriz]]
+        |}}
+        |""".stripMargin
+    print(wiki)
+
+    val quads = extractPageNode(
+      wiki,
+      infoboxExtractor,
+      Language.English)
+
+    //TODO how do you print the triples as ntriples (for posting into answer)
+    quads.foreach(println)
+
+    val answer: Seq[Quad] = Seq(
+      Quad.unapply("<http://dbpedia.org/resource/TestPage> <http://xmlns.com/foaf/0.1/homepage> <http://www.homepage.com> .")
+    ).map(_.get)
+
+    //(quads) should be(answer)
   }
-  private val homepageExtractor: PageNodeExtractor = new HomepageExtractor(context)
 
 
-  private def extractPageNode(input : String, extractor: PageNodeExtractor) : Seq[Quad] =
-  {
-    val title = "TestPage"
-    val subjectUri = "http://dbpedia.org/resource/" + title
-    val page = new WikiPage(WikiTitle.parse("TestPage", Language.English), input)
-
-    extractor.extract(page.pageNode.get, subjectUri)
-        .map(quad => quad.copy(dataset =  null, context = null, language = if (quad.datatype != null && quad.datatype.endsWith("langString")) quad.datatype else null)) // remove dataset
-
-  }
 }
 
