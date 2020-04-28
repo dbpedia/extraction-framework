81c30d61bc1c49ec95547b0428b6fe013ee0da1d
vehnem <vehnem@yahoo.de>
2020-04-29 00:20:23 +0200

    Construct Validation: GenericLiteralLangTagValidator

--- dump/src/main/scala/org/dbpedia/validation/construct/model/validators/generic/GenericLiteralLangTagValidator.scala
+++ dump/src/main/scala/org/dbpedia/validation/construct/model/validators/generic/GenericLiteralLangTagValidator.scala
@@ -1,18 +1,23 @@
 package org.dbpedia.validation.construct.model.validators.generic
 
-import org.apache.jena.rdf.model.impl.NTripleReader
 import org.dbpedia.validation.construct.model
 import org.dbpedia.validation.construct.model.{ValidatorID, ValidatorIRI, ValidatorType}
 import org.dbpedia.validation.construct.model.validators.Validator
 
+/**
+ * TODO: use jena strict?
+ * @param ID
+ */
 case class GenericLiteralLangTagValidator(ID: ValidatorID) extends Validator {
 
   override val METHOD_TYPE: model.ValidatorType.Value = ValidatorType.TYPED_LITERAL
   override val iri: ValidatorIRI = "#GENERIC_LTIERAL_LANG_TAG_VALIDATOR"
 
-  //https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+  private val pattern = ".*@[a-zA-Z]+(-[a-zA-Z0-9]+)*$".r.pattern
+
   override def run(nTriplePart: String): Boolean = {
-    true
+
+    pattern.matcher(nTriplePart).matches()
   }
 
   override def info(): String = "Literal language tag conformity to BCP47 (prevalence:= literals with lang tags)"
