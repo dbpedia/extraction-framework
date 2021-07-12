a02c5e8e2da78b3013ac3a40405d1bdfc49098c0
jlareck <medik2507@gmail.com>
2021-07-13 00:37:16 +0300

    add more unit tests for removing broken brackets function

--- core/src/test/scala/org/dbpedia/extraction/util/WikiUtilsTest.scala
+++ core/src/test/scala/org/dbpedia/extraction/util/WikiUtilsTest.scala
@@ -8,4 +8,22 @@ class WikiUtilsTest extends FunSuite {
     val resultText = WikiUtil.removeBrokenBracketsInAbstracts(text)
     assert(resultText == expectedText)
   }
+  test("test removing broken brackets function with only empty brackets") {
+    val text = "Berlin () is the capital and largest () city of Germany by both area and population."
+    val expectedText = "Berlin is the capital and largest city of Germany by both area and population."
+    val resultText = WikiUtil.removeBrokenBracketsInAbstracts(text)
+    assert(resultText == expectedText)
+  }
+  test("test removing broken brackets function with bracket and semicolon") {
+    val text = "Berlin (; German: [bɛʁˈliːn]) is the capital and largest city of Germany by both area and population."
+    val expectedText = "Berlin is the capital and largest city of Germany by both area and population."
+    val resultText = WikiUtil.removeBrokenBracketsInAbstracts(text)
+    assert(resultText == expectedText)
+  }
+  test("test removing broken brackets function with bracket and comma") {
+    val text = "Berlin (, German: [bɛʁˈliːn]) is the capital and largest city of Germany by both area and population."
+    val expectedText = "Berlin is the capital and largest city of Germany by both area and population."
+    val resultText = WikiUtil.removeBrokenBracketsInAbstracts(text)
+    assert(resultText == expectedText)
+  }
 }
